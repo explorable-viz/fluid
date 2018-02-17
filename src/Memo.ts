4f3737fc78ba62420ee7818d3026c859c0ad95a4
Roly Perera <roly.perera@dynamicaspects.org>
2018-02-17 13:00:52 +0000

    See if I can force a build...

--- src/Memo.ts
+++ src/Memo.ts
@@ -9,7 +9,7 @@ export function key (callee: Function, args: IArguments): Addr {
    return addr(callee) + "(" + Array.from(args).map(o => addr(__nonNull(o))).join(",") + ")"
 }
 
-// Rather than using a fresh address, we require top-level functions to have globally unique names.
+// Rather than using a fresh address, require top-level functions to have globally unique names.
 // TODO: store these in the instances map.
 export function __def <T> (f: (...xs: Object[]) => T): void {
    assert(f.name !== undefined, "Memo-functions must be named.")
