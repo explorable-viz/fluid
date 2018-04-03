5dc509bf6802246a187b35b7aebf475dce32b2c5
Roly Perera <roly.perera@dynamicaspects.org>
2018-04-03 19:51:56 +0100

    Actually reinstate 'failfast' assertion in create...quite essential.

--- src/Eval.ts
+++ src/Eval.ts
@@ -70,40 +70,40 @@ function evalSeq (ρ: Env, j: EnvId, κ: Object, es: Expr.Expr[]): EvalResults {
 
 export function eval_<T> (ρ: Env, j: EnvId, σ: Trie.Trie<T>, e: Expr.Expr): EvalResult<T> {
    const k: EvalId = EvalId.make(j, e.__id),
-         kʹ: EvalTraceId = EvalTraceId.make(k)
+         kʹ: EvalTraceId = EvalTraceId.make(k),
+         empty: Trace.Empty = Trace.Empty.at(kʹ)
    if (Trie.Var.is(σ)) {
-      const t: Trace.Trace = Trace.Empty.at(EvalTraceId.make(k)),
-            δ_id: Expr.RecDefsId = Expr.RecDefsId.make(e.__id),
+      const δ_id: Expr.RecDefsId = Expr.RecDefsId.make(e.__id),
             entry: EnvEntry = new EnvEntry(ρ, j, Expr.RecDefs.at(δ_id, []), e),
             l: EnvEntryId = EnvEntryId.make(j, δ_id, e.__id)
-      return __result(k, t, null, Env.singleton(σ.x.str, entry), EnvId.singleton(l), σ.body)
+      return __result(k, empty, null, Env.singleton(σ.x.str, entry), EnvId.singleton(l), σ.body)
    } else {
       if (e instanceof Expr.Constr && Trie.Constr.is(σ) && σ.cases.has(e.ctr.str)) {
          const σʹ: Object = σ.cases.get(e.ctr.str)!,
                [tvs, ρʹ, jʹ, κ]: EvalResults = evalSeq(ρ, j, σʹ, e.args)
          // have to cast κ without type information on constructor
-         return __result(k, Trace.Empty.at(kʹ), Value.Constr.at(k, e.ctr, tvs), ρʹ, jʹ, κ as T)
+         return __result(k, empty, Value.Constr.at(k, e.ctr, tvs), ρʹ, jʹ, κ as T)
       } else
       if (e instanceof Expr.ConstInt && Trie.ConstInt.is(σ)) {
-         return __result(k, Trace.Empty.at(kʹ), Value.ConstInt.at(k, e.val), Env.empty(), EnvId.empty(), σ.body)
+         return __result(k, empty, Value.ConstInt.at(k, e.val), Env.empty(), EnvId.empty(), σ.body)
       } else
       if (e instanceof Expr.ConstStr && Trie.ConstStr.is(σ)) {
-         return __result(k, Trace.Empty.at(kʹ), Value.ConstStr.at(k, e.val), Env.empty(), EnvId.empty(), σ.body)
+         return __result(k, empty, Value.ConstStr.at(k, e.val), Env.empty(), EnvId.empty(), σ.body)
       } else
       if (e instanceof Expr.Fun && Trie.Fun.is(σ)) {
          const δ_id: Expr.RecDefsId = Expr.RecDefsId.make(e.__id)
-         return __result(k, Trace.Empty.at(kʹ), Value.Closure.at(k, ρ, j, Expr.RecDefs.at(δ_id, []), e), Env.empty(), EnvId.empty(), σ.body)
+         return __result(k, empty, Value.Closure.at(k, ρ, j, Expr.RecDefs.at(δ_id, []), e), Env.empty(), EnvId.empty(), σ.body)
       } else
       if (e instanceof Expr.PrimOp && Trie.Fun.is(σ)) {
-         return __result(k, Trace.Empty.at(kʹ), e.op, Env.empty(), EnvId.empty(), σ.body)
+         return __result(k, empty, e.op, Env.empty(), EnvId.empty(), σ.body)
       } else
-      if (e instanceof Expr.OpName || e instanceof Expr.Var) {
+      if (e instanceof Expr.Var || e instanceof Expr.OpName) {
          const x: string = e instanceof Expr.OpName ? e.opName.str : e.ident.str
          if (!ρ.has(x)) {
             return assert(false, "Name not found.", x)
          } else {
-            const cls: EnvEntry = ρ.get(x)!,
-                  [tv, ρʺ, jʺ, σv]: EvalResult<T> = eval_(cls.ρ, cls.j, σ, cls.e),
+            const {ρ: ρʹ, j: jʹ, e: eʹ}: EnvEntry = ρ.get(x)!,
+                  [tv, ρʺ, jʺ, σv]: EvalResult<T> = eval_(ρʹ, jʹ, σ, eʹ),
                   t: Trace.Trace = e instanceof Expr.OpName 
                      ? Trace.OpName.at(kʹ, e.opName, tv.trace)
                      : Trace.Var.at(kʹ, e.ident, tv.trace)
