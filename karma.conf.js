4c2bc27427e87624d7b1be1d1be303df8f06bfec
Roly Perera <roly.perera@dynamicaspects.org>
2018-04-03 11:41:51 +0100

    Somehow, managed to switch to Headless Chrome, and then remove the es6-shim dependency from Karma, which in turn was bringing in es5-shim, which was (bizarrely) leading to a weird implementation of parseInt.

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ var webpackConfig = require('./webpack.config');
 module.exports = function (config) {
    config.set({
       basePath: '',
-      frameworks: ['mocha', 'chai', 'es6-shim'],
+      frameworks: ['mocha', 'chai'],
       files: [
          './test/**/*.ts',
          {
@@ -45,7 +45,7 @@ module.exports = function (config) {
       autoWatch: true,
       singleRun: false,
       concurrency: Infinity,
-      browsers: ['PhantomJS_debug'],
+      browsers: ['ChromeHeadless'], // ['PhantomJS_debug'],
       customLaunchers: {
          'PhantomJS_debug': {
             base: 'PhantomJS',
@@ -77,3 +77,4 @@ module.exports = function (config) {
       },
    })
 }
+ 
