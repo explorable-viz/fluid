7abefda9b91035737b73983478ac8b6eab9ee769
Roly Perera <roly.perera@dynamicaspects.org>
2018-04-05 12:30:56 +0100

    To avoid introducing more complexity, we now have interned nodes in the syntax. They have the same children at every state ('rigid designators'), and so can't be 'edited' directly. But I'll revisit that when I start thinking about the protocol for editing ASTs.

--- src/Parse.ts
+++ src/Parse.ts
@@ -1,8 +1,9 @@
 import { 
    Parser, ParseResult, ParseState, between, butnot, ch, chainl1, choice, constant, dropFirst,
-   dropSecond, lexeme, negate, optional, range, repeat, repeat1, satisfying, sepBy1, seq, sequence, 
-   symbol, withAction, withJoin
+   dropSecond, lazySeq, lexeme, negate, optional, range, repeat, repeat1, satisfying, sepBy1, seq, 
+   sequence, symbol, withAction, withJoin
 } from "./util/parse/Core"
+import { RecDefs, EmptyRecDefs, ExtendRecDefs } from "./Env"
 import { Lex, Traced, str, ν } from "./Syntax"
 import { Expr, Trie } from "./Syntax"
 
@@ -204,14 +205,25 @@ const recDef: Parser<Expr.RecDef> =
          Expr.RecDef.at(Expr.RecDefId.make(ν()), name, Expr.Fun.at(ν(), σ))
    )
 
+// These are *interned*; need to think about implications for editing the AST. 
+const recDefs: Parser<RecDefs> = optional(recDefs1(), EmptyRecDefs.make())
+
+function recDefs1 (): Parser<RecDefs> {
+   return (state: ParseState) =>
+      withAction(
+         lazySeq(recDef, () => recDefs),
+         ([def, δ]: [Expr.RecDef, RecDefs]) => ExtendRecDefs.make(δ, def)
+      )(state)
+}
+
 const letrec: Parser<Expr.LetRec> =
    withAction(
       seq(
-         dropFirst(keyword(str.letRec), repeat1(recDef)),
+         dropFirst(keyword(str.letRec), recDefs),
          dropFirst(keyword(str.in_), expr)
       ),
-      ([defs, body]: [Expr.RecDef[], Expr.Expr]) =>
-         Expr.LetRec.at(ν(), Expr.RecDefs.at(Expr.RecDefsId.make(ν()), defs), body)
+     ([δ, body]: [RecDefs, Expr.Expr]) => 
+         Expr.LetRec.at(ν(), δ, body)
    )
 
 const constr: Parser<Expr.Constr> =
