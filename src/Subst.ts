888e8c1979457d6bfb92c4d6615fd91700b2cd77
Roly Perera <roly.perera@dynamicaspects.org>
2018-04-13 10:36:00 +0200

    Skeleton of substitution. Still a bit to think about...

--- src/Subst.ts
+++ src/Subst.ts
@@ -25,6 +25,30 @@ export function subst (e: Expr.Expr, θ: Env): Traced {
    } else
    if (e instanceof Expr.Constr) {
       return Traced.at(i, Trace.Empty.at(i), Value.Constr.at(i, e.ctr, substSeq(e.args, θ)))
+   } else
+   if (e instanceof Expr.Fun) {
+      return Traced.at(i, Trace.Empty.at(i), Value.Closure.at(i, θ, e))
+   } else
+   if (e instanceof Expr.PrimOp) {
+      return Traced.at(i, Trace.Empty.at(i), e.op)
+   } else
+   if (e instanceof Expr.Var) {
+      return Traced.at(i, Trace.Var.at(i, e.ident, null), null)
+   } else
+   if (e instanceof Expr.OpName) {
+      return Traced.at(i, Trace.OpName.at(i, e.opName, null), null)
+   } else
+   if (e instanceof Expr.Let) {
+      return Traced.at(i, Trace.Let.at(i, subst(e.e, θ), null), null)
+   } else
+   if (e instanceof Expr.LetRec) {
+      return Traced.at(i, Trace.LetRec.at(i, e.δ, subst(e.e, θ).trace!), null)
+   } else
+   if (e instanceof Expr.MatchAs) {
+      return Traced.at(i, Trace.Match.at(i, subst(e.e, θ), null), null)
+   } else
+   if (e instanceof Expr.App) {
+      return Traced.at(i, Trace.App.at(i, subst(e.func, θ), subst(e.arg, θ), null), null)
    } else {
       return assert(false)
    }
