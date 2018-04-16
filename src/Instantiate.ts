ca99fe5242cd0b04e8269a367c94586e216bdb17
Roly Perera <roly.perera@dynamicaspects.org>
2018-04-16 11:22:53 +0100

    Substitution -> Instantiation.

--- src/Instantiate.ts
+++ src/Instantiate.ts
@@ -1,12 +1,12 @@
 import { assert} from "./util/Core"
-import { Env } from "./Env"
+import { Env, EnvEntry } from "./Env"
 import { Eval } from "./Eval"
 import { Cons, List, Nil } from "./List"
 import { Expr, Trace, Traced, Value } from "./Syntax"
 
-export function substSeq (es: List<Expr.Expr>, θ: Env): List<Traced> {
+export function instantiateSeq (es: List<Expr.Expr>, θ: Env): List<Traced> {
    if (Cons.is(es)) {
-      return Cons.make(subst(es.head, θ), substSeq(es.tail, θ))
+      return Cons.make(instantiate(es.head, θ), instantiateSeq(es.tail, θ))
    } else
    if (Nil.is(es)) {
       return Nil.make()
@@ -15,7 +15,7 @@ export function substSeq (es: List<Expr.Expr>, θ: Env): List<Traced> {
    }
 }
 
-export function subst (e: Expr.Expr, θ: Env): Traced {
+export function instantiate (e: Expr.Expr, θ: Env): Traced {
    const i: Eval.Evaluand = Eval.Evaluand.make(θ.entries(), e)
    if (e instanceof Expr.ConstInt) {
       return Traced.at(i, Trace.Empty.at(i), Value.ConstInt.at(i, e.val))
@@ -24,9 +24,10 @@ export function subst (e: Expr.Expr, θ: Env): Traced {
       return Traced.at(i, Trace.Empty.at(i), Value.ConstStr.at(i, e.val))
    } else
    if (e instanceof Expr.Constr) {
-      return Traced.at(i, Trace.Empty.at(i), Value.Constr.at(i, e.ctr, substSeq(e.args, θ)))
+      return Traced.at(i, Trace.Empty.at(i), Value.Constr.at(i, e.ctr, instantiateSeq(e.args, θ)))
    } else
    if (e instanceof Expr.Fun) {
+      // TODO: θ doesn't look right here...
       return Traced.at(i, Trace.Empty.at(i), Value.Closure.at(i, θ, e))
    } else
    if (e instanceof Expr.PrimOp) {
@@ -39,16 +40,16 @@ export function subst (e: Expr.Expr, θ: Env): Traced {
       return Traced.at(i, Trace.OpName.at(i, e.opName, null), null)
    } else
    if (e instanceof Expr.Let) {
-      return Traced.at(i, Trace.Let.at(i, subst(e.e, θ), null), null)
+      return Traced.at(i, Trace.Let.at(i, instantiate(e.e, θ), null), null)
    } else
    if (e instanceof Expr.LetRec) {
-      return Traced.at(i, Trace.LetRec.at(i, e.δ, subst(e.e, θ).trace!), null)
+      return Traced.at(i, Trace.LetRec.at(i, e.δ, instantiate(e.e, θ).trace!), null)
    } else
    if (e instanceof Expr.MatchAs) {
-      return Traced.at(i, Trace.Match.at(i, subst(e.e, θ), null), null)
+      return Traced.at(i, Trace.Match.at(i, instantiate(e.e, θ), null), null)
    } else
    if (e instanceof Expr.App) {
-      return Traced.at(i, Trace.App.at(i, subst(e.func, θ), subst(e.arg, θ), null), null)
+      return Traced.at(i, Trace.App.at(i, instantiate(e.func, θ), instantiate(e.arg, θ), null), null)
    } else {
       return assert(false)
    }
