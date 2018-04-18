af275e0ddd7f90323aff14104974a96d85e92dd9
Roly Perera <roly.perera@dynamicaspects.org>
2018-04-18 19:20:53 +0100

    Perhaps interned objects shouldn't have an identity (in which case they don't really need to be interned - we can just revert to dynamic objects, which would certainly make life simpler. We would then have to structurally recurse to enforce LVar semantics for such object. My initial worry about this was that this would nest, leading to a traditional recursive implementation of structural equality (losing all the benefits of hash-consing). But actually we would only recurse into immediate children and then be back to 'versioned' objects, no?

--- src/Syntax.ts
+++ src/Syntax.ts
@@ -328,8 +328,7 @@ export namespace Expr {
    }
 }
 
-// Can these be interned, rather than versioned? Just realised interning objects with null fields won't respect 
-// the LVar semantics :-o
+// Can these be interned, rather than versioned?
 export class Traced<T extends Value = Value> extends VersionedObject<Eval.Evaluand> {
    trace: Trace | null
    val: T | null
