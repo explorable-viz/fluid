9a1a9377cbb6501a58aa8c8425d1af6526aadd67
Roly Perera <roly.perera@dynamicaspects.org>
2018-05-22 19:47:08 +0100

    zipW only failing match case...

--- test/Test.ts
+++ test/Test.ts
@@ -16,70 +16,70 @@ describe("example", () => {
 	describe("arithmetic", () => {
 		const file: TestFile = loadExample("arithmetic")
 		it("ok", () => {
-			runTest(__nonNull(file.text), Profile.Run, τ.int(null))
+			runTest(__nonNull(file.text), Profile.Match, τ.int(null))
 		})
 	})
 
 	describe("compose", () => {
 		const file: TestFile = loadExample("compose")
 		it("ok", () => {
-			runTest(__nonNull(file.text), Profile.Run, τ.int(null))
+			runTest(__nonNull(file.text), Profile.Match, τ.int(null))
 		})
 	})
 
 	describe("factorial", () => {
 		const file: TestFile = loadExample("factorial")
 		it("ok", () => {
-			runTest(__nonNull(file.text), Profile.Run, τ.int(null))
+			runTest(__nonNull(file.text), Profile.Match, τ.int(null))
 		})
 	})
 
 	describe("filter", () => {
 		const file: TestFile = loadExample("filter")
 		it("ok", () => {
-			runTest(__nonNull(file.text), Profile.Run, τ.cons(τ.arg(τ.int(τ.arg(τ.var_(τ.endArgs(null)))))))
+			runTest(__nonNull(file.text), Profile.Match, τ.cons(τ.arg(τ.int(τ.arg(τ.var_(τ.endArgs(null)))))))
 		})
 	})
 
 	describe("foldr_sumSquares", () => {
 		const file: TestFile = loadExample("foldr_sumSquares")
 		it("ok", () => {
-			runTest(__nonNull(file.text), Profile.Run, τ.int(null))
+			runTest(__nonNull(file.text), Profile.Match, τ.int(null))
 		})
 	})
 
 	describe("length", () => {
 		const file: TestFile = loadExample("length")
 		it("ok", () => {
-			runTest(__nonNull(file.text), Profile.Run, τ.int(null))
+			runTest(__nonNull(file.text), Profile.Match, τ.int(null))
 		})
 	})
 
 	describe("lexicalScoping", () => {
 		const file: TestFile = loadExample("lexicalScoping")
 		it("ok", () => {
-			runTest(__nonNull(file.text), Profile.Run, τ.str(null))
+			runTest(__nonNull(file.text), Profile.Match, τ.str(null))
 		})
 	})
 
 	describe("lookup", () => {
 		const file: TestFile = loadExample("lookup")
 		it("parses ok", () => {
-			runTest(__nonNull(file.text), Profile.Run, τ.some(τ.arg(τ.str(τ.endArgs(null)))))
+			runTest(__nonNull(file.text), Profile.Match, τ.some(τ.arg(τ.str(τ.endArgs(null)))))
 		})
 	})
 
 	describe("map", () => {
 		const file: TestFile = loadExample("map")
 		it("ok", () => {
-			runTest(__nonNull(file.text), Profile.Run, τ.cons(τ.arg(τ.int(τ.arg(τ.var_(τ.endArgs(null)))))))
+			runTest(__nonNull(file.text), Profile.Match, τ.cons(τ.arg(τ.int(τ.arg(τ.var_(τ.endArgs(null)))))))
 		})
 	})
 
 	describe("mergeSort", () => {
 		const file: TestFile = loadExample("mergeSort")
 		it("ok", () => {
-			runTest(__nonNull(file.text), Profile.Run, τ.cons(τ.arg(τ.int(τ.arg(τ.var_(τ.endArgs(null)))))))
+			runTest(__nonNull(file.text), Profile.Match, τ.cons(τ.arg(τ.int(τ.arg(τ.var_(τ.endArgs(null)))))))
 		})
 	})
 
@@ -93,7 +93,7 @@ describe("example", () => {
 	describe("reverse", () => {
 		const file: TestFile = loadExample("reverse")
 		it("ok", () => {
-			runTest(__nonNull(file.text), Profile.Run, τ.cons(τ.arg(τ.int(τ.arg(τ.var_(τ.endArgs(null)))))))
+			runTest(__nonNull(file.text), Profile.Match, τ.cons(τ.arg(τ.int(τ.arg(τ.var_(τ.endArgs(null)))))))
 		})
 	})
 
