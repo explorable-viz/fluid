dee6b9b5b880425092b40894b900f5cded69fe2c
Roly Perera <rperera@turing.ac.uk>
2019-01-12 17:24:25 +0000

    Draw line instead of cube.

--- test/App.ts
+++ test/App.ts
@@ -7,17 +7,20 @@ const file: TestFile = loadTestFile("example", "factorial")
 runTest(__nonNull(file.text), Profile.Match, τ.var_(null))
 
 var scene = new THREE.Scene()
-var camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 )
+var camera = new THREE.PerspectiveCamera( 45, 1, 1, 500 )
+camera.position.set( 0, 0, 100 );
+camera.lookAt( new THREE.Vector3(0, 0, 0) );
 
 var renderer = new THREE.WebGLRenderer()
 renderer.setSize( 600, 600 )
 document.body.appendChild( renderer.domElement )
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 )
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
-var cube = new THREE.Mesh( geometry, material )
-scene.add( cube )
-
-camera.position.z = 5
+var geometry = new THREE.Geometry()
+geometry.vertices.push(new THREE.Vector3( -10, 0, 0) )
+geometry.vertices.push(new THREE.Vector3( 0, 10, 0) )
+geometry.vertices.push(new THREE.Vector3( 10, 0, 0) )
+var material = new THREE.LineBasicMaterial( { color: 0x0000ff } )
+var line = new THREE.Line( geometry, material )
+scene.add(line)
 
 renderer.render( scene, camera )
