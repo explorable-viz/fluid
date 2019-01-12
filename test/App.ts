9945d4eb1d4b2b51c28850672a1fc06c5a5d0fe5
Roly Perera <rperera@turing.ac.uk>
2019-01-12 17:57:14 +0000

    Next: assume output list of known length, and extract that many points from it.

--- test/App.ts
+++ test/App.ts
@@ -1,27 +1,33 @@
 import * as THREE from "three"
 import { __nonNull } from "../src/util/Core"
-import { Profile, TestFile, τ, initialise, loadTestFile, runTest } from "../test/Helpers"
+import { Eval } from "../src/Eval"
+import { instantiate } from "../src/Instantiate"
+import { Profile, TestFile, ρ, τ, initialise, loadTestFile, runTest } from "../test/Helpers"
 
 initialise()
 const file: TestFile = loadTestFile("example", "factorial")
 runTest(__nonNull(file.text), Profile.Match, τ.var_(null))
 
-var scene = new THREE.Scene()
+function getPoints (n: number): THREE.Vector3[] {
+   const [tv, , ]: Eval.Result<null> = Eval.eval_(ρ, instantiate(ρ)(e), σ)
+   return []
+}
+
+const scene = new THREE.Scene()
 scene.background = new THREE.Color( 0xffffff )
-var camera = new THREE.PerspectiveCamera( 45, 1, 1, 500 )
+const camera = new THREE.PerspectiveCamera( 45, 1, 1, 500 )
 camera.position.set( 0, 0, 100 )
 camera.lookAt( new THREE.Vector3(0, 0, 0) )
 
-var renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer()
 renderer.setSize( 600, 600 )
 document.body.appendChild( renderer.domElement )
 
-var geometry = new THREE.Geometry()
+const geometry = new THREE.Geometry()
 geometry.vertices.push(new THREE.Vector3( -10, 0, 0) )
 geometry.vertices.push(new THREE.Vector3( 0, 10, 0) )
 geometry.vertices.push(new THREE.Vector3( 10, 0, 0) )
-var material = new THREE.LineBasicMaterial( { color: 0x0000ff } )
-var line = new THREE.Line( geometry, material )
-scene.add(line)
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } )
+scene.add(new THREE.Line( geometry, material ))
 
 renderer.render( scene, camera )
