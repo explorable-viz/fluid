ef1981353520ca7daf50edecc213496e8f09cae1
Roly Perera <rperera@turing.ac.uk>
2019-02-05 14:15:26 +0000

    Actually 'wholesale' worlds don't really make sense: I need a 'persistent' representation with copy-on-write.

--- src/Runtime.ts
+++ src/Runtime.ts
@@ -98,6 +98,40 @@ export class VersionedObject<K extends PersistentObject = PersistentObject> exte
 type InstancesMap = Map<PersistentObject, VersionedObject<PersistentObject>>
 const __ctrInstances: Map<Ctr<VersionedObject>, InstancesMap> = new Map
 
+// Or "revision"?
+export class World {
+   __ctrInstances: Map<Ctr<VersionedObject>, InstancesMap> = new Map
+
+   create<K extends PersistentObject, T extends VersionedObject<K>> (α: K, ctr: Ctr<T>): T {
+      let instances: InstancesMap | undefined = __ctrInstances.get(ctr)
+      if (instances === undefined) {
+         instances = new Map
+         __ctrInstances.set(ctr, instances)
+      }
+
+      let o: VersionedObject<K> | undefined = instances.get(α) as VersionedObject<K>
+      if (o === undefined) {
+         o = Object.create(ctr.prototype) as T // new ctr doesn't work any more
+         // This may massively suck, performance-wise. Define these here rather than on VersionedObject
+         // to avoid constructors everywhere.
+         Object.defineProperty(o, "__id", {
+            value: α,
+            enumerable: false
+         })
+         Object.defineProperty(o, "__history", {
+            value: [],
+            enumerable: false
+         })
+         instances.set(α, o)
+      } else {
+         // initialisation should always version, which will enforce single-assignment, so this additional
+         // check strictly unnecessary. However failing now avoids weird ill-formed objects.
+         assert(o.constructor === ctr, "Address collision (different constructor).", α, className(o), funName(ctr))
+      }
+      return o as T
+      }
+}
+
 // The (possibly already extant) object uniquely identified by a memo-key. Needs to be initialised afterwards.
 // Unfortunately the Id type constraint is rather weak in TypeScript because of "bivariance".
 export function create<K extends PersistentObject, T extends VersionedObject<K>> (α: K, ctr: Ctr<T>): T {
