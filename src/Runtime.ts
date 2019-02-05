6d94e0d7fd10cb495f75fd74fd486c5ce8be95da
Roly Perera <rperera@turing.ac.uk>
2019-02-05 20:01:28 +0000

    mostRecent helper.

--- src/Runtime.ts
+++ src/Runtime.ts
@@ -1,4 +1,4 @@
-import { Class, assert, className, funName, make, __nonNull } from "./util/Core"
+import { Class, assert, className, funName, make, __nonNull, absurd } from "./util/Core"
 import { Ord } from "./util/Ord"
 import { PersistentObject } from "./util/Core"
 
@@ -34,56 +34,69 @@ export const ν: () => ExternalObject =
       }
    })()
 
-   function __blankCopy<T extends Object> (src: T): T {
-      const tgt: T = Object.create(src.constructor.prototype)
-      for (let x of Object.keys(src)) {
-         (tgt as any)[x] = null
+function __blankCopy<T extends Object> (src: T): T {
+   const tgt: T = Object.create(src.constructor.prototype)
+   for (let x of Object.keys(src)) {
+      (tgt as any)[x] = null
+   }
+   return tgt
+}
+
+// Argument tgtState is a "value object" whose identity doesn't matter but whose state represents what we currently 
+// know about src. Precondition: the two are upper-bounded; postcondition is that they are equal.
+function __mergeAssign (tgtState: Object, src: VersionedObject): boolean {
+   assert(__nonNull(tgtState).constructor === __nonNull(src.constructor))
+   const tgtState_: any = tgtState as any,
+         src_: any = src as any
+   let modified: boolean = false
+   Object.keys(tgtState).forEach((k: string): void => {
+      const v: any = __merge(tgtState_[k], src_[k])
+      if (tgtState_[k] !== v || src_[k] !== v) {
+         modified = true
+         tgtState_[k] = src_[k] = v
       }
+   })
+   return modified
+}
+
+// Least upper bound of two upper-bounded objects.
+function __merge (tgt: Object, src: Object): Object {
+   if (src === null) {
       return tgt
-   }
-   
-   // Argument tgtState is a "value object" whose identity doesn't matter but whose state represents what we currently 
-   // know about src. Precondition: the two are upper-bounded; postcondition is that they are equal.
-   function __mergeAssign (tgtState: Object, src: VersionedObject): boolean {
-      assert(__nonNull(tgtState).constructor === __nonNull(src.constructor))
-      const tgtState_: any = tgtState as any,
-            src_: any = src as any
-      let modified: boolean = false
-      Object.keys(tgtState).forEach((k: string): void => {
-         const v: any = __merge(tgtState_[k], src_[k])
-         if (tgtState_[k] !== v || src_[k] !== v) {
-            modified = true
-            tgtState_[k] = src_[k] = v
-         }
+   } else 
+   if (tgt === null) {
+      return src
+   } else
+   if (src === tgt) {
+      return src
+   } else {
+      assert(tgt.constructor === src.constructor)
+      assert(!(tgt instanceof VersionedObject), "Upper-bounded versioned objects have the same address")
+      assert(tgt instanceof InternedObject) // ignore other case for now
+      const args: any[] = Object.keys(tgt).map((k: string): any => {
+         return __merge((tgt as any)[k], (src as any)[k])
       })
-      return modified
+      // Two dubious assumptions, but hard to see another technique:
+      // (1) entries are supplied in declaration-order (not guaranteed by language spec)
+      // (2) constructor arguments also match declaration-order (easy constraint to violate)
+      return make(src.constructor as Class<InternedObject>, ...args)
    }
-   
-   // Least upper bound of two upper-bounded objects.
-   function __merge (tgt: Object, src: Object): Object {
-      if (src === null) {
-         return tgt
-      } else 
-      if (tgt === null) {
-         return src
-      } else
-      if (src === tgt) {
-         return src
+}   
+
+function mostRecent (history: Map<World, Object>, w: World): [World, Object] {
+   const v: Object | undefined = history.get(w)
+   if (v === undefined) {
+      if (w.parent !== null) {
+         return mostRecent(history, w.parent)
       } else {
-         assert(tgt.constructor === src.constructor)
-         assert(!(tgt instanceof VersionedObject), "Upper-bounded versioned objects have the same address")
-         assert(tgt instanceof InternedObject) // ignore other case for now
-         const args: any[] = Object.keys(tgt).map((k: string): any => {
-            return __merge((tgt as any)[k], (src as any)[k])
-         })
-         // Two dubious assumptions, but hard to see another technique:
-         // (1) entries are supplied in declaration-order (not guaranteed by language spec)
-         // (2) constructor arguments also match declaration-order (easy constraint to violate)
-         return make(src.constructor as Class<InternedObject>, ...args)
+         return absurd("No initial state.")
       }
-   }   
-   
-   export abstract class VersionedObject<K extends PersistentObject = PersistentObject> extends PersistentObject {
+   } else {
+      return [w, v]
+   }
+}
+
+export abstract class VersionedObject<K extends PersistentObject = PersistentObject> extends PersistentObject {
    // Initialise these at object creation (not enumerable).
    __history: Map<World, Object> = undefined as any // history records only enumerable fields
    __id: K = undefined as any
