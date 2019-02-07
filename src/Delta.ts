d9d838d75cd0beea32eafb51bc898650f8085194
Roly Perera <rperera@turing.ac.uk>
2019-02-07 18:26:27 +0000

    First delta experiment. Mapped types are bonkers.

--- src/Delta.ts
+++ src/Delta.ts
@@ -1,9 +1,16 @@
-import { Env, EmptyEnv } from "./Env"
-import { Value } from "./Traced"
+import { Env } from "./Env"
+import { World } from "./Runtime"
+import { Traced, Value } from "./Traced"
+import { PersistentObject } from "./util/Core"
+
+type Trie<T> = Traced.Trie<T>
+
+// https://stackoverflow.com/questions/48215950
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
 // EXPERIMENT
-export type Delta<T> = {
-   [P in keyof T]: DeltaRef<Delta<T[P]>> 
+export type Delta<T, K extends keyof T> = {
+   [P in keyof Omit<T, K>]: T[P] extends PersistentObject ? DeltaRef<T[P]> : T[P] 
 }
 
 enum RefDelta {
@@ -12,11 +19,20 @@ enum RefDelta {
 }
 
 class DeltaRef<T extends Object> {
-   delta: RefDelta
-   ref: T
+   constructor (
+      public delta: RefDelta,
+      public ref: T
+   ) {
+   }
 }
 
-export function blah (v: Delta<Value.Closure>): void {
-   const ρ: Delta<Env> = v.ρ.ref
-   ρ instanceof EmptyEnv // proxies so instanceof works?
+type OmittedProps = "__id" | "__commit" | "__mostRecent" | "__tag" | "__subtag" | "eq" | "constructor_"
+
+export function diff (tgt: Value.Closure, src: Value.Closure): Delta<Value.Closure, OmittedProps> {
+   const ρ: DeltaRef<Env> = new DeltaRef<Env>(RefDelta.Unchanged, tgt.ρ)
+   const σ: DeltaRef<Trie<Traced>> = new DeltaRef<Trie<Traced>>(RefDelta.Changed, src.σ)
+   return {
+      ρ: ρ,
+      σ: σ
+   }
 }
