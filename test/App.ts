21663926eec9629e17e030479f67f52b9bce8a9f
Roly Perera <rperera@turing.ac.uk>
2019-02-17 20:04:02 +0000

    Seems that continuation 'bottom' propagates destructively, destroying the entire computation downstream.

--- test/App.ts
+++ test/App.ts
@@ -104,8 +104,8 @@ function populateScene (): void {
       World.newRevision()
       const args: Cons<Expr> = as(here_.args, Cons)
       Expr.Constr.at(here.__id, Lex.Ctr.make("Cons"), Cons.make<Expr>(args.head.bottom(), args.tail)) // clunky
-      const vʹ: Value = __nonNull(runExample(e).v)
-      assert(vʹ !== v) // make consistent - should it be an invariant that every world is consistent?
+      const vʹ: Value = __nonNull(runExample(e).v) // make consistent - should it be an invariant that every world is consistent?
+      assert(vʹ !== v) // should be equal :-/
 //      World.undo()
 
       const elems: List<Persistent> = as(reflect(vʹ), List)
