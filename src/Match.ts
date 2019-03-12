c626653225d3a77003ad80c2adc6cafd6a0f83f7
Roly Perera <rperera@turing.ac.uk>
2019-03-12 16:02:42 +0000

    lookup -> match.

--- src/Match.ts
+++ src/Match.ts
@@ -8,10 +8,9 @@ import { Expr } from "./Expr"
 import Args = Expr.Args
 import Kont = Expr.Kont
 import Match = ExplVal.Match
-import ExplValMatch = ExplVal.ExplValMatch
 import Trie = Expr.Trie
 
-export function lookup<K extends Kont<K>> (tv: ExplVal, σ: Trie<K>): [Env, Match<K>, Annotation] {
+export function match<K extends Kont<K>> (tv: ExplVal, σ: Trie<K>): [Env, Match<K>, Annotation] {
    const v: Value = tv.v
    if (Trie.Var.is(σ)) {
       return [Env.singleton(σ.x.str, tv), Match.Var.make(σ.x, σ.κ), ann.top]
@@ -20,7 +19,7 @@ export function lookup<K extends Kont<K>> (tv: ExplVal, σ: Trie<K>): [Env, Matc
       let ρ_α: [Env, Annotation] // actually may be null, but TypeScript can't handle it
       const ξ: Match<K> = Match.Constr.make(σ.cases.map(({ fst: ctr, snd: Π }): Pair<string, Args<K> | Match.Args<K>> => {
          if (v.ctr.str === ctr) {
-            const [ρ, Ψ, α]: [Env, Match.Args<K>, Annotation] = lookupArgs(v.args, Π)
+            const [ρ, Ψ, α]: [Env, Match.Args<K>, Annotation] = matchArgs(v.args, Π)
             ρ_α = [ρ, α]
             return Pair.make(ctr, Ψ) // ann.meet(α, v.α)
          } else {
@@ -37,16 +36,16 @@ export function lookup<K extends Kont<K>> (tv: ExplVal, σ: Trie<K>): [Env, Matc
    }
 }
 
-export function unlookup<K extends Kont<K>> (ρ: Env, κ: K, α: Annotation): [ExplVal, Trie<K>] {
+export function unmatch<K extends Kont<K>> (ρ: Env, κ: K, α: Annotation): [ExplVal, Trie<K>] {
    throw new Error("Not yet implemented")
 }
 
-function lookupArgs<K extends Kont<K>> (tvs: List<ExplVal>, Π: Args<K>): [Env, Match.Args<K>, Annotation] {
+function matchArgs<K extends Kont<K>> (tvs: List<ExplVal>, Π: Args<K>): [Env, Match.Args<K>, Annotation] {
    // Parser ensures constructor patterns agree with constructor signatures.
    if (Cons.is(tvs) && Args.Next.is(Π)) {
       // codomain of ξ is Args; promote to Args | Match.Args:
-      const [ρ, ξ, α]: [Env, Match<Args<K>>, Annotation] = lookup(tvs.head, Π.σ), 
-            [ρʹ, κ, αʹ]: [Env, Match.Args<K>, Annotation] = lookupArgs(tvs.tail, ξ.κ)
+      const [ρ, ξ, α]: [Env, Match<Args<K>>, Annotation] = match(tvs.head, Π.σ), 
+            [ρʹ, κ, αʹ]: [Env, Match.Args<K>, Annotation] = matchArgs(tvs.tail, ξ.κ)
       return [Env.concat(ρ, ρʹ), κ, ann.meet(α, αʹ)]
    } else
    if (Nil.is(tvs) && Args.End.is(Π)) {
