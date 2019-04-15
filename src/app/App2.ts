a39ca578a7dcb7b4af55cda015eb2b0452e1aade
Roly Perera <rperera@turing.ac.uk>
2019-04-15 09:41:48 +0100

    App class structure...

--- src/app/App2.ts
+++ src/app/App2.ts
@@ -1,34 +1,42 @@
 import { __nonNull } from "../util/Core"
 import { GraphicsPane3D2 } from "./GraphicsPane3D2"
 
-const canvas: HTMLCanvasElement = document.createElement("canvas"),
-      ctx = __nonNull(canvas.getContext('2d')),
-      graphicsPane3D = new GraphicsPane3D2(window.innerWidth, window.innerHeight / 2)
+class App2 {
+   canvas: HTMLCanvasElement
+   ctx: CanvasRenderingContext2D
+   graphicsPane3D: GraphicsPane3D2
+   
+   constructor () {
+      this.canvas = document.createElement("canvas")
+      this.ctx = __nonNull(this.canvas.getContext('2d')),
+      this.graphicsPane3D = new GraphicsPane3D2(window.innerWidth, window.innerHeight / 2)
+   }
 
-function renderCanvas() {
-   ctx.font = '20pt Arial'
-   ctx.fillStyle = 'red'
-   ctx.fillRect(0, 0, canvas.width, canvas.height)
-   ctx.fillStyle = 'white'
-   ctx.fillRect(10, 10, canvas.width - 20, canvas.height - 20)
-   ctx.fillStyle = 'black'
-   ctx.textAlign = "center"
-   ctx.textBaseline = "middle"
-   ctx.fillText(new Date().getTime().toString(), canvas.width / 2, canvas.height / 2)
+   init() {
+      document.body.appendChild(this.graphicsPane3D.renderer.domElement)
+      this.graphicsPane3D.setCanvas(this.canvas)
+      this.canvas.width = this.canvas.height = 256
+      this.render()
+   }
+   
+   render() {
+      this.renderCanvas()
+      this.graphicsPane3D.texture.needsUpdate = true
+      this.graphicsPane3D.mesh.rotation.y += 1
+      this.graphicsPane3D.renderer.render(this.graphicsPane3D.scene, this.graphicsPane3D.camera)
+   }
+
+   renderCanvas() {
+      this.ctx.font = '20pt Arial'
+      this.ctx.fillStyle = 'red'
+      this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+      this.ctx.fillStyle = 'white'
+      this.ctx.fillRect(10, 10, this.canvas.width - 20, this.canvas.height - 20)
+      this.ctx.fillStyle = 'black'
+      this.ctx.textAlign = "center"
+      this.ctx.textBaseline = "middle"
+      this.ctx.fillText(new Date().getTime().toString(), this.canvas.width / 2, this.canvas.height / 2)
+   }
 }
 
-function init() {
-   document.body.appendChild(graphicsPane3D.renderer.domElement)
-   graphicsPane3D.setCanvas(canvas)
-   canvas.width = canvas.height = 256
-}
-
-function render() {
-   renderCanvas()
-   graphicsPane3D.texture.needsUpdate = true
-   graphicsPane3D.mesh.rotation.y += 1
-   graphicsPane3D.renderer.render(graphicsPane3D.scene, graphicsPane3D.camera)
-}
-
-init()
-render()
+new App2().init()
