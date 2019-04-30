9c0983c91a6131bd82336b3e1af76da0f047f5d7
Roly Perera <roly.perera@dynamicaspects.org>
2019-04-30 11:46:36 +0100

    Trying for type-safe constructors...

--- src/Experiment.ts
+++ src/Experiment.ts
@@ -3,9 +3,18 @@ import { __nonNull } from "./util/Core"
 class Expl {
 }
 
+type State<T> = {
+   readonly [prop in keyof T]: T[prop]
+}
+
 abstract class Explainable<T> {
    expl (prop: keyof T): Expl {
       return __nonNull(__nonNull(this.__expl).get(prop))
+   }
+
+   constructor_ (state: State<T>): this {
+      // copy state to fields
+      return this
    }
 
    __expl?: Map<keyof T, Expl>
@@ -33,4 +42,8 @@ export class Cons<T> extends List<T> {
    classify (σ: ListTrie<T>): void {
       σ.isCons(this)
    }
+}
+
+export function cons<T> (head: T, tail: List<T>): List<T> {
+   return new Cons().constructor_({ head: "", tail: null })
 }
