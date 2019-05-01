71b2c8ac4d39f7acedb8a0cdda4e94714f0f8223
Roly Perera <roly.perera@dynamicaspects.org>
2019-05-01 12:54:47 +0100

    TrieFun.

--- src/Experiment.ts
+++ src/Experiment.ts
@@ -69,11 +69,29 @@ namespace Expr {
    }
 
    namespace Trie {
-      export abstract class Trie<K extends Kont<K>> implements Kont<Trie<K>> {
+      export abstract class Trie<K extends Kont<K>> extends Explainable<Trie<K>> implements Kont<Trie<K>> {
       }
 
+      interface TrieFun<K, U> {
+         Constr (cases: FiniteMap<string, Args<K>>): U
+         Var (x: string, κ: K): U
+      }
+      
       export class Constr<K extends Kont<K>> extends Trie<K> {
          cases: FiniteMap<string, Args<K>>
+
+         match<U> (σ: TrieFun<K, U>): U {
+            return σ.Constr(this.cases)
+         }
+      }
+
+      export class Var<K extends Kont<K>> extends Trie<K> {
+         x: string
+         κ: K
+
+         match<U> (σ: TrieFun<K, U>): U {
+            return σ.Var(this.x, this.κ)
+         }
       }
    }
 }
