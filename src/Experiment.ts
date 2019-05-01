79767a6d10661b2a892602d7a5fddcf5732aea4c
Roly Perera <roly.perera@dynamicaspects.org>
2019-05-01 11:14:36 +0100

    onto FiniteMap.

--- src/Experiment.ts
+++ src/Experiment.ts
@@ -1,3 +1,4 @@
+import { Ord } from "./util/Ord"
 import { Class, __nonNull, absurd, funName } from "./util/Core"
 import { Env } from "./Env"
 
@@ -8,6 +9,35 @@ namespace Expr {
    export class Constr {
       ctr: string
       args: List<Expr>
+   }
+
+   export type Trie<K extends Kont<K>> = Trie.Trie<K>
+
+   export interface Kont<K> {
+   }
+
+   export type Args<K extends Kont<K>> = Args.Args<K>
+
+   export namespace Args {
+      export abstract class Args<K extends Kont<K>> implements Kont<Args<K>> {
+      }
+
+      export class End<K extends Kont<K>> extends Args<K> {
+         κ: K
+      }
+
+      export class Next<K extends Kont<K>> extends Args<K> {
+         σ: Trie<Args<K>>
+      }
+   }
+
+   namespace Trie {
+      export abstract class Trie<K extends Kont<K>> implements Kont<Trie<K>> {
+      }
+
+      export class Constr<K extends Kont<K>> extends Trie<K> {
+         cases: FiniteMap<string, Args<K>>
+      }
    }
 }
 
@@ -30,12 +60,14 @@ export interface Stateʹ {
    [prop: string]: Value
 }
 
-type ExplState<T> = { [prop in keyof CoreProps<T>]: Expl }
+type ExplState<T> = { 
+   [prop in keyof CoreProps<T>]: Expl 
+}
 
 // Gather the metadata properties associated with T.
 interface Metadata<T> {
    expl?: ExplState<T>
-   classify (σ: T): void
+   match (σ: T): void
 }  
 
 type CoreProps<T> = Pick<T, Exclude<keyof T, keyof Metadata<T>>>
@@ -46,7 +78,7 @@ abstract class Value {
 
 abstract class Explainable<T> extends Value implements Metadata<T> {
    expl?: ExplState<T>
-   abstract classify<U> (σ: Trie<U>): U
+   abstract match<U> (σ: Fun<U>): U
 }
 
 // Not easy to put this into Explainable and have it be specifically typed enough.
@@ -56,25 +88,24 @@ function construct<T> (tgt: T, state: State<T>): T {
 
 // Dynamic version of construct.
 function constructʹ (tgt: Value, state: Stateʹ): Value {
-   // copy state to fields of tgt
+   // TODO: copy state to fields of tgt
    return tgt
 }
 
+function make<T> (ctr: Class<T>, state: State<T>): T {
+   return construct(new ctr, state)
+}
+
+interface Fun<U> {
+}
+
 abstract class List<T> extends Explainable<List<T>> {
-   abstract classify<U> (σ: ListTrie<T, U>): U
-}
-
-interface Trie<U> {
-}
-
-interface ListTrie<T, U> extends Trie<U> {
-   isNil (xs: Nil<T>): U
-   isCons (xs: Cons<T>): U
+   abstract match<U> (σ: ListFun<T, U>): U
 }
 
 export class Nil<T> extends List<T> {
-   classify<U> (σ: ListTrie<T, U>): U {
-      return σ.isNil(this)
+   match<U> (σ: ListFun<T, U>): U {
+      return σ.Nil()
    }
 }
 
@@ -82,18 +113,37 @@ export class Cons<T> extends List<T> {
    head: T
    tail: List<T>
 
-   classify<U> (σ: ListTrie<T, U>): U {
-      return σ.isCons(this)
+   match<U> (σ: ListFun<T, U>): U {
+      return σ.Cons(this.head, this.tail)
    }
 }
 
-export function cons<T> (head: T, tail: List<T>): List<T> {
-   return make(Cons, { head: head, tail: tail })
+interface ListFun<T, U> extends Fun<U> {
+   Nil (): U
+   Cons (x: T, xs: List<T>): U
 }
 
-function make<T> (ctr: Class<T>, state: State<T>): T {
-   return construct(new ctr, state)
+export function cons<T> (head: T, tail: List<T>): List<T> {
+   return make(Cons, { head, tail })
 }
+
+export class Pair<T, U> extends Explainable<Pair<T, U>> {
+   fst: T
+   snd: U
+
+   match<V> (σ: PairFun<T, U, V>): V {
+      return σ.isPair(this)
+   }
+}
+
+interface PairFun<T, U, V> extends Fun<V> {
+   isPair (xs: Pair<T, U>): V}
+
+export function pair<T, U> (fst: T, snd: U): Pair<T, U> {
+   return make<Pair<T, U>>(Pair, { fst, snd })
+}
+
+export type FiniteMap<K extends Ord<K>, V> = Tree<Pair<K, V>>
 
 type ExplVal = [Expl, Value]
 
@@ -128,13 +178,13 @@ export function eval_ (ρ: Env, e: Expr): ExplVal {
             state: Stateʹ = {}
       let e̅: List<Expr> = e.args
       for (const f of d.fields) {
-         e̅.classify({
-            isNil(e̅ʹ: Nil<Expr>): void {
+         e̅.match({
+            Nil(): void {
                absurd()
             },
-            isCons(e̅ʹ: Cons<Expr>): void {
-               state[f] = eval_(ρ, e̅ʹ.head)
-               e̅ = e̅ʹ.tail
+            Cons(e: Expr, e̅ʹ: List<Expr>): void {
+               state[f] = eval_(ρ, e)
+               e̅ = e̅ʹ
             }
          })
       }
