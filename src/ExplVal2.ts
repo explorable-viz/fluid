77fba65eaa30a1e5c96f43045b4c088c86ae9d3f
Roly Perera <roly.perera@dynamicaspects.org>
2019-05-12 13:49:17 +0100

    Delete old closure implementation.

--- src/ExplVal2.ts
+++ src/ExplVal2.ts
@@ -1,18 +1,4 @@
-import { List } from "./BaseTypes2"
-import { Expr } from "./Expr2"
-import { Func, Env } from "./Func2"
 import { Value, _, make } from "./Value2"
-
-// TODO: break dependency on Expr, by compiling expressions to values?
-export class Closure extends Value {
-   ρ: Env = _ // ρ is _not_ closing for σ; need to extend with the bindings in δ
-   δ: List<Expr.RecDef> = _
-   f: Func = _
-}
-
-export function closure (ρ: Env, δ: List<Expr.RecDef>, f: Func): Closure {
-   return make(Closure, ρ, δ, f)
-}
 
 export namespace Expl {
    export abstract class Expl extends Value {
