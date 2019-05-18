e00e7b72314dce6736dd27222ddd54f21ca9f16d
Roly Perera <roly.perera@dynamicaspects.org>
2019-05-18 14:54:35 +0100

    Missing annotation-propagation rule for constructors.

--- src/Func2.ts
+++ src/Func2.ts
@@ -1,9 +1,10 @@
-import { Annotation } from "./util/Annotated2"
-import { as, className, error } from "./util/Core"
+import { Annotation, ann } from "./util/Annotated2"
+import { className, error } from "./util/Core"
 import { Constr } from "./DataType2"
 import { Kont } from "./Expr2"
 import { Env } from "./Env2"
 import { Value, _ } from "./Value2"
+import { getα } from "./Versioned2"
 
 // Func to distinguish from expression-level Fun. See GitHub issue #128.
 export abstract class Func<K> extends Value<"Func"> {
@@ -14,7 +15,8 @@ export abstract class Func<K> extends Value<"Func"> {
 export abstract class ConstrFunc<K extends Kont<K>> extends Func<K> {
    __apply (v: Value): [Env, K, Annotation] {
       if (v instanceof Constr) {
-         return as((this as any)[className(v)], ArgsFunc).__apply(v.fieldValues())
+         const [ρ, κ, α]: [Env, K, Annotation] = ((this as any)[className(v)] as ArgsFunc<K>).__apply(v.fieldValues())
+         return [ρ, κ, ann.meet(getα(v), α)]
       } else {
          return error(`Pattern mismatch: ${className(v)} is not a data type.`, v, this)
       }
