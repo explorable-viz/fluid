3658f1574693660ad875561cb50bff4299f1f6f8
Roly Perera <roly.perera@dynamicaspects.org>
2019-06-04 13:47:04 +0100

    Working out how to do axes with rounding, precision, log...

--- src/Primitive.ts
+++ src/Primitive.ts
@@ -35,6 +35,7 @@ const ceiling = (x: Num) => (k: Id): Versioned<Num> => numʹ(k, Math.ceil(x.val)
 // Used to take arbitrary value as additional argument, but now primitives have primitive arguments.
 const error = (message: Str) => (k: Id): Versioned<Value> => assert(false, "LambdaCalc error:\n" + message.val)
 const floor = (x: Num) => (k: Id): Versioned<Num> => numʹ(k, Math.floor(x.val))
+const log = (x: Num) => (k: Id): Versioned<Num> => numʹ(k, Math.log(as(x, Num).val))
 const intToString = (x: Num) => (k: Id): Versioned<Str> => strʹ(k, x.val.toString())
 // No longer support overloaded functions, since the pattern-matching semantics is non-trivial; might require typecase.
 const equalInt = (x: Num, y: Num): (k: Id) => Versioned<Bool> => as(x, Num).val === as(y, Num).val ? trueʹ : falseʹ
@@ -64,6 +65,7 @@ export const unaryOps: Map<string, UnaryOp> = new Map([
    [ceiling.name, unary_(ceiling)],
    [error.name, unary_(error)],
    [floor.name, unary_(floor)],
+   [log.name, unary_(log)],
    [intToString.name, unary_(intToString)],
 ])
    
