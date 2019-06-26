56e4509ef3e497d549b8edc69bece8d8ca869315
Roly Perera <roly.perera@dynamicaspects.org>
2019-06-26 13:49:43 +0200

    Whitespace.

--- src/Parse2.ts
+++ src/Parse2.ts
@@ -34,6 +34,13 @@ export var ParserRules: NearleyRule[] = [
     {"name": "productExpr", "symbols": ["exponentExpr"]},
     {"name": "exponentExpr", "symbols": ["exponentExpr", "exponentOp", "appChain"]},
     {"name": "exponentExpr", "symbols": ["appChain"]},
+    {"name": "appChain", "symbols": ["simpleExpr"]},
+    {"name": "simpleExpr", "symbols": ["variable"]},
+    {"name": "simpleExpr", "symbols": ["parenthExpr"]},
+    {"name": "variable$ebnf$1", "symbols": []},
+    {"name": "variable$ebnf$1", "symbols": ["variable$ebnf$1", /[0-9a-zA-Z_]/], "postprocess": (d) => d[0].concat([d[1]])},
+    {"name": "variable", "symbols": [/[a-zA-Z_]/, "variable$ebnf$1"]},
+    {"name": "parenthExpr", "symbols": [{"literal":"("}, "_", "expr", "_", {"literal":")"}]},
     {"name": "compareOp$string$1", "symbols": [{"literal":"="}, {"literal":"="}], "postprocess": (d) => d.join('')},
     {"name": "compareOp", "symbols": ["compareOp$string$1"]},
     {"name": "compareOp$string$2", "symbols": [{"literal":"="}, {"literal":"="}, {"literal":"="}], "postprocess": (d) => d.join('')},
@@ -55,7 +62,10 @@ export var ParserRules: NearleyRule[] = [
     {"name": "sumOp", "symbols": [{"literal":"+"}]},
     {"name": "sumOp", "symbols": [{"literal":"-"}]},
     {"name": "sumOp$string$1", "symbols": [{"literal":"+"}, {"literal":"+"}], "postprocess": (d) => d.join('')},
-    {"name": "sumOp", "symbols": ["sumOp$string$1"]}
+    {"name": "sumOp", "symbols": ["sumOp$string$1"]},
+    {"name": "_$ebnf$1", "symbols": []},
+    {"name": "_$ebnf$1", "symbols": ["_$ebnf$1", /[\s]/], "postprocess": (d) => d[0].concat([d[1]])},
+    {"name": "_", "symbols": ["_$ebnf$1"], "postprocess": d => null}
 ];
 
 export var ParserStart: string = "expr";
--- src/grammar.ne
+++ src/grammar.ne
@@ -6,7 +6,20 @@ sumExpr -> sumExpr sumOp productExpr | productExpr
 productExpr -> productExpr productOp exponentExpr | exponentExpr
 exponentExpr -> exponentExpr exponentOp appChain | appChain
 
+appChain -> simpleExpr
+
+simpleExpr -> 
+   variable |
+   parenthExpr
+
+variable -> [a-zA-Z_] [0-9a-zA-Z_]:*
+
+parenthExpr ->
+   "(" _ expr _ ")"
+
 compareOp -> "==" | "===" | "<=" | "<==" | "<" | ">=" | ">==" | ">"
 exponentOp -> "**"
 productOp -> "*" | "/"
 sumOp -> "+" | "-" | "++"
+
+_ -> [\s]:* {% d => null %}
