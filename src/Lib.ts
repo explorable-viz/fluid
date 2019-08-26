c871050b7abf42be3ec724653037fd0d0ca5b947
Roly Perera <roly.perera@dynamicaspects.org>
2019-08-26 14:55:20 +0100

    Trying to get a 'main' Lib.d.ts published...

---
+++ src/Lib.ts
@@ -0,0 +1,5 @@
+// "typings" property of package.json seems to require a single TypeScript interface.
+import { Eval } from "./Eval"
+import { parseWithImports } from "./Module"
+
+export { Eval, parseWithImports }
