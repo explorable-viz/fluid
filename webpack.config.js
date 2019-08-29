f35176ae010d825ce166e84b72c579d5cf0b334a
Roly Perera <roly.perera@dynamicaspects.org>
2019-08-29 09:34:01 +0100

    Sensible options for file-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,9 @@ module.exports = {
          {
             test: /\.lcalc$/,
             loader: "file-loader",
+            options: {
+               name: '[path][name].[ext]',
+             }
          }
       ]
    },
