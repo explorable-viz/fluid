9c8bbd78a107dba29055d72a04cf823fa1ef6b97
Roly Perera <roly.perera@dynamicaspects.org>
2019-09-03 09:51:09 +0100

    This stuff is a serious mess: when I import my webpack.config settings into karma.conf.js, I now need to extract the first element of an array :-o

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,4 @@
-var webpackConfig = require('./webpack.config');
+var webpackConfig = require('./webpack.config')[0] // ouch - pick out first configuration!
 
 module.exports = function (config) {
    config.set({
