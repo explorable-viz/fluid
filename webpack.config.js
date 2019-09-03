dcfd2f5f64ee607c96b5b70ab181117b3a0f7aa1
Roly Perera <roly.perera@dynamicaspects.org>
2019-09-03 08:20:27 +0100

    Separate config for app. Reinstate HMTLWebpack plugin. Drop unneeded library settings.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,46 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
-module.exports = {
+// Specify two configs; first one (app) picked up automatically by webpack-dev-server. Second is lib.
+module.exports = [{
+   entry: ["./src/app/App.ts"],
+   output: {
+      filename: "bundle.js"
+   },
+   resolve: {
+      // Add '.ts' as a resolvable extension.
+      extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+   },
+   module: {
+      rules: [
+         // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+         {
+             test: /\.ts$/,
+             loader: "ts-loader"
+         },
+         // css files; import css files in .ts using 'import' or 'require'
+         {
+             test: /\.css$/,
+             loaders: ["style-loader", "css-loader"]
+         }
+      ]
+   },
+   devServer: {
+      port: 8000
+   },
+   plugins: [
+      new HtmlWebpackPlugin(),
+   ],
+   devtool: "inline-source-map"
+},{
    mode: "development",
    entry: {
-      main: "./src/app/App.ts", 
       lib: "./src/Lib.ts"
    },
-   // This took a lot of painful trial-and-error to figure out. Some notes:
+   // This took a *lot* of painful trial-and-error to figure out. Some notes:
    // - "commonjs2" causes module not to resolve on the client (silently, of course)
-   // - specifying "libraryExport" (silently) overrides the exports of the module; avoid
-   // - specifying "library" adds an extra level of indirection (fluid_1.Fluid) which breaks client
+   // - "libraryExport" (silently) overrides the exports of the module; avoid
+   // - non-empty "library" adds an extra level of indirection (fluid_1.Fluid) which breaks client
    output: {
       filename: "[name].bundle.js",
       library: "", // avoids extra indirection; can I drop entirely?
@@ -35,9 +65,6 @@ module.exports = {
          }
       ]
    },
-   devServer: {
-      port: 8000
-   },
    plugins: [
       // cobbled together from https://github.com/webpack-contrib/karma-webpack/issues/66
       function()
@@ -54,4 +81,4 @@ module.exports = {
       }
   ],
   devtool: "inline-source-map"
-}
+}]
