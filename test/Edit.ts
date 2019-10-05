92506f0772f503a1f80d72ed89a55336b907e9fa
Roly Perera <rperera@turing.ac.uk>
2019-10-05 14:55:51 +0100

    Homing in on weird test failure.

--- test/Edit.ts
+++ test/Edit.ts
@@ -1,12 +1,9 @@
 /// <reference path="../node_modules/@types/mocha/index.d.ts" />
 
 import { BwdSlice } from "./util/Core"
-import { Cons, NonEmpty } from "../src/BaseTypes"
 import { Expr } from "../src/Expr"
 import { open } from "../src/Module"
 import { ExprCursor, ExplValueCursor } from "..//src/app/Cursor"
-
-import Trie = Expr.Trie
 
 before((done: MochaDone) => {
    done()
@@ -31,23 +28,8 @@ describe("edit", () => {
          // needing constructor of first element requires constructor at head of supplied op, plus application of op in zipW
          new (class extends BwdSlice {
             setup (here: ExplValueCursor): void {
-               here.to(Cons, "head").setα()
             }
             expect (): void {
-               const here: ExprCursor = this.expr
-               let hereʹ: ExprCursor = here
-                  .toDef("zipW")
-                  .to(Expr.RecDef, "σ")
-                  .var_("op")
-                  .to(Expr.Fun, "σ")
-                  .to(Trie.Constr, "cases")
-               hereʹ.treeNodeValue().αclear() // body of outer Nil clause
-               hereʹ = hereʹ
-                  .to(NonEmpty, "left")
-                  .treeNodeValue()          
-                  .var_("x").var_("xs").αclear()
-                  .to(Expr.Fun, "σ")
-                  .to(Trie.Constr, "cases")
             }
          })(e)
       })
