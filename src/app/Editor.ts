f1e2c1a8ebc3333b3ccf011d2cebc005ce1a79a1
Roly Perera <rperera@turing.ac.uk>
2019-10-09 07:46:56 +0100

    Create a graphic.

--- src/app/Editor.ts
+++ src/app/Editor.ts
@@ -1,10 +1,14 @@
-import { __nonNull } from "../util/Core"
-import { __deltas } from "../Delta"
-import { createSvg } from "./Core"
+import { createSvg, svgNS } from "./Core"
 
 class Editor {
    constructor () {
-      createSvg(400, 400, false)
+      const root: SVGSVGElement = createSvg(400, 400, false),
+            polygon: SVGPolygonElement = document.createElementNS(svgNS, "polygon")
+      polygon.setAttribute("points", "0, 0 100, 0 0, 100, 100, 100")
+      polygon.setAttribute("stroke", "black")
+      polygon.setAttribute("fill", "gray")
+      root.appendChild(polygon)
+      document.body.appendChild(root)
    }
 }
 
