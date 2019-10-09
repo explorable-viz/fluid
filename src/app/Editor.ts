cdbd10aea88a534bf6ef567ddf19fd728dfc8571
Roly Perera <rperera@turing.ac.uk>
2019-10-09 08:40:54 +0100

    Think I'll switch to yarn, seems to cope with unmet peer dependency problems better...

--- src/app/Editor.ts
+++ src/app/Editor.ts
@@ -1,17 +1,34 @@
+import { Expr } from "../Expr"
 import { openWithImports } from "../Module"
 import { createSvg, svgNS } from "./Core"
+import "./styles.css"
+
+function textElement (x: number, y: number, str: string): SVGTextElement {
+   const text: SVGTextElement = document.createElementNS(svgNS, "text")
+   text.setAttribute("stroke", "none")
+   text.setAttribute("font-size", "12")
+   text.setAttribute("font-family", "courier")
+   text.setAttribute("transform", `translate(${x.toString()},${y.toString()}) scale(1,-1)`)
+   text.setAttribute("fill", "black")
+   text.appendChild(document.createTextNode(str))
+   return text
+}
+
+function render (e: Expr): SVGElement {
+   return textElement(50, 50, "An expression")
+}
 
 class Editor {
    constructor () {
-      openWithImports("foldr_sumSquares")
-
       const root: SVGSVGElement = createSvg(400, 400, false),
             polygon: SVGPolygonElement = document.createElementNS(svgNS, "polygon")
-      polygon.setAttribute("points", "0, 0 100, 0 0, 100, 100, 100")
+      polygon.setAttribute("points", "0, 0 0, 100 100, 0, 100, 100")
       polygon.setAttribute("stroke", "black")
       polygon.setAttribute("fill", "gray")
       root.appendChild(polygon)
       document.body.appendChild(root)
+      const e: Expr = openWithImports("foldr_sumSquares")
+      root.appendChild(render(e))
    }
 }
 
