bcdd314c4d9b7e7c3619a4d15622a23e620eabe6
Roly Perera <rperera@turing.ac.uk>
2019-10-09 11:48:43 +0100

    Now start on expression cases...

--- src/app/Editor.ts
+++ src/app/Editor.ts
@@ -1,9 +1,14 @@
+import "../../src/BaseTypes" // otherwise mysterious cyclic initialisation error
 import { Expr } from "../Expr"
 import { openWithImports } from "../Module"
 import { createSvg, svgNS, textElement } from "./Core"
 import "./styles.css"
 
 function render (e: Expr): SVGElement {
+   if (e instanceof Expr.App) {
+
+   }
+
    const text: SVGTextElement = textElement(50, 50, 18, "An expression")
    text.setAttribute("class", "code")
    return text
