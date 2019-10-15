d76116d7217b6a1d477b6cdd6f67aa6e1e7e315a
Roly Perera <rperera@turing.ac.uk>
2019-10-15 16:10:58 +0100

    Switch to pattern-match example to deal with list highlighting. Not gonna be pretty.

--- src/app/IDE.ts
+++ src/app/IDE.ts
@@ -1,7 +1,7 @@
 import "../BaseTypes"
 import "../Graphics"
-import { module_graphics, openDatasetAs, openWithImports } from "../Module"
+import { openWithImports } from "../Module"
 import "../app/GraphicsRenderer"
 import { Editor } from "./Editor"
 
-new Editor(openWithImports("bar-chart", module_graphics), openDatasetAs("renewables", "data"))
+new Editor(openWithImports("pattern-match"))
