a4003d1a3ef3a94d6e365b9bd937b93d9946860c
Roly Perera <rperera@turing.ac.uk>
2019-10-18 15:02:59 +0100

    Next: notion of 'current node'.

--- src/app/IDE.ts
+++ src/app/IDE.ts
@@ -1,7 +1,7 @@
 import "../BaseTypes"
 import "../Graphics"
-import { module_graphics, openWithImports, openDatasetAs } from "../Module"
+import { openWithImports } from "../Module"
 import "../app/GraphicsRenderer"
 import { Editor } from "./Editor"
 
-new Editor(openWithImports("bar-chart", module_graphics), openDatasetAs("renewables", "data"))
+new Editor(openWithImports("ic2019"))
