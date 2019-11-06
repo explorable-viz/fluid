be829ba704f04f3b6a9a43bae6dbea690c85ade1
Roly Perera <rperera@turing.ac.uk>
2019-11-06 16:45:46 +0000

    Progress externalising plotPoint.

--- src/app/GraphicsRenderer.ts
+++ src/app/GraphicsRenderer.ts
@@ -144,6 +144,7 @@ export class GraphicsRenderer {
       const path: SVGPolylineElement = document.createElementNS(SVG.NS, "polyline")
       const g: Polyline = as(tg.tv.v, Polyline)
       if (Some.is(g.marker)) {
+         g.points.toArray().map(p => this.plotPoint(2.5, p))
       }
       path.setAttribute("points", this.points(g.points))
       path.setAttribute("stroke", "black")
@@ -153,6 +154,23 @@ export class GraphicsRenderer {
       })
       this.current.appendChild(path)
       this.pointHighlights(tg.to(Polyline, "points"))
+   }
+
+   plotPoint (radius: number, { x, y }: Point): void {
+      const line1: SVGLineElement = document.createElementNS(SVG.NS, "line")
+      line1.setAttribute("x1", `${x.val - radius}`)
+      line1.setAttribute("y1", `${y.val - radius}`)
+      line1.setAttribute("x2", `${x.val + radius}`)
+      line1.setAttribute("y2", `${y.val + radius}`)
+      line1.setAttribute("stroke", "black")
+      const line2: SVGLineElement = document.createElementNS(SVG.NS, "line")
+      line2.setAttribute("x1", `${x.val - radius}`)
+      line2.setAttribute("y1", `${y.val + radius}`)
+      line2.setAttribute("x2", `${x.val + radius}`)
+      line2.setAttribute("y2", `${y.val - radius}`)
+      line2.setAttribute("stroke", "black")
+      this.current.appendChild(line1)
+      this.current.appendChild(line2)
    }
 
    pointHighlights (tp̅: ExplValueCursor/*<List<Point>>*/): void {
