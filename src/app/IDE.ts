85d1269cacea3ff8a98e358d5db2be10d53936ea
Roly Perera <rperera@turing.ac.uk>
2019-11-11 10:53:36 +0000

    Fix stackRight (should be sep / 2).

--- src/app/IDE.ts
+++ src/app/IDE.ts
@@ -4,4 +4,4 @@ import { openDatasetAs, openWithImports } from "../Module"
 import "../app/GraphicsRenderer2"
 import { Editor } from "./Editor"
 
-new Editor(openWithImports("graphics/stacked-rectangles"), openDatasetAs("renewables", "data"))
+new Editor(openWithImports("graphics/nested-rectangles"), openDatasetAs("renewables", "data"))
