fe102788cdb2e91c5cbd972133224b929cbf4e15
Roly Perera <rperera@turing.ac.uk>
2019-11-11 16:16:55 +0000

    Fix scaleToWidth to multiple with current scale, rather than replace.

--- src/Graphics2.ts
+++ src/Graphics2.ts
@@ -1,20 +1,9 @@
-import { List, Option } from "./BaseTypes"
+import { List, Option, Pair } from "./BaseTypes"
 import { initDataType } from "./DataType"
 import { DataValue } from "./DataValue"
 import { Num, Str, _ } from "./Value"
 
-// Basic graphical datatypes.
-
-export class Point extends DataValue<"Point"> {
-   x: Num = _
-   y: Num = _
-
-   toString(): string {
-      return `Point(${this.x},${this.y})`
-   }
-}
-
-export type GraphicsElementTag = "Rect" | "Group"
+export type GraphicsElementTag = "Polyline" | "Rect" | "Group"
 
 // Every graphics element has coordinates and dimensions expressed in the coordinate system of its parent.
 export class GraphicsElement<Tag extends GraphicsElementTag = GraphicsElementTag> extends DataValue<Tag> {
@@ -33,6 +22,10 @@ export class Rect extends GraphicsElement<"Rect"> {
    fill: Str = _
 }
 
+export class Polyline extends GraphicsElement<"Polyline"> {
+   points: List<Pair<Num, Num>> = _
+}
+
 export class Scale extends DataValue<"Scale"> {
    x: Num = _
    y: Num = _
@@ -40,4 +33,3 @@ export class Scale extends DataValue<"Scale"> {
 
 initDataType(GraphicsElement, [Group, Rect])
 initDataType(Scale, [Scale])
-initDataType(Point, [Point])
