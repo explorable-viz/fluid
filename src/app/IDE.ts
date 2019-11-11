cba90bb1f069866341ee5c7932815d620b13c90e
Roly Perera <rperera@turing.ac.uk>
2019-11-11 11:47:18 +0000

    If something is less than 0.5 pixels wide/high then it will be invisible.

--- src/app/IDE.ts
+++ src/app/IDE.ts
@@ -4,4 +4,4 @@ import { openDatasetAs, openWithImports } from "../Module"
 import "../app/GraphicsRenderer2"
 import { Editor } from "./Editor"
 
-new Editor(openWithImports("graphics/stacked-bar-chart"), openDatasetAs("renewables", "data"))
+new Editor(openWithImports("graphics/grouped-bar-chart"), openDatasetAs("renewables", "data"))
