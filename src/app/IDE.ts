7d9d9cb9a0214883794791531864cb3e9c9bc9d9
Roly Perera <rperera@turing.ac.uk>
2019-11-19 17:22:28 +0000

    Add some captions.

--- src/app/IDE.ts
+++ src/app/IDE.ts
@@ -4,4 +4,4 @@ import { openDatasetAs, openWithImports } from "../Module"
 import "../app/GraphicsRenderer2"
 import { Editor } from "./Editor"
 
-new Editor(openWithImports("graphics/line-chart"), openDatasetAs("renewables", "data"))
+new Editor(openWithImports("graphics/stacked-bar-chart"), openDatasetAs("renewables", "data"))
