5800f985031bc5f1f86bf28f54a56ff94f461e32
Roly Perera <rperera@turing.ac.uk>
2019-11-22 13:33:16 +0000

    Fix theme, font, font size and multiline format.

--- src/app/Interactor.ts
+++ src/app/Interactor.ts
@@ -1,10 +1,9 @@
-import { __log, __nonNull } from "../util/Core"
-import { Rect } from "../Graphics2"
-
 import tippy from "tippy.js"
 import { Instance } from "tippy.js"
 import "tippy.js/dist/tippy.css"
 import "tippy.js/themes/light-border.css"
+import { __log, __nonNull } from "../util/Core"
+import { Rect } from "../Graphics2"
 
 export class Interactor {
    tooltip: Instance | null = null // just have one for now
@@ -21,7 +20,7 @@ export class Interactor {
       const y_prop: number = Math.min(rect.bottom - e.clientY, rect.height) / rect.height
       const x_or_width: string = x_prop >= 0.5 ? `width: ${g.width}` : `x: ${g.x}`
       const y_or_height: string = y_prop >= 0.5 ? `height: ${g.height}` : `y: ${g.y}`
-      const content: string = `${x_or_width}\n${y_or_height}`
+      const content: string = `${x_or_width}<br/>${y_or_height}`
       const tooltip: Instance = __nonNull(this.tooltips.get(r))
       tooltip.setContent(content)
    }
