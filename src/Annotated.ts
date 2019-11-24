6a2c4e6a056955d7cd0f2b62bf275329154cd512
Roly Perera <rperera@turing.ac.uk>
2019-11-24 07:45:14 +0000

    Add parallel test assertions for new implementation.

--- src/Annotated.ts
+++ src/Annotated.ts
@@ -23,6 +23,10 @@ export function annotated<T extends Object> (v: T): v is T & Annotated {
    return v.hasOwnProperty("__α")
 }
 
+export function getα<T extends Annotated & Value> (v: T): Annotation {
+   return __annotations.get(v)
+}
+
 export function setα<T extends Annotated & Value> (α: Annotation, v: T): T {
    if (v.__α !== α) {
       __annotations.set(v, α)
@@ -80,6 +84,19 @@ export class Annotations {
    ann: Map<Value, Annotation> = new Map()
    direction: Direction = Direction.Fwd
 
+   // This is rather weak, in that we no longer require annotations to be set to top/bot upfront.
+   get (v: Value): Annotation {
+      const α: Annotation | undefined = this.ann.get(v)
+      if (α !== undefined) {
+         return α
+      } else 
+      if (this.direction === Direction.Fwd) {
+         return bool_.top
+      } else {
+         return bool_.bot
+      }
+   }
+   
    // Going forward, assume everything is available; annotation updates must be decreasing. 
    // Going backward, assume everything is not needed; annotation updates must be increasing.
    set (v: Value, α: Annotation): void {
@@ -94,7 +111,9 @@ export class Annotations {
       if (this.direction === Direction.Fwd && α > current ||
          this.direction === Direction.Bwd && α < current) {
          absurd(`Incompatible update of annotation from ${current} to ${α}.`, current, α)
-      } 
+      } else {
+         // idempotent
+      }
    }
 
    reset (direction: Direction): void {
--- src/app/Cursor.ts
+++ src/app/Cursor.ts
@@ -1,7 +1,7 @@
 import { last, nth } from "../../src/util/Array"
 import { AClass, Class, __check, __nonNull, absurd, as, assert, className, userError } from "../../src/util/Core"
 import { bool_ } from "../../src/util/Lattice"
-import { Annotated, annotated, setα } from "../../src/Annotated"
+import { Annotated, annotated, getα, setα } from "../../src/Annotated"
 import { Cons, List, NonEmpty, Pair } from "../../src/BaseTypes"
 import { exprClass } from "../../src/DataType"
 import { DataValue, ExplValue, explValue } from "../../src/DataValue"
@@ -31,11 +31,13 @@ export abstract class Cursor {
 
    αset (): this {
       assert(this.annotated.__α === bool_.top)
+      assert(getα(this.annotated) === bool_.top)
       return this
    }
 
    αclear (): this {
       assert(this.annotated.__α === bool_.bot)
+      assert(getα(this.annotated) === bool_.bot)
       return this
    }
 
