3723ee46aaa41df78f0707ddcdb3e21765190885
Roly Perera <rperera@turing.ac.uk>
2019-11-25 20:02:25 +0000

    Woohoo, much of the interactor code is generic.

--- src/app/Interactor.ts
+++ src/app/Interactor.ts
@@ -2,7 +2,7 @@ import tippy from "tippy.js"
 import { Instance as Tooltip, Placement } from "tippy.js"
 import "tippy.js/dist/tippy.css"
 import "tippy.js/themes/light-border.css"
-import { __log, __nonNull, as, assert } from "../util/Core"
+import { Class, __log, __nonNull, as, assert } from "../util/Core"
 import { bool_ } from "../util/Lattice"
 import { Direction, isα, setα } from "../Annotation"
 import { Pair } from "../BaseTypes"
@@ -31,83 +31,69 @@ function propValues<T extends Value> (g: T, props: (keyof T)[]): string {
    return lines.join("</br>")
 }
 
-function focusedProps<T extends DataValue> (direction: Direction, tv: ExplValue<T>): (keyof T)[] {
-   return fields(tv.v).filter((prop: keyof T) => {
-      const tv_: ExplValue = Expl.explChild(tv.t, tv.v, prop)
-      return __nonNull(direction) === Direction.Fwd ? bool_.negate(isα(tv_)) : isα(tv_)
-   })
-}
-
-export class PointInteractor {
+abstract class Interactor<T, U extends SVGElement> {
    editor: Editor.Editor
    tooltip: Tooltip
-   tp: ExplValueCursor/*<Pair<Num, Num>>*/
-   marker: SVGElement
-   propFocus: keyof Pair | null = null
+   cursor: ExplValueCursor/*<Pair<Num, Num>>*/
+   element: U
+   propFocus: keyof T | null = null
 
-   constructor (editor: Editor.Editor, tp: ExplValueCursor/*<Pair<Num, Num>>*/, marker: SVGElement) {
+   constructor (editor: Editor.Editor, C: Class<T>, cursor: ExplValueCursor/*<Pair<Num, Num>>*/, element: U) {
       this.editor = editor
-      this.tooltip = createTooltip(marker, editor.tooltipPlacement)
+      this.tooltip = createTooltip(element, editor.tooltipPlacement)
       this.editor.tooltips.add(this.tooltip)
-      this.tp = tp
-      this.marker = marker
-      const p: Pair<Num, Num> = as(tp.tv.v, Pair)
-      const propsFocus: (keyof Pair)[] = focusedProps(editor.direction, tp.tv as ExplValue<Pair>)
+      this.cursor = cursor
+      this.element = element
+      const p: Pair<Num, Num> = as(cursor.tv.v, C)
+      const propsFocus: (keyof Pair)[] = this.focusedProps(cursor.tv as ExplValue<Pair>)
       if (propsFocus.length > 0) {
          this.tooltip.setContent(propValues(p, propsFocus))
          this.tooltip.show()
-         marker.classList.add("focus")
+         element.classList.add("focus")
       }
-      marker.addEventListener("mousemove", (e: MouseEvent): void => {
+      element.addEventListener("mousemove", (e: MouseEvent): void => {
          e.stopPropagation()
          this.onMouseMove(e)
       })
-      marker.addEventListener("mouseout", (e: MouseEvent): void => {
+      element.addEventListener("mouseout", (e: MouseEvent): void => {
          e.stopPropagation()
          this.onMouseOut(e)
       })
    }
 
-   onMouseMove (e: MouseEvent): void {
+   focusedProps<T extends DataValue> (tv: ExplValue<T>): (keyof T)[] {
+      return fields(tv.v).filter((prop: keyof T) => {
+         const tv_: ExplValue = Expl.explChild(tv.t, tv.v, prop)
+         return __nonNull(this.editor.direction) === Direction.Fwd ? bool_.negate(isα(tv_)) : isα(tv_)
+      })
    }
+   
+   abstract onMouseMove (e: MouseEvent): void
 
    onMouseOut (e: MouseEvent): void {
+      this.propFocus = null
+      assert(this.editor.bwdSlice(() => {}).size === 0)
+      this.element.classList.remove("focus")
    }
 }
 
-export class RectInteractor {
-   editor: Editor.Editor
-   tooltip: Tooltip
-   tg: ExplValueCursor/*<Rect>*/
-   r: SVGRectElement
-   propFocus: keyof Rect | null = null
-
-   constructor (editor: Editor.Editor, tg: ExplValueCursor/*<Rect>*/, r: SVGRectElement) {
-      this.editor = editor
-      this.tooltip = createTooltip(r, editor.tooltipPlacement)
-      this.editor.tooltips.add(this.tooltip)
-      this.tg = tg
-      this.r = r
-      const g: Rect = as(tg.tv.v, Rect)
-      const propsFocus: (keyof Rect)[] = focusedProps(editor.direction, tg.tv as ExplValue<Rect>)
-      if (propsFocus.length > 0) {
-         this.tooltip.setContent(propValues(g, propsFocus))
-         this.tooltip.show()
-         r.classList.add("focus")
-      }
-      r.addEventListener("mousemove", (e: MouseEvent): void => {
-         e.stopPropagation()
-         this.onMouseMove(e)
-      })
-      r.addEventListener("mouseout", (e: MouseEvent): void => {
-         e.stopPropagation()
-         this.onMouseOut(e)
-      })
+export class PointInteractor extends Interactor<Pair, SVGElement> {
+   constructor (editor: Editor.Editor, tp: ExplValueCursor/*<Pair<Num, Num>>*/, marker: SVGElement) {
+      super(editor, Pair, tp, marker)
    }
 
    onMouseMove (e: MouseEvent): void {
-      const g: Rect = as(this.tg.tv.v, Rect)
-      const rect: ClientRect = this.r.getBoundingClientRect()
+   }
+}
+
+export class RectInteractor extends Interactor<Rect, SVGRectElement> {
+   constructor (editor: Editor.Editor, tg: ExplValueCursor/*<Rect>*/, r: SVGRectElement) {
+      super(editor, Rect, tg, r)
+   }
+
+   onMouseMove (e: MouseEvent): void {
+      const g: Rect = as(this.cursor.tv.v, Rect)
+      const rect: ClientRect = this.element.getBoundingClientRect()
       // invert sign on y axis because of global inversion for SVG graphics
       const x_prop: number = Math.max(e.clientX - rect.left, 0) / rect.width
       const y_prop: number = Math.min(rect.bottom - e.clientY, rect.height) / rect.height
@@ -115,17 +101,11 @@ export class RectInteractor {
       if (this.propFocus !== propFocus) {
          this.propFocus = propFocus
          this.editor.bwdSlice(() => {
-            setα(bool_.top, this.tg.to(Rect, propFocus).tv)
+            setα(bool_.top, this.cursor.to(Rect, propFocus).tv)
          })
          this.tooltip.setContent(propValues(g, [propFocus]))
-         this.r.classList.add("focus")
+         this.element.classList.add("focus")
       }
-   }
-
-   onMouseOut (e: MouseEvent): void {
-      this.propFocus = null
-      assert(this.editor.bwdSlice(() => {}).size === 0)
-      this.r.classList.remove("focus")
    }
 
    // Determine which "diagonal quadrant" of the rectangle [width, height] contains [x, y], and
