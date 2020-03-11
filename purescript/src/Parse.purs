3e788b53994648f304ae9c9f4fe0301ba8f7fab1
Roly Perera <rperera@turing.ac.uk>
2020-03-11 21:21:21 +0000

    Not resolving LanguageDef properly..

--- purescript/src/Parse.purs
+++ purescript/src/Parse.purs
@@ -1,9 +1,33 @@
-module Parse where 
+module Parse where
 
 import Prelude hiding (between)
-import Text.Parsing.Parser (ParserT)   
-import Text.Parsing.Parser.Combinators (between)   
-import Text.Parsing.Parser.String (string)   
+import Text.Parsing.Parser (ParserT)
+import Text.Parsing.Parser.Combinators (between)
+import Text.Parsing.Parser.Language (emptyDef)
+import Text.Parsing.Parser.String (oneOf, string)
+import Text.Parsing.Parser.Token (LanguageDef, makeTokenParser, unGenLanguageDef)
 
 parens :: forall m a. Monad m => ParserT String m a -> ParserT String m a
 parens = between (string "(") (string ")")
+
+-- based on haskellStyle
+languageDef :: LanguageDef
+languageDef = LanguageDef (unGenLanguageDef emptyDef)
+                { commentStart    = "{-"
+                , commentEnd      = "-}"
+                , commentLine     = "--"
+                , nestedComments  = true
+                , identStart      = letter
+                , identLetter     = alphaNum <|> oneOf ['_', '\'']
+                , opStart         = op'
+                , opLetter        = op'
+                , reservedOpNames = []
+                , reservedNames   = []
+                , caseSensitive   = true
+                }
+  where
+    op' :: forall m . (Monad m) => ParserT String m Char
+    op' = oneOf [':', '!', '#', '$', '%', '&', '*', '+', '.', '/', '<', '=', '>', '?', '@', '\\', '^', '|', '-', '~']
+
+tokenParser :: TokenParser
+tokenParser = makeTokenParser languageDef
