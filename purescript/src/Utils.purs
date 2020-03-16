c77aa7d7d4a305a54cae61bd5dfefd1d57fd91c8
min-nguyen <minhnguyen1995@googlemail.com>
2020-03-16 15:31:24 +0000

    Added free var function

---
+++ purescript/src/Utils.purs
@@ -0,0 +1,55 @@
+module Utils where
+  
+import Data.List (List(..), (:), difference, union, singleton)
+import Prelude ((<>))
+
+import Expr
+
+class FreeVars a where 
+    freeVars :: a -> List Var
+
+instance freeVarsExpr :: FreeVars Expr where
+    freeVars (ExprNum i) = Nil
+    freeVars (ExprVar x)  = singleton x
+    freeVars (ExprLet x e1 e2)
+        = union (freeVars e1) (difference (freeVars e2) (singleton x))
+    freeVars (ExprAdd e1 e2)
+        = union (freeVars e1) (freeVars e2)
+    freeVars ExprTrue  = Nil
+    freeVars ExprFalse = Nil
+    freeVars (ExprPair e1 e2) = union (freeVars e1) (freeVars e2)
+    freeVars (ExprPair_Fst e1) = freeVars e1 
+    freeVars (ExprPair_Snd e2) = freeVars e2
+    freeVars ExprNil = Nil
+    freeVars (ExprCons e es) = (freeVars e <> freeVars es)
+    freeVars (ExprCons_Head e) = freeVars e
+    freeVars (ExprCons_Tail es) = freeVars es
+    freeVars (ExprLet_Body e2) = freeVars e2
+    freeVars (ExprMatch e elim) = union (freeVars e) (freeVars elim)
+    freeVars (ExprApp e1 e2) = union (freeVars e1) (freeVars e2)
+    freeVars (ExprApp_Fun e1) = freeVars e1
+    freeVars (ExprFun elim) = freeVars elim
+
+instance freeVarsElim :: FreeVars Elim where 
+    freeVars (ElimVar x e) = difference (freeVars e) (singleton x)
+    freeVars (ElimPair x y e) = difference (freeVars e) (x:y:Nil)
+    freeVars (ElimPair_Fst x e) = difference (freeVars e) (singleton x)
+    freeVars (ElimPair_Snd y e) = difference (freeVars e) (singleton y)
+    freeVars (ElimList bNil bCons) = union (freeVars bNil) (freeVars bCons)
+    freeVars (ElimBool bTrue bFalse) = Nil
+
+instance freeVarsBranchNil :: FreeVars BranchNil where 
+    freeVars (BranchNil e) = freeVars e
+
+instance freeVarsBranchCons :: FreeVars BranchCons where
+    freeVars (BranchCons x xs e) = difference (freeVars e) (x:xs:Nil)
+    freeVars (BranchCons_Head x e) = difference (freeVars e) (singleton x)
+    freeVars (BranchCons_Tail xs e) = difference (freeVars e) (singleton xs)
+
+instance freeVarsBranchTrue :: FreeVars BranchTrue where
+    freeVars (BranchTrue e) = freeVars e 
+
+instance freeVarsBranchFalse :: FreeVars BranchFalse where
+    freeVars (BranchFalse e) = freeVars e 
+
+
