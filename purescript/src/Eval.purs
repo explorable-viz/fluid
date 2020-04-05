261068cc8b8a6a15cf9d945bbe3d3a1992b3819f
Roly Perera <rperera@turing.ac.uk>
2020-04-05 09:24:50 +0100

    Extract Expl file.

--- purescript/src/Eval.purs
+++ purescript/src/Eval.purs
@@ -3,13 +3,14 @@ module Eval where
 import Prelude ((<>), ($))
 import Data.Maybe (Maybe(..))
 import Data.Semiring ((+))
+import Expl (Match(..), Trace(..))
 import Expr
 
 
 match :: Val -> Elim -> Maybe (T3 Env Expr Match)
 match val σ
- = case  val, σ of 
-    _, ElimVar x t expr 
+ = case  val, σ of
+    _, ElimVar x t expr
         ->  Just $ T3 (EnvNil :∈: T2 x val) expr (MatchVar x)
     ValTrue, ElimBool (BranchTrue expr1) (BranchFalse expr2)
         ->  Just $ T3 EnvNil expr1 MatchTrue
@@ -18,9 +19,9 @@ match val σ
     ValPair x' y', ElimPair x _ y _ expr
         ->  let ρ' = (EnvNil :∈: T2 y y' :∈: T2 x x')
             in  Just $ T3 ρ' expr (MatchPair x y)
-    ValNil, ElimList (BranchNil _ expr2) (BranchCons x xs _ expr1) 
+    ValNil, ElimList (BranchNil _ expr2) (BranchCons x xs _ expr1)
         ->  Just $ T3 EnvNil expr2 MatchNil
-    ValCons v vs, ElimList (BranchNil _ expr2) (BranchCons x xs _ expr1) 
+    ValCons v vs, ElimList (BranchNil _ expr2) (BranchCons x xs _ expr1)
         ->  let ρ' = (EnvNil :∈: T2 xs vs :∈: T2 x v)
             in  Just $ T3 ρ' expr1 (MatchCons x xs)
     _, _ ->  Nothing
@@ -37,12 +38,12 @@ eval (ExprVar x) ρ
 eval ExprTrue  ρ             = T2 TraceTrue ValTrue
 eval ExprFalse  ρ            = T2 TraceFalse ValFalse
 eval (ExprNum n)  ρ          = T2 (TraceNum n) (ValNum n)
-eval (ExprPair e1 e2)  ρ     
+eval (ExprPair e1 e2)  ρ
  = let T2 t1 v1 = eval e1 ρ
        T2 t2 v2 = eval e2 ρ
-   in  T2 (TracePair t1 t2) (ValPair v1 v2) 
+   in  T2 (TracePair t1 t2) (ValPair v1 v2)
 eval (ExprLetrec fun σ e)  ρ   = let T2 t v = eval e (ρ :∈: T2 fun (ValClosure ρ fun σ))
-                                     t'     = TraceLetrec fun (TraceClosure ρ σ) t 
+                                     t'     = TraceLetrec fun (TraceClosure ρ σ) t
                                  in  T2 t' v
 eval (ExprApp e e')  ρ
  = case eval e ρ  of
@@ -58,19 +59,19 @@ eval (ExprAdd e1 e2) ρ
        T2 t2 v2 = eval e2 ρ
    in  case v1, v2 of
           (ValNum n1), (ValNum n2) -> T2 (TraceAdd t1 t2) (ValNum (n1 + n2))
-          _,          _            -> T2 TraceBottom (ValFailure "Arithemetic type error: e1 or/and e2 do not evaluate to ints")
+          _,          _            -> T2 TraceBottom (ValFailure "Arithmetic type error: e1 or/and e2 do not evaluate to ints")
 eval (ExprLet x e1 e2) ρ
  = let T2 t1 v1  = eval e1 ρ
        ρ'  = (ρ :∈: T2 x v1)
        T2 t2 v2  = eval e2 ρ'
    in  T2 (TraceLet x t1 t2) (v2)
 eval ExprNil ρ               = T2 TraceNil ValNil
-eval (ExprCons e es)  ρ      
- = let T2 t1 v1 = (eval e ρ) 
+eval (ExprCons e es)  ρ
+ = let T2 t1 v1 = (eval e ρ)
        T2 t2 v2 = (eval es ρ)
    in  T2 (TraceCons t1 t2) (ValCons v1 v2)
 eval (ExprMatch e σ) ρ
- = let T2 t1 v1 =  (eval e ρ) 
+ = let T2 t1 v1 =  (eval e ρ)
    in case (match v1 σ) of
         Nothing            -> T2 TraceBottom (ValFailure "Match not found")
         Just (T3 ρ' e' m)  -> let T2 t2 v2 = eval e' (concEnv ρ ρ')
