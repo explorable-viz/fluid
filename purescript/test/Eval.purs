d8ff09e0bf33749a169f213d3d209b927c5d42f4
Roly Perera <rperera@turing.ac.uk>
2020-04-29 16:31:33 +0100

    Simplify eval test.

--- purescript/test/Eval.purs
+++ purescript/test/Eval.purs
@@ -1,20 +1,16 @@
 module Test.Eval where
 
-import Expr (Bindings(..), Elim(..), Expr(..), Val)
+import Expr (Bindings(..), Elim(..), Expr(..))
 import Eval (ExplVal, eval)
 
 
 expr1 :: Expr
-expr1 = let e = (ExprCons (ExprInt 1) (ExprCons (ExprInt 2) (ExprCons (ExprInt 3) ExprNil)))
-            f = ExprLetrec "f" (ElimList { bnil  : ExprNil,
-                                          bcons : { x : "x", y : "xs", e : ExprCons (ExprAdd (ExprVar "x") (ExprInt 1))
-                                                                                    (ExprApp (ExprVar "f") (ExprVar "xs"))}})
-                                                                                    (ExprApp (ExprVar "f") e)
-        in  f
+expr1 = let e = ExprCons (ExprInt 1) (ExprCons (ExprInt 2) (ExprCons (ExprInt 3) ExprNil))
+        in ExprLetrec "f" (ElimList { bnil  : ExprNil,
+                                      bcons : { x : "x", y : "xs", e : ExprCons (ExprAdd (ExprVar "x") (ExprInt 1))
+                                                                                (ExprApp (ExprVar "f") (ExprVar "xs"))}})
+                      (ExprApp (ExprVar "f") e)
 
-
-env1 :: Bindings Val
-env1 = Empty
 
 evalExpr1 :: Partial => ExplVal
 evalExpr1 = eval Empty expr1
