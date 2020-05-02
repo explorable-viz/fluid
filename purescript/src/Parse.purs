41b5ce4f6961a2ae109e305a7359419499a907e5
Roly Perera <rperera@turing.ac.uk>
2020-05-02 11:25:45 +0100

    Import spec of old (JSON-compatible) number parser.

--- purescript/src/Parse.purs
+++ purescript/src/Parse.purs
@@ -53,12 +53,38 @@ variable = ident >>= compose pure ExprVar
 ident ∷ SParser Var
 ident = tokenParser.identifier
 
+-- JSON grammar for numbers, https://tools.ietf.org/html/rfc7159.html#section-6.
+-- I'm assuming (but haven't checked that) DIGIT is equivalent to LanguageDef's "digit" parser.
+
+{-}
+-- decimal-point = %x2E       ; .
+const decimal_point = ch(".")
+-- digit1-9 = %x31-39         ; 1-9
+const digit1to9: Parser<string> = range("1", "9")
+-- e = %x65 / %x45            ; e E
+const e: Parser<string> = choice([ch("e"), ch("E")])
+-- minus = %x2D               ; -
+const minus: Parser<string> = ch("-")
+-- plus = %x2B                ; +
+const plus: Parser<string> = ch("+")
+-- zero = %x30                ; 0
+const zero: Parser<string> = ch("0")
+-- exp = e [ minus / plus ] 1*DIGIT
+const exp: Parser<string> = withJoin(sequence([e, optional(choice([minus, plus]), () => ""), withJoin(repeat1(DIGIT))]))
+-- frac = decimal-point 1*DIGIT
+const frac = withJoin(sequence([decimal_point, withJoin(repeat1(DIGIT))]))
+-- int = zero / ( digit1-9 *DIGIT )
+const int: Parser<string> = choice([zero, withJoin(sequence([digit1to9, withJoin(repeat(DIGIT))]))])
+-- number = [ minus ] int [ frac ] [ exp ]
+const numberʹ: Parser<string> =
+   withJoin(sequence([optional(minus, () => ""), int, optional(frac, () => ""), optional(exp, () => "")]))
+-}
+
 simpleExpr :: SParser Expr -> SParser Expr
 simpleExpr expr' =
-   variable <|> let_ expr'
-{-   variable {% id %} |
-   string {% id %} |
-   number {% id %} |
+   variable <|>
+   let_ expr'
+{- string {% id %} |
    parenthExpr {% id %} |
    pair {% id %} |
    list {% id %} |
@@ -71,12 +97,12 @@ let_ term' = do
    x ← ident
    e1 ← do
       tokenParser.reservedOp "="
-      term
+      term'
    e2 ← do
       keyword strIn
       term'
    pure $ ExprLet x e1 e2
 
-term :: SParser Expr
-term = fix $ \p ->
+expr :: SParser Expr
+expr = fix $ \p ->
    simpleExpr p
