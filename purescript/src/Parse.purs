69a74f997390186814fce84103331bf4710379a3
Roly Perera <rperera@turing.ac.uk>
2020-05-02 12:05:13 +0100

    Parenthesised expressions. Ignore floats and string literals for now.

--- purescript/src/Parse.purs
+++ purescript/src/Parse.purs
@@ -3,7 +3,7 @@ module Parse where
 import Prelude hiding (between)
 import Control.Alt ((<|>))
 import Control.Lazy (fix)
-import Text.Parsing.Parser (Parser, ParserT)
+import Text.Parsing.Parser (Parser)
 import Text.Parsing.Parser.Combinators (between)
 import Text.Parsing.Parser.Language (emptyDef)
 import Text.Parsing.Parser.String (oneOf, string)
@@ -15,12 +15,14 @@ import Expr (Expr(..), Var)
 
 type SParser = Parser String
 
--- constants
+-- constants (should also be used by prettyprinter)
 strIn = "in" :: String
 strLet = "let" :: String
+strLParen = "(" :: String
+strRParen = "(" :: String
 
-parens :: forall m a. Monad m => ParserT String m a -> ParserT String m a
-parens = between (string "(") (string ")")
+parens :: forall a . SParser a -> SParser a
+parens = between (string strLParen) (string strRParen)
 
 languageDef :: LanguageDef
 languageDef = LanguageDef (unGenLanguageDef emptyDef)
@@ -37,7 +39,7 @@ languageDef = LanguageDef (unGenLanguageDef emptyDef)
                 , caseSensitive   = true
                 }
    where
-      op' :: forall m . (Monad m) => ParserT String m Char
+      op' :: SParser Char
       op' = oneOf [':', '!', '#', '$', '%', '&', '*', '+', '.', '/', '<', '=', '>', '?', '@', '\\', '^', '|', '-', '~']
 
 tokenParser :: TokenParser
@@ -53,44 +55,17 @@ variable = ident >>= compose pure ExprVar
 ident ∷ SParser Var
 ident = tokenParser.identifier
 
--- JSON grammar for numbers, https://tools.ietf.org/html/rfc7159.html#section-6.
--- I'm assuming (but haven't checked that) DIGIT is equivalent to LanguageDef's "digit" parser.
-
-{-
--- decimal-point = %x2E       ; .
-const decimal_point = ch(".")
--- digit1-9 = %x31-39         ; 1-9
-const digit1to9: Parser<string> = range("1", "9")
--- e = %x65 / %x45            ; e E
-const e: Parser<string> = choice([ch("e"), ch("E")])
--- minus = %x2D               ; -
-const minus: Parser<string> = ch("-")
--- plus = %x2B                ; +
-const plus: Parser<string> = ch("+")
--- zero = %x30                ; 0
-const zero: Parser<string> = ch("0")
--- exp = e [ minus / plus ] 1*DIGIT
-const exp: Parser<string> = withJoin(sequence([e, optional(choice([minus, plus]), () => ""), withJoin(repeat1(DIGIT))]))
--- frac = decimal-point 1*DIGIT
-const frac = withJoin(sequence([decimal_point, withJoin(repeat1(DIGIT))]))
--- int = zero / ( digit1-9 *DIGIT )
-const int: Parser<string> = choice([zero, withJoin(sequence([digit1to9, withJoin(repeat(DIGIT))]))])
--- number = [ minus ] int [ frac ] [ exp ]
-const numberʹ: Parser<string> =
-   withJoin(sequence([optional(minus, () => ""), int, optional(frac, () => ""), optional(exp, () => "")]))
--}
-
 int :: SParser Expr
 int = tokenParser.integer >>= compose pure ExprInt
 
+-- TODO: string, float
 simpleExpr :: SParser Expr -> SParser Expr
 simpleExpr expr' =
    variable <|>
    let_ expr' <|>
-   int
-{- string {% id %} |
-   parenthExpr {% id %} |
-   pair {% id %} |
+   int <|>
+   parens expr'
+{- pair {% id %} |
    list {% id %} |
    constr {% id %}
 -}
