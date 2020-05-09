3c9bcf03b6b5e2e7b26b129fc939acccfe215b41
Roly Perera <rperera@turing.ac.uk>
2020-05-09 13:43:32 +0100

    New test file just for parser work.

--- purescript/test/Parse.purs
+++ purescript/test/Parse.purs
@@ -8,5 +8,5 @@ import Node.FS.Sync (readTextFile)
 
 testParse1 :: Effect Unit
 testParse1 = do
-  text <- readTextFile ASCII "../fluid/example/normalise.fld"
+  text <- readTextFile ASCII "../fluid/example/parser-wip.fld"
   log text
