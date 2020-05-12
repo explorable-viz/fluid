380c9fb07a8fa70d3dc060131e5531d06ebe5e8b
Roly Perera <rperera@turing.ac.uk>
2020-05-12 18:26:26 +0100

    Ok, problem with parsing operators - can't fail, need 'try' like semantics.

--- purescript/test/Main.purs
+++ purescript/test/Main.purs
@@ -2,13 +2,8 @@ module Test.Main where
 
 import Prelude
 import Effect (Effect)
-import Effect.Class.Console (log)
-import Test.Eval (evalExpr1)
 import Test.Parse (testParse1)
-import Pretty (pretty)
-import Partial.Unsafe (unsafePartial)
 
 main :: Effect Unit
 main = do
-  log $ show (pretty (unsafePartial evalExpr1))
   testParse1
