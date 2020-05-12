562ff4c7a51110876a3aca0d2790eeb038717374
Roly Perera <rperera@turing.ac.uk>
2020-05-12 18:23:36 +0100

    Ditto eval.

--- purescript/test/Main.purs
+++ purescript/test/Main.purs
@@ -4,7 +4,6 @@ import Prelude
 import Effect (Effect)
 import Effect.Class.Console (log)
 import Test.Eval (evalExpr1)
-import Test.Fwd (v)
 import Test.Parse (testParse1)
 import Pretty (pretty)
 import Partial.Unsafe (unsafePartial)
@@ -12,5 +11,4 @@ import Partial.Unsafe (unsafePartial)
 main :: Effect Unit
 main = do
   log $ show (pretty (unsafePartial evalExpr1))
-  log $ show (pretty v)
   testParse1
