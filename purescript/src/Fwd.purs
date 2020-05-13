977771cdad98d57b38d13cbdcedd88d907e3a43d
Roly Perera <rperera@turing.ac.uk>
2020-05-13 12:04:07 +0100

    fwd pattern-matching no longer needs a trace.

--- purescript/src/Fwd.purs
+++ purescript/src/Fwd.purs
@@ -13,23 +13,23 @@ import Val (Env, Val)
 import Val (RawVal(..)) as V
 
 
-fwd_match :: Val -> Elim -> Match -> Maybe (T3 Env Expr Selected)
+fwd_match :: Val -> Elim -> Maybe (T3 Env Expr Selected)
 -- var
-fwd_match v (ElimVar { x, e }) (MatchVar _) = Just $ T3 (Empty :+: Bind x v) e Top
+fwd_match v (ElimVar { x, e }) = Just $ T3 (Empty :+: Bind x v) e Top
 -- true
-fwd_match { α, u: V.True } (ElimBool { true: e, false: _ }) MatchTrue = Just $ T3 Empty e α
+fwd_match { α, u: V.True } (ElimBool { true: e, false: _ }) = Just $ T3 Empty e α
 -- false
-fwd_match { α, u: V.False } (ElimBool { true: _, false: e }) MatchFalse = Just $ T3 Empty e α
+fwd_match { α, u: V.False } (ElimBool { true: _, false: e }) = Just $ T3 Empty e α
 -- pair
-fwd_match { α, u: V.Pair u v } (ElimPair { x, y, e }) (MatchPair _ _) =
+fwd_match { α, u: V.Pair u v } (ElimPair { x, y, e }) =
    Just $ T3 (Empty :+: Bind x u :+: Bind y v) e α
 -- nil
-fwd_match { α, u: V.Nil } (ElimList { nil: e, cons: _ }) MatchNil = Just $ T3 Empty e α
+fwd_match { α, u: V.Nil } (ElimList { nil: e, cons: _ }) = Just $ T3 Empty e α
 -- cons
-fwd_match { α, u: V.Cons u v } (ElimList { nil: _, cons: { x, y, e } }) (MatchCons _ _) =
+fwd_match { α, u: V.Cons u v } (ElimList { nil: _, cons: { x, y, e } }) =
    Just $ T3 (Empty :+: Bind x u :+: Bind y v) e Top
 -- failure
-fwd_match _ _ _ =  Nothing
+fwd_match _ _ =  Nothing
 
 fwd :: Env -> Expr -> Expl -> Selected -> Val
 -- var
@@ -55,7 +55,7 @@ fwd ρ { r: Letrec f σ e } (T.Letrec _ _ t) α = fwd (ρ :+: Bind f { α, u: V.
 fwd ρ { r: App e e' } (T.App t t' ξ t'') α =
    case fwd ρ e t α  of
       { α: α', u: V.Closure ρ' f σ } ->
-         case fwd_match (fwd ρ e' t' α) σ ξ of
+         case fwd_match (fwd ρ e' t' α) σ of
             Just (T3 ρ'' e'' α'') ->
                let ρ_f = (ρ' <> ρ'') :+: Bind f { α: α', u: (V.Closure ρ' f σ) } in fwd ρ_f e'' t'' (α' ∧ α'')
             Nothing -> absurd
@@ -70,7 +70,7 @@ fwd ρ { r: Let x e1 e2 } (T.Let _ t1 t2) α =
    fwd (ρ :+: Bind x (fwd ρ e1 t1 α)) e2 t2 α
 -- match
 fwd ρ { r: Match e σ } (T.Match t ξ t') α =
-   case fwd_match (fwd ρ e t α) σ ξ of
+   case fwd_match (fwd ρ e t α) σ of
       Just (T3 ρ' e' α') -> fwd (ρ <> ρ') e' t' α'
       Nothing -> absurd
 -- trace/expression mismatch
