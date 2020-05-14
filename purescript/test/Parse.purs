b4428134aea42597be82f0a0c6ab72cd266bd84e
Roly Perera <rperera@turing.ac.uk>
2020-05-14 19:26:30 +0100

    Plug in evaluator.

--- purescript/test/Parse.purs
+++ purescript/test/Parse.purs
@@ -4,7 +4,6 @@ import Prelude
 import Data.Either (Either(..))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Effect.Class.Console (log)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
 import Text.Parsing.Parser (runParser)
@@ -12,8 +11,11 @@ import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
+import Eval (eval)
 import Parse (program)
-import Pretty (prettyProgram)
+import Pretty (pretty)
+import Util (error)
+import Val (primitives)
 
 
 testParse1 :: Effect Unit
@@ -24,8 +26,8 @@ testParse1 = do
       describe "Parse" do
          it "blah" do
             case result of
-               Left error -> do
-                  log $ show error
-                  true `shouldEqual` false
+               Left parseError -> do
+                  error $ show parseError
                Right e -> do
-                  (show $ prettyProgram e) `shouldEqual` text
+                  let { u } = (eval primitives e).v
+                  (show $ pretty u) `shouldEqual` "5"
