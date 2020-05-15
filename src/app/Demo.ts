d365d6013a7799da36b5524a879cd73595b3ac0e
Roly Perera <rperera@turing.ac.uk>
2020-05-15 12:22:51 +0100

    For now omit grouped-bar-chart.

--- src/app/Demo.ts
+++ src/app/Demo.ts
@@ -14,12 +14,12 @@ export function initialise (): void {
    // TODO: eliminate redundancy with "renewables" test
    const appRoot: HTMLElement = __nonNull(document.getElementById("app-root"))
    const coordinator: PaneCoordinator = new PaneCoordinator(appRoot, openDatasetAs("renewables-restricted", "data"))
-   const [ρ1, e1]: [Env, Expr] = openWithImports("graphics/grouped-bar-chart")
+// const [ρ1, e1]: [Env, Expr] = openWithImports("graphics/grouped-bar-chart")
    const [ρ2, e2]: [Env, Expr] = openWithImports("graphics/stacked-bar-chart")
    const [ρ3, e3]: [Env, Expr] = openWithImports("graphics/line-chart")
    // Wait for fonts to load before rendering, otherwise metrics will be wrong.
    window.onload = (ev: Event): void => {
-      coordinator.addPane(ρ1, e1)
+//    coordinator.addPane(ρ1, e1)
       coordinator.addPane(ρ2, e2)
       coordinator.addPane(ρ3, e3)
    }
