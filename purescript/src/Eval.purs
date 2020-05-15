64ff0cadb8c03b9e8eefe11d49979027f444d4b7
Roly Perera <rperera@turing.ac.uk>
2020-05-15 07:57:04 +0100

    Strip case-analysis comments.

--- purescript/src/Eval.purs
+++ purescript/src/Eval.purs
@@ -12,58 +12,41 @@ import Val (Env, Val, toValues, val)
 import Val (RawVal(..)) as V
 
 match :: Val -> Elim -> Maybe (T3 Env Expr Match)
--- var
 match v (ElimVar { x, e }) = Just $ T3 (ε :+: x ↦ v) e (MatchVar x)
--- true
 match { u: V.True } (ElimBool { true: e1, false: _ }) = Just $ T3 ε e1 MatchTrue
--- false
 match { u: V.False } (ElimBool { true: _, false: e2 }) = Just $ T3 ε e2 MatchFalse
--- pair
 match { u: V.Pair v v' } (ElimPair { x, y, e }) = Just $ T3 (ε :+: x ↦ v :+: y ↦ v') e (MatchPair x y)
--- nil
 match { u: V.Nil } (ElimList { nil: e, cons: _ }) = Just $ T3 ε e MatchNil
--- cons
 match { u : V.Cons v v' } (ElimList { nil: _, cons: { x, y, e } }) =
    Just $ T3 (ε :+: x ↦ v :+: y ↦ v') e (MatchCons x y)
--- failure
 match _ _ = Nothing
 
 type ExplVal = { t :: Expl, v :: Val }
 
 eval :: Env -> Expr -> ExplVal
--- var
 eval ρ { r: Var x } =
    case find x ρ of
       Just v -> { t: T.Var x, v }
       _ -> error $ "variable " <> x <> " not found"
--- op
 eval ρ { r: Op op } =
    case find op ρ of
       Just v -> { t: T.Op op, v }
       _ -> error $ "operator " <> op <> " not found"
--- true
 eval ρ { r: True } = { t: T.True, v: val V.True }
--- false
 eval ρ { r: False } = { t: T.False, v: val V.False }
--- int
 eval ρ { r: Int n } = { t: T.Int n, v: val $ V.Int n }
--- pair
 eval ρ { r: Pair e e' } =
    let { t, v } = eval ρ e
        { t: t', v: v' } = eval ρ e'
    in  { t: T.Pair t t', v: val $ V.Pair v v' }
--- nil
 eval ρ { r: Nil } = { t: T.Nil, v: val V.Nil }
--- cons
 eval ρ { r: Cons e e' } =
    let { t, v } = eval ρ e
        { t: t', v: v' } = eval ρ e'
    in { t: T.Cons t t', v: val $ V.Cons v v' }
--- letrec
 eval ρ { r: Letrec f σ e } =
    let { t, v } = eval (ρ :+: f ↦ (val $ V.Closure ρ f σ)) e
    in { t: T.Letrec f (T.Fun ρ σ) t, v }
--- apply
 eval ρ { r: App e e' } =
    case eval ρ e, eval ρ e' of
       { t, v: { u: V.Closure ρ' f σ } }, { t: t', v } ->
@@ -77,7 +60,6 @@ eval ρ { r: App e e' } =
       { t, v: { u: V.PartialApp op v } }, { t: t', v: v' } ->
          { t: T.AppOp t t', v: toValues (opFun op) v v' }
       _, _ -> error "Expected closure or operator"
--- binary app
 eval ρ { r : BinaryApp e op e' } =
    let { t, v } = eval ρ e
        { t: t', v: v' } = eval ρ e' in
@@ -85,12 +67,10 @@ eval ρ { r : BinaryApp e op e' } =
       Just { u: V.Op φ } -> { t: T.BinaryApp t op t', v: toValues (opFun φ) v v' }
       Just _ -> absurd
       Nothing -> error $ "operator " <> op <> " not found"
--- let
 eval ρ { r : Let x e e' } =
    let { t, v } = eval ρ e
        { t: t', v: v' } = eval (ρ :+: x ↦ v) e'
    in { t: T.Let x t t', v: v' }
--- match
 eval ρ { r : Match e σ } =
    let { t, v } = eval ρ e
    in case match v σ of
--- purescript/src/Fwd.purs
+++ purescript/src/Fwd.purs
@@ -12,49 +12,30 @@ import Val (RawVal(..)) as V
 
 
 match_fwd :: Val -> Elim -> Maybe (T3 Env Expr Selected)
--- var
 match_fwd v (ElimVar { x, e }) = Just $ T3 (ε :+: x ↦ v) e Top
--- true
 match_fwd { α, u: V.True } (ElimBool { true: e, false: _ }) = Just $ T3 ε e α
--- false
 match_fwd { α, u: V.False } (ElimBool { true: _, false: e }) = Just $ T3 ε e α
--- pair
-match_fwd { α, u: V.Pair u v } (ElimPair { x, y, e }) =
-   Just $ T3 (ε :+: x ↦ u :+: y ↦ v) e α
--- nil
+match_fwd { α, u: V.Pair u v } (ElimPair { x, y, e }) = Just $ T3 (ε :+: x ↦ u :+: y ↦ v) e α
 match_fwd { α, u: V.Nil } (ElimList { nil: e, cons: _ }) = Just $ T3 ε e α
--- cons
-match_fwd { α, u: V.Cons u v } (ElimList { nil: _, cons: { x, y, e } }) =
-   Just $ T3 (ε :+: x ↦ u :+: y ↦ v) e Top
--- failure
+match_fwd { α, u: V.Cons u v } (ElimList { nil: _, cons: { x, y, e } }) = Just $ T3 (ε :+: x ↦ u :+: y ↦ v) e Top
 match_fwd _ _ = Nothing
 
 fwd :: Env -> Expr -> Selected -> Val
--- var
 fwd ρ { r: Var x } _ =
    case find x ρ of
       Just v -> v
       _ -> absurd
--- op
 fwd ρ { r: Op op } _ =
    case find op ρ of
       Just v -> v
       _ -> absurd
--- true
 fwd ρ { α, r: True } α' = { α: α ∧ α', u: V.True }
--- false
 fwd ρ { α, r: False } α' = { α: α ∧ α', u: V.False }
--- int
 fwd ρ { α, r: Int n } α' = { α: α ∧ α', u: V.Int n }
--- pair
 fwd ρ { α, r: Pair e1 e2 } α' = { α: α ∧ α', u: V.Pair (fwd ρ e1 α') (fwd ρ e2 α') }
--- nil
 fwd ρ { α, r: Nil} α' = { α: α ∧ α', u: V.Nil }
--- cons
 fwd ρ { α, r: Cons e e' } α' = { α: α ∧ α', u: V.Cons (fwd ρ e α') (fwd ρ e' α') }
--- letrec
 fwd ρ { r: Letrec f σ e } α = fwd (ρ :+: f ↦ { α, u: V.Closure ρ f σ }) e α
--- app
 fwd ρ { r: App e e' } α =
    case fwd ρ e α, fwd ρ e' α of
       { α: α', u: V.Closure ρ' f σ }, v ->
@@ -65,14 +46,11 @@ fwd ρ { r: App e e' } α =
       { α: α', u: V.Op op }, v -> { α: α', u: V.PartialApp op v }
       { α: α', u: V.PartialApp op v }, v' -> toValues_fwd (opFun op) α' v v'
       _, _ -> absurd
--- binary app
 fwd ρ { r: BinaryApp e1 op e2 } α =
    case find op ρ of
       Just { α: α', u: V.Op φ } -> toValues_fwd (opFun φ) α' (fwd ρ e1 α) (fwd ρ e2 α)
       _ -> absurd
--- let
 fwd ρ { r: Let x e1 e2 } α = fwd (ρ :+: x ↦ fwd ρ e1 α) e2 α
--- match
 fwd ρ { r: Match e σ } α =
    case match_fwd (fwd ρ e α) σ of
       Just (T3 ρ' e' α') -> fwd (ρ <> ρ') e' α'
