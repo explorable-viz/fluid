0499de003e8a2fe04df0ff84df035ece6de00550
Roly Perera <rperera@turing.ac.uk>
2020-05-16 12:47:46 +0100

    Drop dependency on Node.FS.

--- new/src/Module.purs
+++ new/src/Module.purs
@@ -6,8 +6,8 @@ import Affjax.ResponseFormat (string)
 import Data.Either (Either(..))
 import Data.HTTP.Method (Method(..))
 import Effect (Effect)
-import Effect.Aff (launchAff)
-import Effect.Console (log)
+import Effect.Aff (Aff, launchAff_)
+import Util (error)
 
 
 -- Placeholder for Wrattler integration. Should not end in "/", use "." for local run
@@ -15,25 +15,10 @@ resourceServerUrl :: String
 resourceServerUrl = "."
 
 --
-loadFile :: String -> String -> Effect String
+loadFile :: String -> String -> Aff String
 loadFile folder file = do
    let url = resourceServerUrl <> "/" <> folder <> "/" <> file <> ".fld"
-   log $ "Opening" <> url
-   _ <- launchAff $ do
-      result <- request (defaultRequest { url = url, method = Left GET, responseFormat = string })
-      case result of
-         Left err -> do
-            pure $ printError err
-         Right response -> pure response.body
-   pure "hello"
-{-
-   xmlhttp.send()
-   if (xmlhttp.status === 200) {
-      text = xmlhttp.responseText
-   }
-   if (text! === undefined) {
-      return assert(false, `${url} could not be loaded.`)
-   } else  {
-      return text!
-   }
--}
+   result <- request (defaultRequest { url = url, method = Left GET, responseFormat = string })
+   case result of
+      Left err -> error $ printError err
+      Right response -> pure response.body
--- new/test/Main.purs
+++ new/test/Main.purs
@@ -4,14 +4,13 @@ import Prelude
 import Data.Either (Either(..))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Node.Encoding (Encoding(..))
-import Node.FS.Sync (readTextFile)
 import Text.Parsing.Parser (runParser)
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
 import Eval (eval)
+import Module (loadFile)
 import Parse (program)
 import Pretty (pretty)
 import Util (error)
@@ -23,14 +22,15 @@ main = do
 
 test_normalise :: Effect Unit
 test_normalise = do
-   text <- readTextFile ASCII "fluid/example/normalise.fld"
-   let result = runParser text program
-   launchAff_ $ runSpec [consoleReporter] do
-      describe "Parse" do
-         it "blah" do
-            case result of
-               Left parseError -> do
-                  error $ show parseError
-               Right e -> do
-                  let { u } = (eval primitives e).v
-                  (show $ pretty u) `shouldEqual` "5"
+   launchAff_ do
+      text <- loadFile "fluid/example" "normalise.fld"
+      let result = runParser text program
+      runSpec [consoleReporter] do
+         describe "Parse" do
+            it "blah" do
+               case result of
+                  Left parseError -> do
+                     error $ show parseError
+                  Right e -> do
+                     let { u } = (eval primitives e).v
+                     (show $ pretty u) `shouldEqual` "5"
