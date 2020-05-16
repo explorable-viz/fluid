3b3c92dd7e8aff377a106b7ddb5b3e3b5482ab7e
Roly Perera <rperera@turing.ac.uk>
2020-05-16 13:13:35 +0100

    Hardcode dev server as experiment..

--- new/src/Module.purs
+++ new/src/Module.purs
@@ -12,11 +12,11 @@ import Util (error)
 
 -- Placeholder for Wrattler integration. Should not end in "/", use "." for local run
 resourceServerUrl :: String
-resourceServerUrl = "."
+resourceServerUrl = "http://127.0.0.1:1234"
 
 loadFile :: String -> String -> Aff String
 loadFile folder file = do
-   let url = folder <> "/" <> file <> ".fld"
+   let url = resourceServerUrl <> "/" <> folder <> "/" <> file <> ".fld"
    let request_ = defaultRequest { url = url, method = Left GET, responseFormat = string }
    trace request_ \_ -> do
       result <- request request_
