6b72a90d69f44ecd06f83938c8d57d6415494168
Roly Perera <rperera@turing.ac.uk>
2020-05-19 19:23:51 +0100

    Compiling Mocha running. Trick I was missing was liftEffect.

--- new/test/Main.purs
+++ new/test/Main.purs
@@ -4,12 +4,11 @@ import Prelude
 import Data.Either (Either(..))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
 import Text.Parsing.Parser (runParser)
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
-import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Mocha (runMocha)
-import Test.Spec.Runner (runSpec)
 import Eval (eval)
 import Module (loadFile)
 import Parse (program)
@@ -21,9 +20,8 @@ main :: Effect Unit
 main = do
    test_normalise
 
-main2 :: Effect Unit
-main2 = runMocha do
-   let text = "source code"
+main2 :: String -> Effect Unit
+main2 text = runMocha do
    let result = runParser text program
    describe "Parse" do
       it "blah" do
@@ -38,13 +36,4 @@ test_normalise :: Effect Unit
 test_normalise =
    launchAff_ do
       text <- loadFile "fluid/example" "normalise"
-      let result = runParser text program
-      runSpec [consoleReporter] do
-         describe "Parse" do
-            it "blah" do
-               case result of
-                  Left parseError -> do
-                     error $ show parseError
-                  Right e -> do
-                     let { u } = (eval primitives e).v
-                     (show $ pretty u) `shouldEqual` "(492, 984)"
+      liftEffect $ main2 text
