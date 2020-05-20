b9389bf92515eaa64df3e93a3a59bfaa03deb58f
Roly Perera <rperera@turing.ac.uk>
2020-05-20 12:35:33 +0100

    Check test fails correctly...

--- new/test/Main.purs
+++ new/test/Main.purs
@@ -1,13 +1,18 @@
 module Test.Main where
 
 import Prelude
+import Data.Either (Either(..))
 import Effect (Effect)
 import Test.Spec (before, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Mocha (runMocha)
 import Text.Parsing.Parser (runParser)
+import Eval (eval)
 import Module (loadFile)
 import Parse (program)
+import Pretty (pretty)
+import Util (error)
+import Val (primitives)
 
 
 main :: Effect Unit
@@ -16,7 +21,12 @@ main = runMocha $
       describe "feature" $
          it "works" $ \text -> do
             let result = runParser text program
-            (2 + 3) `shouldEqual` 5
+            case result of
+               Left parseError -> do
+                  error $ show parseError
+               Right e -> do
+                  let { u } = (eval primitives e).v
+                  (show $ pretty u) `shouldEqual` "(492, 983)"
 
 {-
 test_normalise :: Effect Unit
