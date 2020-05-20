2bd6a90c79e7e260f01c081923eb5cbc85e0d516
Roly Perera <rperera@turing.ac.uk>
2020-05-20 12:28:42 +0100

    Progress.

--- new/test/Main.purs
+++ new/test/Main.purs
@@ -2,24 +2,20 @@ module Test.Main where
 
 import Prelude
 import Effect (Effect)
-import Effect.Aff (Milliseconds(..), delay, launchAff_)
-import Effect.Class (liftEffect)
 import Test.Spec (Spec, before, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Mocha (runMocha)
 import Module (loadFile)
 
 
-main2 :: Effect Unit
-main2 = launchAff_ do
-   _ <- delay $ Milliseconds 10.0
-   liftEffect $ runMocha $
-      describe "feature" $
-         it "works" $
-            (2 + 3) `shouldEqual` 5
+main :: Effect Unit
+main = runMocha $
+   describe "feature" $
+      it "works" $
+         (2 + 3) `shouldEqual` 5
 
-main :: Spec Unit
-main =
+test :: Spec Unit
+test =
    before (loadFile "fluid/example" "normalise") do
       describe "feature" $
          it "works" $ \text ->
