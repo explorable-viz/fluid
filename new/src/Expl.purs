6b2f003131d2dad6c228951f6109e0365a3ee1b1
Roly Perera <rperera@turing.ac.uk>
2020-05-23 11:45:20 +0100

    Experiment with 'continuation-indexed' elim and expr forms.

--- new/src/Expl.purs
+++ new/src/Expl.purs
@@ -2,7 +2,7 @@ module Expl where
 
 import Prelude
 import Bindings (Var)
-import Expr (Elim)
+import Expr (Elim, Elim2)
 import Val (Env)
 
 data Expl =
@@ -31,5 +31,13 @@ data Match =
    | MatchPair Var Var
    | MatchNil
    | MatchCons Var Var
+
+data Match2 k =
+     MatchVar2 Var
+   | MatchTrue2 k
+   | MatchFalse2 k
+   | MatchPair2 (Match2 (Match2 k))
+   | MatchNil2 (Elim2 (Elim2 k))
+   | MatchCons2 k (Match2 (Match2 k))
 
 derive instance eqMatch :: Eq Match
--- new/src/Expr.purs
+++ new/src/Expr.purs
@@ -21,7 +21,22 @@ data RawExpr =
   | App Expr Expr
   | BinaryApp Expr Var Expr
 
+data RawExpr2 k =
+    Int2 Int
+  | Var2 Var
+  | True2 | False2
+  | Pair2 (Expr2 k) (Expr2 k)
+  | Nil2 | Cons2 (Expr2 k) (Expr2 k)
+  | Op2 Var
+  | Let2 Var (Expr2 k) (Expr2 k)
+  | Match2 (Expr2 k) (Elim2 (Expr2 k))
+  | Letrec2 String (Elim2 (Expr2 k)) (Expr2 k)
+  | App2 (Expr2 k) (Expr2 k)
+  | BinaryApp2 (Expr2 k) Var (Expr2 k)
+
 type Expr = { α :: Selected, r :: RawExpr }
+
+type Expr2 k = { α :: Selected, r :: RawExpr2 k }
 
 expr :: RawExpr -> Expr
 expr r = { α: Bot, r }
@@ -31,6 +46,12 @@ data Elim =
    | ElimPair { x :: Var, y :: Var, e:: Expr }
    | ElimList { nil :: Expr, cons :: { x :: Var, y :: Var, e:: Expr } }
    | ElimBool { true :: Expr, false :: Expr }
+
+data Elim2 k =
+     ElimVar2 { x :: Var, κ :: k }
+   | ElimPair2 { σ :: Elim2 (Elim2 k) }
+   | ElimList2 { nil :: k, cons :: Elim2 (Elim2 k) }
+   | ElimBool2 { true :: k, false :: k }
 
 derive instance eqT3 :: (Eq a, Eq b, Eq c) => Eq (T3 a b c)
 instance showT3 :: (Show a, Show b, Show c) => Show (T3 a b c) where
