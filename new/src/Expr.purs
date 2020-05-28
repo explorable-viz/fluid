dd71fa8cc2ada1008027d79c84a3c7618e6ced75
Roly Perera <rperera@turing.ac.uk>
2020-05-28 15:11:32 +0100

    Extract file.

--- new/src/Expr.purs
+++ new/src/Expr.purs
@@ -1,12 +1,9 @@
 module Expr where
 
 import Prelude hiding (join)
-import Data.List (List, (:))
-import Data.List (List(..)) as L
-import Data.Maybe (Maybe(..))
+import Data.List (List)
 import Bindings (Var)
 import Selected (Selected(..))
-import Util (error)
 
 data T3 a b c = T3 a b c
 
@@ -44,92 +41,3 @@ instance elimFunctor :: Functor Elim where
    map f (ElimBool { true: κ, false: κ' }) = ElimBool { true: f κ, false: f κ' }
    map f (ElimPair σ) = ElimPair $ map (map f) σ
    map f (ElimList { nil: κ, cons: σ }) = ElimList { nil: f κ, cons: map (map f) σ }
-
--- A "partial" eliminator. A convenience for the parser, which must assemble eliminators out of these.
-data PElim k =
-   PElimVar Var k |
-   PElimTrue k |
-   PElimFalse k |
-   PElimBool { true :: k, false :: k } |
-   PElimPair (PElim (PElim k)) |
-   PElimNil k |
-   PElimCons (PElim (PElim k)) |
-   PElimList { nil :: k, cons :: PElim (PElim k) }
-
-instance pElimFunctor :: Functor PElim where
-   map f (PElimVar x κ) = PElimVar x (f κ)
-   map f (PElimTrue κ) = PElimTrue (f κ)
-   map f (PElimFalse κ) = PElimFalse (f κ)
-   map f (PElimBool { true: κ, false: κ' }) = PElimBool { true: f κ, false: f κ' }
-   map f (PElimPair σ) = PElimPair $ map (map f) σ
-   map f (PElimNil κ) = PElimNil (f κ)
-   map f (PElimCons σ) = PElimCons $ map (map f) σ
-   map f (PElimList { nil: κ, cons: σ }) = PElimList { nil: f κ, cons: map (map f) σ }
-
-class Joinable k where
-   join :: List k -> Maybe k
-
-instance pElimJoinable :: Joinable k => Joinable (PElim k) where
-   join L.Nil = Nothing
-   join (b : L.Nil) = Just b
-   join (PElimVar x κ : PElimVar x' κ' : bs) =
-      if x == x'
-      then do
-         κ'' <- join (κ : κ' : L.Nil)
-         join $ PElimVar x κ'' : bs
-      else Nothing
-   join (PElimTrue κ : PElimTrue κ' : bs) = do
-      κ'' <- join (κ : κ' : L.Nil)
-      join $ PElimTrue κ'' : bs
-   join (PElimFalse κ : PElimFalse κ' : bs) = do
-      κ'' <- join (κ : κ' : L.Nil)
-      join $ PElimTrue κ'' : bs
-   join (PElimBool { true: κ1, false: κ2 } : PElimTrue κ1' : bs) = do
-      κ1'' <- join (κ1 : κ1' : L.Nil)
-      join $ PElimBool { true: κ1'', false: κ2 } : bs
-   join (PElimBool { true: κ1, false: κ2 } : PElimFalse κ2' : bs) = do
-      κ2'' <- join (κ2 : κ2' : L.Nil)
-      join $ PElimBool { true: κ1, false: κ2'' } : bs
-   join (PElimBool { true: κ1, false: κ2 } : PElimBool { true: κ1', false: κ2' } : bs) = do
-      κ1'' <- join (κ1 : κ1' : L.Nil)
-      κ2'' <- join (κ2 : κ2' : L.Nil)
-      join $ PElimBool { true: κ1'', false: κ2'' } : bs
-   join (PElimPair σ : PElimPair σ' : bs) = do
-      σ'' <- join (σ : σ' : L.Nil)
-      join $ PElimPair σ'' : bs
-   join (PElimNil κ : PElimNil κ' : bs) = do
-      κ'' <- join (κ : κ' : L.Nil)
-      join $ PElimNil κ'' : bs
-   join (PElimCons σ : PElimCons σ' : bs) = do
-      σ'' <- join (σ : σ' : L.Nil)
-      join $ PElimCons σ'' : bs
-   join (PElimList { nil: κ, cons: σ } : PElimList { nil: κ', cons: σ' } : bs) = do
-      κ'' <- join (κ : κ' : L.Nil)
-      σ'' <- join (σ : σ' : L.Nil)
-      join $ PElimList { nil: κ'', cons: σ'' } : bs
-   join _ = Nothing
-
-toElim :: forall k . PElim k -> Maybe (Elim k)
-toElim (PElimVar x κ) = Just $ ElimVar x κ
-toElim (PElimBool { true: κ, false: κ' }) =
-   Just $ ElimBool { true: κ, false: κ' }
-toElim (PElimPair σ) = do
-   σ' <- hoistMaybe (toElim <$> σ) >>= toElim
-   Just $ ElimPair σ'
-toElim (PElimList { nil: κ, cons: σ }) = do
-   σ' <- hoistMaybe (toElim <$> σ) >>= toElim
-   Just $ ElimList { nil: κ, cons: σ' }
-toElim _ = Nothing
-
-hoistMaybe :: forall k . PElim (Maybe k) -> Maybe (PElim k)
-hoistMaybe (PElimVar x (Just κ)) = Just $ PElimVar x κ
-hoistMaybe (PElimTrue (Just κ)) = Just $ PElimTrue κ
-hoistMaybe (PElimFalse (Just κ)) = Just $ PElimFalse κ
-hoistMaybe (PElimBool { true: Just κ, false: Just κ' }) = Just $ PElimBool { true: κ, false: κ' }
-hoistMaybe (PElimPair σ) = hoistMaybe (hoistMaybe <$> σ) >>= Just <<< PElimPair
-hoistMaybe (PElimNil (Just κ)) = Just $ PElimNil κ
-hoistMaybe (PElimCons σ) = hoistMaybe (hoistMaybe <$> σ) >>= Just <<< PElimCons
-hoistMaybe (PElimList { nil: Just κ, cons: σ }) = do
-   σ' <- hoistMaybe (hoistMaybe <$> σ)
-   pure $ PElimList { nil: κ, cons: σ' }
-hoistMaybe _ = Nothing
---
+++ new/src/PElim.purs
@@ -0,0 +1,98 @@
+module PElim where
+
+import Prelude hiding (join)
+import Data.List (List(..), (:))
+import Data.Maybe (Maybe(..))
+import Bindings (Var)
+import Expr (Elim(..))
+
+
+-- A "partial" eliminator. A convenience for the parser, which must assemble eliminators out of these.
+data PElim k =
+   PElimVar Var k |
+   PElimTrue k |
+   PElimFalse k |
+   PElimBool { true :: k, false :: k } |
+   PElimPair (PElim (PElim k)) |
+   PElimNil k |
+   PElimCons (PElim (PElim k)) |
+   PElimList { nil :: k, cons :: PElim (PElim k) }
+
+instance pElimFunctor :: Functor PElim where
+   map f (PElimVar x κ) = PElimVar x (f κ)
+   map f (PElimTrue κ) = PElimTrue (f κ)
+   map f (PElimFalse κ) = PElimFalse (f κ)
+   map f (PElimBool { true: κ, false: κ' }) = PElimBool { true: f κ, false: f κ' }
+   map f (PElimPair σ) = PElimPair $ map (map f) σ
+   map f (PElimNil κ) = PElimNil (f κ)
+   map f (PElimCons σ) = PElimCons $ map (map f) σ
+   map f (PElimList { nil: κ, cons: σ }) = PElimList { nil: f κ, cons: map (map f) σ }
+
+class Joinable k where
+   join :: List k -> Maybe k
+
+-- This will simplify into a more generic style once we reinstate arbitrary data types.
+instance pElimJoinable :: Joinable k => Joinable (PElim k) where
+   join Nil = Nothing
+   join (b : Nil) = Just b
+   join (PElimVar x κ : PElimVar x' κ' : bs) =
+      if x == x'
+      then do
+         κ'' <- join (κ : κ' : Nil)
+         join $ PElimVar x κ'' : bs
+      else Nothing
+   join (PElimTrue κ : PElimTrue κ' : bs) = do
+      κ'' <- join (κ : κ' : Nil)
+      join $ PElimTrue κ'' : bs
+   join (PElimFalse κ : PElimFalse κ' : bs) = do
+      κ'' <- join (κ : κ' : Nil)
+      join $ PElimTrue κ'' : bs
+   join (PElimBool { true: κ1, false: κ2 } : PElimTrue κ1' : bs) = do
+      κ1'' <- join (κ1 : κ1' : Nil)
+      join $ PElimBool { true: κ1'', false: κ2 } : bs
+   join (PElimBool { true: κ1, false: κ2 } : PElimFalse κ2' : bs) = do
+      κ2'' <- join (κ2 : κ2' : Nil)
+      join $ PElimBool { true: κ1, false: κ2'' } : bs
+   join (PElimBool { true: κ1, false: κ2 } : PElimBool { true: κ1', false: κ2' } : bs) = do
+      κ1'' <- join (κ1 : κ1' : Nil)
+      κ2'' <- join (κ2 : κ2' : Nil)
+      join $ PElimBool { true: κ1'', false: κ2'' } : bs
+   join (PElimPair σ : PElimPair σ' : bs) = do
+      σ'' <- join (σ : σ' : Nil)
+      join $ PElimPair σ'' : bs
+   join (PElimNil κ : PElimNil κ' : bs) = do
+      κ'' <- join (κ : κ' : Nil)
+      join $ PElimNil κ'' : bs
+   join (PElimCons σ : PElimCons σ' : bs) = do
+      σ'' <- join (σ : σ' : Nil)
+      join $ PElimCons σ'' : bs
+   join (PElimList { nil: κ, cons: σ } : PElimList { nil: κ', cons: σ' } : bs) = do
+      κ'' <- join (κ : κ' : Nil)
+      σ'' <- join (σ : σ' : Nil)
+      join $ PElimList { nil: κ'', cons: σ'' } : bs
+   join _ = Nothing
+
+toElim :: forall k . PElim k -> Maybe (Elim k)
+toElim (PElimVar x κ) = Just $ ElimVar x κ
+toElim (PElimBool { true: κ, false: κ' }) =
+   Just $ ElimBool { true: κ, false: κ' }
+toElim (PElimPair σ) = do
+   σ' <- hoistMaybe (toElim <$> σ) >>= toElim
+   Just $ ElimPair σ'
+toElim (PElimList { nil: κ, cons: σ }) = do
+   σ' <- hoistMaybe (toElim <$> σ) >>= toElim
+   Just $ ElimList { nil: κ, cons: σ' }
+toElim _ = Nothing
+
+hoistMaybe :: forall k . PElim (Maybe k) -> Maybe (PElim k)
+hoistMaybe (PElimVar x (Just κ)) = Just $ PElimVar x κ
+hoistMaybe (PElimTrue (Just κ)) = Just $ PElimTrue κ
+hoistMaybe (PElimFalse (Just κ)) = Just $ PElimFalse κ
+hoistMaybe (PElimBool { true: Just κ, false: Just κ' }) = Just $ PElimBool { true: κ, false: κ' }
+hoistMaybe (PElimPair σ) = hoistMaybe (hoistMaybe <$> σ) >>= Just <<< PElimPair
+hoistMaybe (PElimNil (Just κ)) = Just $ PElimNil κ
+hoistMaybe (PElimCons σ) = hoistMaybe (hoistMaybe <$> σ) >>= Just <<< PElimCons
+hoistMaybe (PElimList { nil: Just κ, cons: σ }) = do
+   σ' <- hoistMaybe (hoistMaybe <$> σ)
+   pure $ PElimList { nil: κ, cons: σ' }
+hoistMaybe _ = Nothing
