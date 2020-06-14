16e435b1bc7e89ed730dd7f3f1fde8ae70c4f90a
Roly Perera <rperera@turing.ac.uk>
2020-06-14 14:43:09 +0100

    Map.

--- new/test/Main.purs
+++ new/test/Main.purs
@@ -33,6 +33,7 @@ main = do
    runExample "foldr_sumSquares" "661"
    runExample "lexicalScoping" "\"6\""
    runExample "length" "2"
+   runExample "map" "Cons 5 (Cons 7 (Cons 13 (Cons 15 (Cons 4 (Cons 3 (Cons -3 Nil))))))"
    runExample "normalise" "(33, 66)"
 
    runExample "temp" "7"
