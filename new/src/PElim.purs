2980cee21b2b59c5b0c0fd6f7329e5aa111d44bf
Roly Perera <rperera@turing.ac.uk>
2020-06-14 10:39:00 +0100

    Migrate 'join' typeclass to Elim.

--- new/src/PElim.purs
+++ new/src/PElim.purs
@@ -19,44 +19,38 @@ data PElim =
    PElimVar Var PCont |
    PElimConstr (Map Ctr PCont)
 
-class Joinable k where
-   join :: List k -> Maybe k
-
 class Joinable2 k where
    join2 :: k -> k -> Maybe k
-
-instance exprJoinable :: Joinable Expr where
-   join _ = Nothing
 
 instance joinableExpr :: Joinable2 Expr where
    join2 _ _ = Nothing
 
-instance joinableCont :: Joinable2 PCont where
-   join2 PCNone PCNone              = pure $ PCNone
-   join2 (PCExpr e) (PCExpr e')     = PCExpr <$> join2 e e'
-   join2 (PCPElim σ) (PCPElim σ')   = PCPElim <$> join2 σ σ'
-   join2 _ _                        = Nothing
+instance joinableCont :: Joinable2 Cont where
+   join2 CNone CNone          = pure CNone
+   join2 (CExpr e) (CExpr e') = CExpr <$> join2 e e'
+   join2 (CElim σ) (CElim σ') = CElim <$> join2 σ σ'
+   join2 _ _                  = Nothing
 
-instance joinableCtrCont :: Joinable2 (Ctr × PCont) where
+instance joinableCtrCont :: Joinable2 (Ctr × Cont) where
    join2 (Tuple c κ) (Tuple c' κ') = bisequence $ Tuple (c ≟ c') $ join2 κ κ'
 
-instance joinablePElim2 :: Joinable2 PElim where
-   join2 (PElimVar x κ) (PElimVar x' κ')     = PElimVar <$> x ≟ x' <*> join2 κ κ'
-   join2 (PElimConstr κs) (PElimConstr κs')  = PElimConstr <$> (sequence $ unionWithMaybe join2 κs κs')
+instance joinablePElim2 :: Joinable2 Elim where
+   join2 (ElimVar x κ) (ElimVar x' κ')    = ElimVar <$> x ≟ x' <*> join2 κ κ'
+   join2 (ElimConstr κs) (ElimConstr κs') = ElimConstr <$> (sequence $ unionWithMaybe join2 κs κs')
    join2 _ _ = Nothing
 
 joinAll :: forall a . Joinable2 a => List a -> Maybe a
 joinAll Nil = error "Non-empty list expected"
 joinAll (x : xs) = foldl (om join2) (Just x) xs
 
-toCont :: PCont -> Maybe Cont
-toCont PCNone      = pure CNone
-toCont (PCExpr e)  = CExpr <$> pure e
-toCont (PCPElim σ) = CElim <$> toElim σ
+toCont :: Cont -> Maybe Cont
+toCont CNone      = pure CNone
+toCont (CExpr e)  = CExpr <$> pure e
+toCont (CElim σ) = CElim <$> toElim σ
 
-toElim :: PElim -> Maybe Elim
-toElim (PElimVar x κ)   = ElimVar x <$> toCont κ
-toElim (PElimConstr κs) = ElimConstr <$> sequence (toCont <$> κs)
+toElim :: Elim -> Maybe Elim
+toElim (ElimVar x κ)   = ElimVar x <$> toCont κ
+toElim (ElimConstr κs) = ElimConstr <$> sequence (toCont <$> κs)
 
 class MapCont a where
    mapCont :: Cont -> a -> Maybe a
