71ffcc9c3256f4adc0bb806be6c6f14511029cba
Roly Perera <rperera@turing.ac.uk>
2020-06-19 15:18:56 +0100

    Tidyup.

--- new/src/Parse.purs
+++ new/src/Parse.purs
@@ -285,4 +285,4 @@ module_ :: SParser Module
 module_ = Module <$> (topLevel $ concat <$> many defs)
    where
       defs :: SParser (List (Either Def RecDefs))
-      defs = (bisequence <$> choose (try $ toList <$> letDefs expr_) (pure <$> recDefs expr_))
+      defs = (bisequence <$> choose (toList <$> try (letDefs expr_)) (pure <$> recDefs expr_))
