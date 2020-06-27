b0e6f1529c91aa7ea31b4f3d0c65eba30244ca13
Roly Perera <rperera@turing.ac.uk>
2020-06-27 10:14:45 +0100

    Reinstate prelude.

--- new/src/Module.purs
+++ new/src/Module.purs
@@ -46,5 +46,5 @@ successfulParse src p =
 
 parseWithImports :: String -> Aff (Env × Expr)
 parseWithImports src = do
-   (×) <$> pure primitives -- loadModule "prelude" primitives
+   (×) <$> loadModule "prelude" primitives
        <@> successfulParse src program
