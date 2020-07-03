5e234e9fc2a76f1110bf6d00ed128ca60dfa7a1b
Roly Perera <rperera@turing.ac.uk>
2020-07-03 08:06:14 +0100

    Garbage collect Bindings.

--- new/src/Bindings.purs
+++ new/src/Bindings.purs
@@ -1,10 +1,9 @@
 module Bindings where
 
 import Prelude hiding (top, absurd)
-import Data.Either (Either(..), isRight)
 import Data.Maybe (Maybe(..))
 import Lattice (class Lattice, class Selectable, mapα, maybeZipWithα)
-import Util (MayFail, type (×), (×), (≟), absurd, error, report)
+import Util (MayFail, (≟), report)
 
 type Var = String
 
@@ -15,7 +14,6 @@ data Bindings a = Empty | Extend (Bindings a) (Bind a)
 
 infix 6 Bind as ↦
 infixl 5 Extend as :+:
-infixl 5 append as :++:
 infixl 5 update as ◃
 
 instance bindingsSemigroup :: Semigroup (Bindings a) where
@@ -42,31 +40,8 @@ find :: forall a . Var -> Bindings a -> MayFail a
 find x' Empty          = report $ "variable " <> x' <> " not found"
 find x' (xs :+: x ↦ v) = if x == x' then pure v else find x' xs
 
-remove :: forall a . Var -> Bindings a -> MayFail (a × Bindings a)
-remove x' ρ = go ρ Empty
-   where go Empty          acc = Left $ "variable " <> x' <> " not found"
-         go (ρ' :+: x ↦ v) acc = if x == x' then pure $ v × (ρ' <> acc)
-                                 else go ρ' (acc :+: x ↦ v)
-
 update :: forall a . Lattice a => Bindings a -> Bind a -> Bindings a
 update Empty _ = Empty
 update (xs :+: x ↦ v) (x' ↦ v')
    | x == x'    = xs :+: x' ↦ v'
    | otherwise  = (update xs (x' ↦ v')) :+: x ↦ v
-
-elem :: forall a . Var -> Bindings a -> Boolean
-elem x ρ = isRight $ find x ρ
-
-reverse :: forall a. Bindings a -> Bindings a
-reverse = go Empty
-  where
-  go acc Empty = acc
-  go acc (xs :+: x) = go (acc :+: x) xs
-
-length :: forall a. Bindings a -> Int
-length (ρ :+: x ↦ v) = 1 + length ρ
-length Empty = 0
-
-head :: forall a. Bindings a -> Bind a
-head (xs :+: x ↦ v) = (x ↦ v)
-head Empty = error absurd
