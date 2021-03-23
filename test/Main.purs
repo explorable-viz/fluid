bbef46969c30c107d6b98b8157e1c7112b5fafe3
min-nguyen <minhnguyen1995@googlemail.com>
2021-03-23 13:16:44 +0000

    slightly closer to making convolution work

--- test/Main.purs
+++ test/Main.purs
@@ -48,23 +48,24 @@ test_desugaring = [
 
 test_misc :: Array (Test Unit)
 test_misc = [
-   test "range" "[(0, 0), (0, 1), (1, 0), (1, 1)]",
-   test "arithmetic" "42",
-   test "array" "(1, (3, 3))",
-   test "compose" "5",
-   test "factorial" "40320",
-   test "filter" "[8, 7]",
-   test "flatten" "[(3, \"simon\"), (4, \"john\"), (6, \"sarah\"), (7, \"claire\")]",
-   test "foldr_sumSquares" "661",
-   test "lexicalScoping" "\"6\"",
-   test "length" "2",
-   test "lookup" "Some \"sarah\"",
-   test "map" "[5, 7, 13, 15, 4, 3, -3]",
-   test "mergeSort" "[1, 2, 3]",
-   test "normalise" "(33, 66)",
-   test "pattern-match" "4",
-   test "reverse" "[2, 1]",
-   test "zipWith" "[[10], [12], [20]]"
+   test "convolution" "[1, 2]"
+   -- test "range" "[(0, 0), (0, 1), (1, 0), (1, 1)]",
+   -- test "arithmetic" "42",
+   -- test "array" "(1, (3, 3))",
+   -- test "compose" "5",
+   -- test "factorial" "40320",
+   -- test "filter" "[8, 7]",
+   -- test "flatten" "[(3, \"simon\"), (4, \"john\"), (6, \"sarah\"), (7, \"claire\")]",
+   -- test "foldr_sumSquares" "661",
+   -- test "lexicalScoping" "\"6\"",
+   -- test "length" "2",
+   -- test "lookup" "Some \"sarah\"",
+   -- test "map" "[5, 7, 13, 15, 4, 3, -3]",
+   -- test "mergeSort" "[1, 2, 3]",
+   -- test "normalise" "(33, 66)",
+   -- test "pattern-match" "4",
+   -- test "reverse" "[2, 1]",
+   -- test "zipWith" "[[10], [12], [20]]"
 ]
 
 test_graphics :: Array (Test Unit)
