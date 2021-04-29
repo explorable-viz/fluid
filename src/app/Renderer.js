e4047020a7372be92cc3aeb0afd9eac3ebd18c6c
Roly Perera <roly.perera@dynamicaspects.org>
2021-04-29 18:26:57 +0100

    Effectful foreign functions need to have thunked bodies, but unfortunately that isn't in the main PS documentation.

--- src/app/Renderer.js
+++ src/app/Renderer.js
@@ -3,29 +3,30 @@
 const d3 = require("d3")
 
 function drawMatrix (el, i, j) {
-   const w = 30, h = 30, gap = 1.15
-   const data = Array(i).fill(d3.range(j))
-   const grp = el.selectAll('g')
-      .data(data)
-      .enter()
-      .append('g')
-      .attr('transform', (d, i) => 'translate(0, ' + h * gap * i + ')')
+   return () => {
+      const w = 30, h = 30, gap = 1.15
+      const data = Array(i).fill(d3.range(j))
+      const grp = el.selectAll('g')
+         .data(data)
+         .enter()
+         .append('g')
+         .attr('transform', (d, i) => 'translate(0, ' + h * gap * i + ')')
 
-   grp.selectAll('rect')
-      .data(d => d)
-      .enter()
-      .append('rect')
-         .attr('x', (d, j) => w * gap * j)
-         .attr('width', w)
-         .attr('height', h)
+      grp.selectAll('rect')
+         .data(d => d)
+         .enter()
+         .append('rect')
+            .attr('x', (d, j) => w * gap * j)
+            .attr('width', w)
+            .attr('height', h)
+   }
 }
 
 const div = d3.select('#app-root'),
       svg = div.append('svg')
-            .attr('width', 200)
-            .attr('height', 200)
-            .attr('fill', 'green')
-drawMatrix(svg, 5, 5)
+               .attr('width', 200)
+               .attr('height', 200)
+               .attr('fill', 'green')
 
 function curry2 (f) {
    return x => y => f(x, y)
