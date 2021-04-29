e5e47efd0e0ef3725ca8a9502b05f1d718d4144c
Roly Perera <roly.perera@dynamicaspects.org>
2021-04-29 17:39:13 +0100

    drawMatrix.js -> Renderer.js. Use Data.Function.Uncurried to handle multiple arguments.

--- src/app/Renderer.purs
+++ src/app/Renderer.purs
@@ -1,9 +1,12 @@
 module App.Renderer where
 
 import Prelude
+import Data.Function.Uncurried (Fn2)
 import Lattice (𝔹)
 import Util ((×))
 import Val (MatrixRep)
+
+foreign import drawMatrix :: Fn2 Number Number Unit
 
 renderMatrix :: MatrixRep 𝔹 -> Unit
 renderMatrix (vss × (i × _) × (j × _)) = unit
