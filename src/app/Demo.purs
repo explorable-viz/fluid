ca0e8952578c152b884a9aafdf826e2c5c4b75e0
Roly Perera <roly.perera@dynamicaspects.org>
2021-04-30 17:39:54 +0100

    Do enough slicing to valid matrix cell rendering.

--- src/app/Demo.purs
+++ src/app/Demo.purs
@@ -7,10 +7,10 @@ import Effect.Aff (runAff_)
 import Effect.Console (log)
 import App.Renderer (renderMatrix)
 import Module (openWithDefaultImports)
-import Pretty (prettyP)
 import Primitive (match)
-import Test.Util (desugarEval)
+import Test.Util (desugarEval, desugarEval_bwd, desugarEval_fwd)
 import Util ((×))
+import Val (Val(..), holeMatrix, insertMatrix)
 
 main :: Effect Unit
 main =
@@ -19,6 +19,8 @@ main =
       Left e -> log ("Open failed: " <> show e)
       Right (ρ × s) -> case desugarEval ρ s of
          Left msg -> log ("Execution failed: " <> msg)
-         Right (_ × v) -> do
-            log (prettyP v)
-            renderMatrix (match v)
+         Right (t × _) -> do
+            let v = Matrix true (insertMatrix 2 2 (Hole true) (holeMatrix 5 5))
+                ρ' × s' = desugarEval_bwd (t × s) v
+                v' = desugarEval_fwd ρ' s' t
+            renderMatrix (match v')
