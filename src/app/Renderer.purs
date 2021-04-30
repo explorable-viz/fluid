92fd0524071f2cae8ab7e6818d0d2a15bfec6943
Roly Perera <roly.perera@dynamicaspects.org>
2021-04-30 07:58:41 +0100

    Plug drawMatrix into renderMatrix.

--- src/app/Renderer.purs
+++ src/app/Renderer.purs
@@ -3,10 +3,10 @@ module App.Renderer where
 import Prelude
 import Effect (Effect)
 import Lattice (𝔹)
-import Util ((×))
+import Util (type (×), (×))
 import Val (MatrixRep)
 
 foreign import drawMatrix :: Int -> Int -> Effect Unit
 
-renderMatrix :: MatrixRep 𝔹 -> Effect Unit
-renderMatrix (vss × (i × _) × (j × _)) = pure unit
+renderMatrix :: MatrixRep 𝔹 × 𝔹 -> Effect Unit
+renderMatrix (_ × (i × _) × (j × _) × _) = drawMatrix i j
