b173d6e2105db19e074d604c939868b4e4e360b4
Roly Perera <roly.perera@dynamicaspects.org>
2021-06-05 10:53:48 +0100

    First pass over records.

--- test/Main.purs
+++ test/Main.purs
@@ -26,7 +26,7 @@ hole = Hole false
 
 test_scratchpad :: Array (Test Unit)
 test_scratchpad = [
-   test "records" "{a: 5, b: 6, c: 7}"
+   test "records" "{a: 5, b: 6, c: 7, d: [5]}"
 ]
 
 test_linking :: Array (Test Unit)
