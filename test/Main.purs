a7a96da89f7c940d51cb9de4710d1223fa34d563
Roly Perera <roly.perera@dynamicaspects.org>
2021-06-06 14:29:05 +0100

    Port renewables dataset to records.

--- test/Main.purs
+++ test/Main.purs
@@ -11,8 +11,8 @@ import Test.Util (Test, run, test, testBwd, testLink, testWithDataset)
 import Val (Val(..), holeMatrix, insertMatrix)
 
 tests :: Array (Array (Test Unit))
-tests = [ test_desugaring, test_misc, test_bwd, test_linking, test_graphics ]
---tests = [ test_scratchpad ]
+-- tests = [ test_desugaring, test_misc, test_bwd, test_linking, test_graphics ]
+tests = [ test_scratchpad ]
 
 main :: Effect Unit
 main = void (sequence (run <$> concat tests))
