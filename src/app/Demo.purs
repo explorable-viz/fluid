77c70369ffedbeabf9809908556632f8b6295308
Roly Perera <roly.perera@dynamicaspects.org>
2021-06-16 12:59:16 +0100

    fig2 no longer needs a NeededBySpec, but constructs one using the demand on the data returned by the needed analysis.

--- src/app/Demo.purs
+++ src/app/Demo.purs
@@ -139,13 +139,14 @@ fig divId { file, makeSubfigs } = do
    let _ × subfigs = successful (makeSubfigs { ρ0, ρ: ρ <> ρ1, s: s1 })
    pure [ { divId , subfigs } ]
 
-fig2 :: String -> NeededSpec -> NeededBySpec -> File -> File -> Aff (Array Fig)
-fig2 divId spec1 spec2 file1 file2 = do
+fig2 :: String -> File -> File -> NeededSpec -> MakeSubFig -> Aff (Array Fig)
+fig2 divId file1 file2 spec1 o_fig = do
    ρ0 × ρ <- openDatasetAs (File "example/linking/renewables") "data"
    { ρ: ρ1, s: s1 } <- (successful <<< splitDefs (ρ0 <> ρ)) <$> open file1
    { ρ: ρ2, s: s2 } <- (successful <<< splitDefs (ρ0 <> ρ)) <$> open file2
    let { ρ0', ρ': ρρ1' } × subfigs1 = successful (needed spec1 { ρ0, ρ: ρ <> ρ1, s: s1 })
-       _ × subfigs2 = successful (neededBy spec2 { ρ0, ρ: ρρ1', s: s2 })
+       ρ' × _ = splitAt 1 ρρ1' -- data selection
+       _ × subfigs2 = successful (neededBy { vars: [], o_fig, ρ' } { ρ0, ρ: ρ <> ρ2, s: s2 })
    pure [ { divId, subfigs: subfigs1 <> subfigs2 } ]
 
 convolutionFigs :: Partial => Aff (Array Fig)
