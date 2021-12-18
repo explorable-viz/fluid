21c8521404b0d0f75abca95dc0e6c74ac0b14b0f
Roly Perera <roly.perera@dynamicaspects.org>
2021-12-18 12:05:56 +0000

    🧩 [modify-unused]: App test names use divId of fig.

--- test/app/Main.purs
+++ test/app/Main.purs
@@ -3,22 +3,26 @@ module Test.App.Main where
 import Prelude
 import Data.Traversable (sequence)
 import Effect (Effect)
-import Effect.Aff (Aff)
 import Partial.Unsafe (unsafePartial)
 import Test.Spec (before, it)
-import App.Demo (fig, fig1, figConv1, linkingFig)
-import App.Renderer (Fig)
+import App.Demo (FigSpec, LinkingFigSpec, fig, fig1, figConv1, linkingFig)
 import Test.Util (Test, run)
 
 -- For now app tests just exercise figure creation code.
-test_fig :: Aff Fig -> Test Unit
-test_fig setup =
-   before setup $
-      it "hello" \_ ->
+test_fig :: FigSpec -> Test Unit
+test_fig spec =
+   before (fig spec) $
+      it spec.divId \_ ->
+         pure unit
+
+test_linkingFig :: Partial => LinkingFigSpec -> Test Unit
+test_linkingFig spec =
+   before (linkingFig spec) $
+      it spec.divId \_ ->
          pure unit
 
 tests :: Array (Test Unit)
-tests = unsafePartial [test_fig (fig figConv1), test_fig (linkingFig fig1)]
+tests = unsafePartial [test_fig figConv1, test_linkingFig fig1]
 
 main :: Effect Unit
 main = void (sequence (run <$> tests))
