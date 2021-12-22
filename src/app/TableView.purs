f9f115089e4810e591e6d2d21171816d0153ae88
Roly Perera <roly.perera@dynamicaspects.org>
2021-12-22 11:38:58 +0000

    ❗ [fix]: Can't get local build to pick up this error, although server build from scratch does.

--- src/app/TableView.purs
+++ src/app/TableView.purs
@@ -2,7 +2,6 @@ module App.TableView where
 
 import Prelude
 import Data.Tuple (fst)
-import Effect.Console (log)
 import Bindings (Bindings)
 import App.Util (Handler, Renderer, get_intOrNumber, get_prim)
 import Lattice (Slice, 𝔹)
