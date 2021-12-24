dc3794a1aba72f1f0d3f414ff33724590a8fdbf5
Roly Perera <roly.perera@dynamicaspects.org>
2021-12-24 15:21:31 +0000

    🧩 [consolidate]: Use toggleConstrArg to simplify toggleBar.

--- src/app/BarChart.purs
+++ src/app/BarChart.purs
@@ -1,21 +1,21 @@
 module App.BarChart where
 
 import Prelude hiding (absurd)
-import Data.List (List(..), (:))
 import Data.Maybe (Maybe)
 import Data.Tuple (fst)
 import Unsafe.Coerce (unsafeCoerce)
 import Web.Event.Event (target)
 import Web.Event.EventTarget (EventTarget)
 import App.Util (
-   Handler, class Reflect, Renderer, Selector, from, get, get_intOrNumber, get_prim, record, toggleField, toggleNth
+   Handler, class Reflect, Renderer, Selector,
+   from, get, get_intOrNumber, get_prim, record, toggleConstrArg, toggleField, toggleNth
 )
 import Bindings (Bind)
 import DataType (cBarChart, f_caption, f_data, f_x, f_y)
-import Lattice (𝔹, expand, neg)
-import Util (type (×), (×), (!), absurd, error, fromJust)
+import Lattice (𝔹, neg)
+import Util (type (×), (!), absurd, fromJust)
 import Util.SnocList (SnocList)
-import Val (Val(..))
+import Val (Val)
 
 newtype BarChart = BarChart { caption :: String × 𝔹, data :: Array BarChartRecord }
 newtype BarChartRecord = BarChartRecord { x :: String × 𝔹, y :: Number × 𝔹 }
@@ -35,15 +35,14 @@ instance reflectBarChart :: Reflect (SnocList (Bind (Val Boolean))) BarChart whe
    }
 
 barChartHandler :: Handler
-barChartHandler ev = toggleBar $ unsafeBarIndex (target ev)
+barChartHandler ev = toggleBar $ unsafeBarIndex $ target ev
    where
    toggleBar :: Int -> Selector
-   toggleBar i (u × Constr _ c (v1 : Nil)) | c == cBarChart =
-      case expand u (Constr false c (Hole false : Nil)) of
-         Constr α _ (u1 : Nil) ->
-            Constr α c (toggleField f_data (toggleNth i (fst >>> neg)) (u1 × v1) : Nil)
-         _ -> error absurd
-   toggleBar _ _ = error absurd
+   toggleBar i =
+      toggleConstrArg cBarChart 0 $
+      toggleField f_data $
+      toggleNth i $
+      fst >>> neg
 
    -- [Unsafe] Datum associated with bar chart mouse event; 0-based index of selected bar.
    unsafeBarIndex :: Maybe EventTarget -> Int
