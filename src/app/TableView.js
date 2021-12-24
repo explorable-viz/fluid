b4c5bf3037f6d31ed22718020ceb8906c674dc39
Roly Perera <roly.perera@dynamicaspects.org>
2021-12-24 07:50:04 +0000

    ❗ [modify]: TableView tolerates empty list of records.

--- src/app/TableView.js
+++ src/app/TableView.js
@@ -22,38 +22,43 @@ function drawTable (
       const childId = id + '-' + childIndex
       const cellFill = '#ffffff'
       const div = d3.select('#' + id)
-      div.selectAll('#' + childId).remove()
 
-      const HTMLtable = div
-         .append('table')
-         .attr('id', childId)
+      div.selectAll('#' + childId).remove()
       table = table.filter(r => isUsed(r))
 
-      const colNames = Object.keys(table[0])
-      HTMLtable.append('thead')
-         .append('tr')
-         .selectAll('th')
-         .data(colNames)
-         .enter()
-         .append('th')
-         .text(d => d)
-      const rows = HTMLtable
-         .append('tbody')
-         .selectAll('tr')
-         .data(table)
-         .enter()
-         .append('tr')
-      rows.selectAll('td')
-         .data(d => colNames.map(k => { return { 'value': d[k], 'name': k } }))
-         .enter()
-         .append('td')
-         .attr('data-th', d => d.name)
-         .attr('class', d => d.value.value1 ? 'cell-selected' : null)
-         .attr('bgcolor', d => d.value.value1 ? shared.colorShade(cellFill, -40) : cellFill)
-         .text(d => d.value.value0)
-         .on('mouseover', (e, d) =>
-            listener(e)
-         )
+      if (table.length > 0) {
+         const HTMLtable = div
+            .append('table')
+            .attr('id', childId)
+         const colNames = Object.keys(table[0])
+
+         HTMLtable.append('thead')
+            .append('tr')
+            .selectAll('th')
+            .data(colNames)
+            .enter()
+            .append('th')
+            .text(d => d)
+
+         const rows = HTMLtable
+            .append('tbody')
+            .selectAll('tr')
+            .data(table)
+            .enter()
+            .append('tr')
+
+         rows.selectAll('td')
+            .data(d => colNames.map(k => { return { 'value': d[k], 'name': k } }))
+            .enter()
+            .append('td')
+            .attr('data-th', d => d.name)
+            .attr('class', d => d.value.value1 ? 'cell-selected' : null)
+            .attr('bgcolor', d => d.value.value1 ? shared.colorShade(cellFill, -40) : cellFill)
+            .text(d => d.value.value0)
+            .on('mouseover', (e, d) =>
+               listener(e)
+            )
+      }
    }
 }
 
