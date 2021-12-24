ffaedaced478907c3a83f5eb990243a4681c0a7f
Roly Perera <roly.perera@dynamicaspects.org>
2021-12-24 10:00:27 +0000

    🧩 [add-unused]: plots -> plots.entries() in drawLineChart.

--- src/app/LineChart.js
+++ src/app/LineChart.js
@@ -55,15 +55,15 @@ function drawLineChart (
       const color = d3.scaleOrdinal(d3.schemePastel1)
 
       svg.selectAll('lines')
-         .data(plots)
+         .data([...plots.entries()])
          .enter()
          .append('g')
          .append('path')
          .attr('fill', 'none')
-         .attr('stroke', d => color(names.indexOf(d.name.value0)))
+         .attr('stroke', ([, d]) => color(names.indexOf(d.name.value0)))
          .attr('stroke-width', 1)
          .attr('class', 'line')
-         .attr('d', d => line1(d.data))
+         .attr('d', ([_, d]) => line1(d.data))
 
       const smallRadius = 2
       for (const plot of plots) {
