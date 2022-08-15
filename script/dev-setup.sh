1812136d240298f13c01eea292ad297b4dafeae7
Roly Perera <roly.perera@dynamicaspects.org>
2022-08-15 15:23:08 +0100

    🧩 [add-unused]: Extend dev-setup.sh to install .git/hooks/pre-push.

--- script/dev-setup.sh
+++ script/dev-setup.sh
@@ -3,8 +3,10 @@
 
 # This will install:
 #   - various [Git aliases](/.gitconfig.include) to classify different kinds of commit
+#   - pre-push hook that does a clean build & test
 
 # set -x
 set -e
 
 git config --local include.path "../.gitconfig.include"
+. script/install-hooks.sh
