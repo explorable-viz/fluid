c94c9f61106c708b6d190d0ba6061d6ea1863663
Roly Perera <roly.perera@dynamicaspects.org>
2022-08-15 20:43:51 +0100

    🧩 [modify-unused]: Second pass over 'update'. Not convinced this will be efficient..

--- src/Env2.purs
+++ src/Env2.purs
@@ -1,31 +1,30 @@
 module Env2 where
 
 import Prelude
-import Data.List (List, (:))
+import Data.List (List(..))
+import Data.List.NonEmpty (NonEmptyList(..))
 import Data.Map (insert, pop, toUnfoldable)
+import Data.NonEmpty ((:|))
 import Data.Tuple (uncurry)
 import Bindings2 (Bind(..), Bindings, (↦))
 import Bindings2New (Bindings2)
 import Partial.Unsafe (unsafePartial)
-import Util2 (Endo, type (×), (×), fromJust)
+import Util2 (Endo, (×), fromJust)
 import Util.SnocList2 (SnocList(..), (:-))
 import Val2 (Val)
 
-type Env2 a = Bindings2 (List (Val a))
+type Env2 a = Bindings2 (NonEmptyList (Val a))
 
 -- A "singleton" env is one where every variable is mapped to a singleton list.
 -- Second arg here is a singleton.
 update :: forall a . Env2 a -> Endo (Env2 a)
 update γ γ' = update' γ (uncurry Bind <$> toUnfoldable γ')
 
-update' :: forall a . Env2 a -> Bindings (List (Val a)) -> Env2 a
+update' :: forall a . Env2 a -> Bindings (NonEmptyList (Val a)) -> Env2 a
 update' γ Lin              = γ
-update' γ (γ' :- x ↦ us)  =
-   let vs × γ'' = fromJust (x <> " not found") $ pop x γ :: List (Val a) × Env2 a
-   in unsafePartial $ insert x (replaceHead (fromSingleton us) vs) (γ'' `update'` γ')
+update' γ (γ' :- x ↦ us)   =
+   let NonEmptyList (_ :| vs) × γ'' = fromJust (x <> " not found") $ pop x γ
+   in unsafePartial $ insert x (NonEmptyList $ fromSingleton us :| vs) (γ'' `update'` γ')
 
-replaceHead :: Partial => forall a . a -> Endo (List a)
-replaceHead y (_ : xs) = y : xs
-
-fromSingleton :: Partial => forall a . List a -> a
-fromSingleton (x : _) = x
+fromSingleton :: Partial => forall a . NonEmptyList a -> a
+fromSingleton (NonEmptyList (x :| Nil)) = x
