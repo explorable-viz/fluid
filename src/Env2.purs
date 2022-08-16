b144f520aa49e4213af0d2fc979c15372e14056a
Roly Perera <roly.perera@dynamicaspects.org>
2022-08-16 07:09:23 +0100

    🧩 [refactor]: Better with SingletonEnv.

--- src/Env2.purs
+++ src/Env2.purs
@@ -1,28 +1,24 @@
 module Env2 where
 
 import Prelude
-import Data.List (List(..))
 import Data.List.NonEmpty (NonEmptyList(..))
 import Data.Map (insert, pop, toUnfoldable)
 import Data.NonEmpty ((:|))
 import Data.Tuple (uncurry)
 import Bindings2 (Bind(..), Bindings, (↦))
 import Bindings2New (Bindings2)
-import Partial.Unsafe (unsafePartial)
-import Util2 (Endo, (×), fromJust)
+import Util2 ((×), fromJust)
 import Util.SnocList2 (SnocList(..), (:-))
 import Val2 (Val)
 
 type Env2 a = Bindings2 (NonEmptyList (Val a))
+type SingletonEnv a = Bindings2 (Val a)
 
--- A "singleton" env is one where every variable is mapped to a singleton list.
--- Second arg here is a singleton.
-update :: forall a . Env2 a -> Endo (Env2 a)
+update :: forall a . Env2 a -> SingletonEnv a -> Env2 a
 update γ γ' = update' γ (uncurry Bind <$> toUnfoldable γ')
 
-update' :: forall a . Env2 a -> Bindings (NonEmptyList (Val a)) -> Env2 a
-update' γ Lin                          = γ
-update' γ (γ' :- x ↦ NonEmptyList us)  =
+update' :: forall a . Env2 a -> Bindings (Val a) -> Env2 a
+update' γ Lin              = γ
+update' γ (γ' :- x ↦ v)    =
    let NonEmptyList (_ :| vs) × γ'' = fromJust (x <> " not found") $ pop x γ
-       v = unsafePartial (\(y :| Nil) -> y) us
    in insert x (NonEmptyList $ v :| vs) (update' γ'' γ')
