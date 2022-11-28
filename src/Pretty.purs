2849ccb9e205b700323ee0ae240e165a2824fe45
Roly Perera <roly.perera@dynamicaspects.org>
2022-11-28 15:01:00 +0000

    🧩 [modify-unused]: Dictionary pretty-printing uses {|..|}.

--- src/Pretty.purs
+++ src/Pretty.purs
@@ -113,13 +113,16 @@ prettyConstr α c Nil | c == cNil = highlightIf α nil
 prettyConstr α c (x : y : Nil) | c == cCons = parens (hspace [ pretty x, highlightIf α $ text ":", pretty y ])
 prettyConstr α c xs = hspace (highlightIf α (prettyCtr c) : (prettyParensOpt <$> xs))
 
-bracify :: Endo Doc
-bracify = between (text "{") (text "}")
-
-prettyRecord :: forall a. Pretty a => 𝔹 -> List (Bind a) -> Doc
-prettyRecord α xvs =
+prettyRecordOrDict :: forall a. Pretty a => Endo Doc -> 𝔹 -> List (Bind a) -> Doc
+prettyRecordOrDict bracify α xvs =
    xvs <#> (\(x ↦ v) -> hspace [ text x :<>: colon, pretty v ])
       # hcomma >>> bracify >>> highlightIf α
+
+prettyDict :: forall a. Pretty a => 𝔹 -> List (Bind a) -> Doc
+prettyDict = prettyRecordOrDict $ between (text "{|") (text "|}")
+
+prettyRecord :: forall a. Pretty a => 𝔹 -> List (Bind a) -> Doc
+prettyRecord = prettyRecordOrDict $ between (text "{") (text "}")
 
 instance Pretty (E.Expr Boolean) where
    pretty (E.Var x) = text x
@@ -166,7 +169,7 @@ instance Pretty (Val Boolean) where
    pretty (V.Float α n) = highlightIf α (text (show n))
    pretty (V.Str α str) = highlightIf α (text (show str))
    pretty (V.Record α xvs) = prettyRecord α (xvs # D.toUnfoldable)
-   pretty (V.Dict α svs) = prettyRecord α (svs # D.toUnfoldable)
+   pretty (V.Dict α svs) = prettyDict α (svs # D.toUnfoldable)
    pretty (V.Constr α c vs) = prettyConstr α c vs
    pretty (V.Matrix _ (vss × _ × _)) = vert comma (((<$>) pretty >>> hcomma) <$> vss)
    pretty (V.Closure _ _ _ _) = text "<closure>"
