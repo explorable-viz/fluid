34b20f35b42148e1f69b9eab5ef1523f06fa6de8
harleengulati03 <93429233+harleengulati03@users.noreply.github.com>
2023-06-08 14:47:11 +0100

    variable case

--- src/Pretty2.purs
+++ src/Pretty2.purs
@@ -4,9 +4,11 @@ import Prelude
 import Text.Pretty (Doc, empty, text)
 import SExpr (Expr(..))
 
+
 emptyDoc :: Doc
 emptyDoc = empty 0 0
 
 pretty :: forall a. Expr a -> Doc
 pretty (Int _ n) = text (show n) 
+pretty (Var x) = text x
 pretty _ = emptyDoc
