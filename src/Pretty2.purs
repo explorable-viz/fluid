b1f01becbbeaf7e0f7b264452a939e75ce0e4237
harleengulati03 <93429233+harleengulati03@users.noreply.github.com>
2023-06-08 15:53:41 +0100

    binary app case

--- src/Pretty2.purs
+++ src/Pretty2.purs
@@ -1,10 +1,10 @@
 module Pretty2 where
 
-import Prelude
+import Prelude 
 import Text.Pretty (Doc, empty, text, beside, atop)
 import SExpr (Expr(..))
 
--- infixl 5 beside as <>
+infixl 5 beside as :<>:
 infixl 5 atop as .-. 
 
 emptyDoc :: Doc
@@ -13,5 +13,7 @@ emptyDoc = empty 0 0
 pretty :: forall a. Expr a -> Doc
 pretty (Int _ n) = text (show n) 
 pretty (Var x) = text x
-pretty (App (s) (s')) = beside (pretty s) (pretty s')
+pretty (App s s') = pretty s :<>: pretty s'
+pretty(BinaryApp s x s') = pretty s :<>: text x :<>: pretty s'
+
 pretty _ = emptyDoc
