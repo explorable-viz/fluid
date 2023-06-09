40685ad1d2869e5a902b953d41aa7ba3b421bd73
harleengulati03 <93429233+harleengulati03@users.noreply.github.com>
2023-06-09 11:23:55 +0100

    auxillary function written

--- src/Pretty2.purs
+++ src/Pretty2.purs
@@ -1,20 +1,19 @@
 module Pretty2 where
 
-import Prelude 
+import Prelude
 import Text.Pretty (Doc, empty, text, beside, atop)
 import SExpr (Expr(..))
 import Data.List (List(..))
 import Bindings (Bind, key, val)
 
-
 infixl 5 beside as :<>:
-infixl 5 atop as .-. 
+infixl 5 atop as .-.
 
 emptyDoc :: Doc
 emptyDoc = empty 0 0
 
 pretty :: forall a. Expr a -> Doc
-pretty (Int _ n) = text (show n) 
+pretty (Int _ n) = text (show n)
 pretty (Var x) = text x
 pretty (App s s') = pretty s :<>: pretty s'
 pretty (BinaryApp s x s') = pretty s :<>: text x :<>: pretty s'
@@ -22,7 +21,7 @@ pretty (IfElse s s_1 s_2) = text "if" :<>: pretty s :<>: text "then" :<>: pretty
 pretty (Project s x) = pretty s :<>: text "." :<>: text x
 pretty _ = emptyDoc
 
-data Tree a = Leaf | Node a (Tree a) (Tree a) 
+data Tree a = Leaf | Node a (Tree a) (Tree a)
 
 -- subtle error in code needs fixing
 prettyAuxillaryFunc :: forall a. List (Bind (Expr a)) -> Doc
