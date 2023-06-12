690a60ac9060de78c0909ff737890306312168f0
harleengulati03 <93429233+harleengulati03@users.noreply.github.com>
2023-06-12 10:08:09 +0100

    lambda mu case done

--- src/Pretty2.purs
+++ src/Pretty2.purs
@@ -2,12 +2,12 @@ module Pretty2 where
 
 import Prelude
 import Text.Pretty (Doc, empty, text, beside, atop)
-import SExpr (Expr(..),Pattern(..), Clauses(..), Clause(..), Branch(..))
+import SExpr (Expr(..),Pattern(..), Clauses(..), Clause(..))
 import Data.List (List(..))
 import Bindings (Bind, key, val)
 import Data.List.NonEmpty (toList)
 import Data.Foldable(foldl)
-import Util ((×), type (×))
+import Util ((×))
 
 infixl 5 beside as :<>:
 infixl 5 atop as .-.
@@ -25,6 +25,7 @@ pretty (BinaryApp s x s') = pretty s :<>: text x :<>: pretty s'
 pretty (IfElse s s_1 s_2) = text "if" :<>: pretty s :<>: text "then" :<>: pretty s_1 :<>: text "else" :<>: pretty s_2
 pretty (Project s x) = pretty s :<>: text "." :<>: text x
 pretty (Record _ x) = text "{" :<>: prettyAuxillaryFunc x :<>: text "}" -- formatting needs fixing 
+pretty (Lambda (Clauses cs)) = text "fun" :<>: text "{" :<>: prettyAuxillaryFuncClauses Unit (Clauses cs) :<>: text "}"  
 pretty _ = emptyDoc
 
 -- subtle error in code needs fixing
@@ -54,16 +55,18 @@ varClauses :: forall a. Expr a -> Clause a -> Doc
 varClauses (Var x) (Clause (ps × e)) = text x :<>: prettyAuxillaryFuncPatterns (toList ps):<>: text "=" :<>: pretty e
 varClauses _ _ = emptyDoc
 
-varClauses2 :: (forall a. (Expr a) × Clause a) -> Doc 
-varClauses2 ((Var x) × (Clause (ps × e))) =  text x :<>: prettyAuxillaryFuncPatterns (toList ps):<>: text "=" :<>: pretty e
-varClauses2 _ = emptyDoc
-
 prettyAuxillaryFuncClauses :: forall a. InFront a -> Clauses a -> Doc 
 prettyAuxillaryFuncClauses Unit (Clauses cs) = let docs = map unitClauses cs in foldl (.-.) emptyDoc docs 
 prettyAuxillaryFuncClauses (Prefix p@(Var _x)) (Clauses cs) = let docs = map (varClauses p) cs in foldl (.-.) emptyDoc docs 
 prettyAuxillaryFuncClauses _ _ = emptyDoc
 
+--helperFunctionTemp :: forall a. List (Branch a) -> Doc 
+--helperFunctionTemp (Cons x xs) = (varClauses (key x) (val x)) .-. (helperFunctionTemp xs)
+--helperFunctionTemp Nil = emptyDoc
+--helperFunctionTemp
+
 --prettyRecursiveFunc :: forall a. RecDefs a -> Doc 
---prettyRecursiveFunc (RecDefs x) = let docs = map varClauses2 x in foldl (.-.) emptyDoc docs 
+--prettyRecursiveFunc x = helperFunctionTemp (toList x)
 --prettyRecursiveFunc _ = emptyDoc
 
+-- prettyRecursiveFunc (RecDefs x) = let docs = map varClauses2 x in foldl (.-.) emptyDoc docs 
