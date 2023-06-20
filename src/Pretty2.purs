30e8ac58ff0e9576be84f635718728cb459aba5a
harleengulati03 <93429233+harleengulati03@users.noreply.github.com>
2023-06-20 16:23:27 +0100

    all expressions implemented commented case dict_intersectionWith

--- src/Pretty2.purs
+++ src/Pretty2.purs
@@ -22,10 +22,10 @@ emptyDoc = empty 0 0
 
 pretty :: forall a. Expr a -> Doc
 pretty (Int _ n) = text (show n) -- edited
--- pretty (App s s') = (pretty s .-. (emptyDoc :--: emptyDoc :--: emptyDoc :--: letArg s'))
+pretty (App s s') = (pretty s .-. (emptyDoc :--: emptyDoc :--: emptyDoc :--: letArg s'))
 pretty (Var x) = emptyDoc :--: text x :--: emptyDoc -- edited
 pretty (Op x) = text "(" .<>. text x .<>. text ")"
-pretty (App s s') = pretty s :--: pretty s'
+-- pretty (App s s') = pretty s :--: pretty s'
 pretty (BinaryApp s x s') = text "(" .<>. (pretty s :--: emptyDoc) .<>. (text x :--: emptyDoc) .<>. pretty s' .<>. text ")" -- edited
 pretty (IfElse s s_1 s_2) = (emptyDoc :--: text "if" :--: emptyDoc) .<>. pretty s .<>. (emptyDoc :--: text "then" :--: emptyDoc) .<>. pretty s_1 .<>. (emptyDoc :--: text "else" :--: emptyDoc) .<>. pretty s_2
 pretty (Project s x) = pretty s .<>. text "." .<>. text x
