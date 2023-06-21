c339d5b1a2fe254b3be269bc84a56595424afa1e
JosephBond <jecb1998@googlemail.com>
2023-06-21 10:09:57 +0100

    Fixed final failing test case, so this branch can now be merged

--- test/Main.purs
+++ test/Main.purs
@@ -159,13 +159,13 @@ test_bwd =
    , testBwd (File "dict/foldl") (File "dict/foldl.expect")
         topOf
         "_0_"
-   --    , testBwd (File "dict/intersectionWith") (File "dict/intersectionWith.expect")
-   --         ( const $ Dictionary false $ fromFoldable
-   --              [ "b" ↦ (false × Int true 0)
-   --              , "c" ↦ (false × Int true 20)
-   --              ]
-   --         )
-   --         "{|\"b\":= _0_, \"c\":= _20_|}"
+   , testBwd (File "dict/intersectionWith") (File "dict/intersectionWith.expect")
+        ( const $ Dictionary false $ fromFoldable
+             [ "b" ↦ (false × Int true 0)
+             , "c" ↦ (false × Int true 10)
+             ]
+        )
+        "{|\"b\":= _0_, \"c\":= _20_|}"
    , testBwd (File "dict/fromRecord") (File "dict/fromRecord.expect")
         ( const $ Dictionary false $ fromFoldable
              [ "a" ↦ (false × Int false 5)
