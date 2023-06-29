a30f5eb922f9822fa4ff6d9d01a1c78d6913b5a5
harleengulati03 <93429233+harleengulati03@users.noreply.github.com>
2023-06-29 16:43:28 +0100

    yarn tidied

--- src/Pretty3.purs
+++ src/Pretty3.purs
@@ -58,8 +58,8 @@ instance Ann a => Pretty (Expr a) where
    pretty (LetRec g s) = ((text "let" :--: emptyDoc .<>. pretty (First g)) .-. (emptyDoc :--: text "in" :--: emptyDoc)) .-. pretty s
    pretty (MatchAs s x) = (((text "match" :--: emptyDoc) .<>. text "(" .<>. pretty s .<>. text ")" .<>. (emptyDoc :--: text "as {")) .-. (emptyDoc :--: emptyDoc :--: emptyDoc :--: emptyDoc .<>. pretty x)) .-. text "}"
    pretty (ListEmpty ann) = highlightIf ann $ text "[]"
-   pretty (ListNonEmpty ann (Record _ x) y) =  emptyDoc :--: (((highlightIf ann $ text "[") .<>. (highlightIf ann $ text "{" .<>. pretty (true × x) .<>. text "}")) .-. pretty y)
-   pretty (ListNonEmpty ann s x) =  emptyDoc :--: (highlightIf ann $ text "[") .<>. pretty s .<>. pretty (x)
+   pretty (ListNonEmpty ann (Record _ x) y) = emptyDoc :--: (((highlightIf ann $ text "[") .<>. (highlightIf ann $ text "{" .<>. pretty (true × x) .<>. text "}")) .-. pretty y)
+   pretty (ListNonEmpty ann s x) = emptyDoc :--: (highlightIf ann $ text "[") .<>. pretty s .<>. pretty (x)
    pretty (ListEnum s s') = text "[" .<>. pretty s .<>. text ".." .<>. pretty s' .<>. text "]"
    pretty (Let x s) = text "(" .<>. text "let" :--: emptyDoc .<>. pretty x .<>. (emptyDoc :--: text "in" :--: emptyDoc) .<>. pretty s .<>. text ")"
    pretty (Matrix ann s (v × v') s') = highlightIf ann $ text "[" .<>. text "|" .<>. pretty s .<>. text "|" .<>. text "(" .<>. text v .<>. text "," .<>. text v' .<>. (text ")" :--: emptyDoc) .<>. (text "in" :--: emptyDoc) .<>. pretty s' .<>. text "|" .<>. text "]"
@@ -70,14 +70,13 @@ instance Ann a => Pretty (Expr a) where
    pretty (ListComp ann s q) = highlightIf ann $ text "[" .<>. pretty s .<>. text "|" .<>. pretty q .<>. text "]"
 
 instance Ann a => Pretty (Boolean × List (Bind (Expr a))) where
-   pretty ( _ × (Cons x Nil)) = text (key x) .<>. text ":" .<>. pretty (val x)
+   pretty (_ × (Cons x Nil)) = text (key x) .<>. text ":" .<>. pretty (val x)
    pretty (false × (Cons x xs)) = (text (key x) .<>. text ":" .<>. pretty (val x) .<>. text ",") .-. pretty (false × xs) -- edited atop
    pretty (true × (Cons x xs)) = (text (key x) .<>. text ":" .<>. pretty (val x) .<>. text ",") .<>. pretty (true × xs)
    pretty (_ × Nil) = emptyDoc
 
-
 instance Ann a => Pretty (ListRest a) where
-   pretty (Next ann (Record _ x) y) = (highlightIf ann $ text ",") .<>. text "" .<>. (highlightIf ann $ text "{" .<>. pretty (true × x) .<>. text "}") .-. pretty y 
+   pretty (Next ann (Record _ x) y) = (highlightIf ann $ text ",") .<>. text "" .<>. (highlightIf ann $ text "{" .<>. pretty (true × x) .<>. text "}") .-. pretty y
    pretty (Next ann s x) = (highlightIf ann $ text ",") .<>. text "" .<>. pretty s .<>. pretty x
    pretty (End ann) = highlightIf ann $ text "]"
 
@@ -143,7 +142,6 @@ instance Ann a => Pretty (VarDef a) where
 
 instance Ann a => Pretty (VarDefs a) where
    pretty x = intersperse' (toList (map pretty x)) (text ";")
-
 
 instance Ann a => Pretty (IsConstrPair a) where
    pretty (_ × ((Cons x Nil))) = pretty x
