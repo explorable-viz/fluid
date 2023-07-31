06a7f18f8c68b44e11550c3febbfda5024515248
JosephBond <jecb1998@googlemail.com>
2023-07-31 20:01:09 +0100

    Fixed failing tests?

--- src/DependenceGraph.purs
+++ src/DependenceGraph.purs
@@ -9,42 +9,43 @@ import Data.Tuple (Tuple(..), fst, snd, swap)
 import Foreign.Object (Object, delete, fromFoldableWith, insert, lookup, singleton, size, toUnfoldable) as O
 import Util (Endo)
 
-
 class Graph g where
-    union     :: Vertex -> Set Vertex -> Endo g
-    outN      :: g -> Vertex -> Set Vertex
-    singleton :: Vertex -> Set Vertex -> g
-    remove    :: Vertex -> Endo g
-    opp       :: Endo g
-    allocate  :: g -> Vertex
+   union :: Vertex -> Set Vertex -> Endo g
+   outN :: g -> Vertex -> Set Vertex
+   singleton :: Vertex -> Set Vertex -> g
+   remove :: Vertex -> Endo g
+   opp :: Endo g
+   allocate :: g -> Vertex
 
 newtype Vertex = Vertex String
 
 unwrap :: Vertex -> String
-unwrap (Vertex string) = string    
+unwrap (Vertex string) = string
+
 newtype AnnGraph = AnnGraph (O.Object (Tuple Vertex (Set Vertex)))
 
-newtype SimpleGraph = SimpleGraph (O.Object (Set Vertex)) 
+newtype SimpleGraph = SimpleGraph (O.Object (Set Vertex))
 
-instance Graph SimpleGraph where 
-    allocate (SimpleGraph obj) = Vertex id 
-                    where
-                        id = show $ 1 + (O.size obj)
-    remove (Vertex key) (SimpleGraph obj) = SimpleGraph (O.delete key obj) 
-    union (Vertex key) neighbs (SimpleGraph obj) = (SimpleGraph newObj)
-                                        where
-                                            newObj = O.insert key neighbs obj
-    outN (SimpleGraph obj) (Vertex key) = case O.lookup key obj of
-                                            Just verts -> verts
-                                            Nothing    -> S.empty
-    singleton (Vertex key) neighbs = SimpleGraph (O.singleton key neighbs)
-    opp s = fromEdges $ reverseEdges $ allEdges s
-                        -- SimpleGraph $ foldrWithIndex combine obj verts
-                        --   where
-                        --   combine v edges = O.unionWith S.union (O.fromFoldable edges)
-                        --   verts = O.keys obj
+instance Graph SimpleGraph where
+   allocate (SimpleGraph obj) = Vertex id
+      where
+      id = show $ 1 + (O.size obj)
+   remove (Vertex key) (SimpleGraph obj) = SimpleGraph (O.delete key obj)
+   union (Vertex key) neighbs (SimpleGraph obj) = (SimpleGraph newObj)
+      where
+      newObj = O.insert key neighbs obj
+   outN (SimpleGraph obj) (Vertex key) = case O.lookup key obj of
+      Just verts -> verts
+      Nothing -> S.empty
+   singleton (Vertex key) neighbs = SimpleGraph (O.singleton key neighbs)
+   opp s = fromEdges $ reverseEdges $ allEdges s
+
+-- SimpleGraph $ foldrWithIndex combine obj verts
+--   where
+--   combine v edges = O.unionWith S.union (O.fromFoldable edges)
+--   verts = O.keys obj
 allEdges :: SimpleGraph -> Set (Tuple Vertex Vertex)
-allEdges (SimpleGraph obj) =  let out = (map adjEdges (O.toUnfoldable obj :: Array (Tuple String (Set Vertex)))) in S.unions out
+allEdges (SimpleGraph obj) = let out = (map adjEdges (O.toUnfoldable obj :: Array (Tuple String (Set Vertex)))) in S.unions out
 
 adjEdges :: Tuple String (Set Vertex) -> Set (Tuple Vertex Vertex)
 adjEdges (Tuple id neighbs) = adjEdges' (Vertex id) neighbs
@@ -57,7 +58,8 @@ reverseEdges edges = S.map swap edges
 
 fromEdges :: Set (Tuple Vertex Vertex) -> SimpleGraph
 fromEdges edges = SimpleGraph $
-                    O.fromFoldableWith S.union (S.map (\pair -> Tuple (unwrap (fst pair)) (S.singleton $ snd pair)) edges)
+   O.fromFoldableWith S.union (S.map (\pair -> Tuple (unwrap (fst pair)) (S.singleton $ snd pair)) edges)
+
 -- instance Foldable Graph where
 --     foldl f z (Graph o) = Graph (foldl f z (map fst (O.values o) :: ?_))
 --     foldr f z (Graph o) = Graph (foldr f z o)
