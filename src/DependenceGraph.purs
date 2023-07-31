9c50fa27217c904105a62cfae306fdc49ecf7ea8
JosephBond <jecb1998@googlemail.com>
2023-07-31 19:57:35 +0100

    initial inefficient simplegraph implementation

--- src/DependenceGraph.purs
+++ src/DependenceGraph.purs
@@ -2,15 +2,12 @@ module DependenceGraph where
 
 import Prelude
 
-import Data.Bifunctor (lmap)
-import Data.Foldable (class Foldable, foldl, foldr, foldMap)
-import Data.FoldableWithIndex (foldrWithIndex)
 import Data.Maybe (Maybe(..))
 import Data.Set (Set)
-import Data.Set (empty, singleton, union) as S
-import Data.Tuple (Tuple(..), fst)
-import Foreign.Object (Object, delete, fromFoldable, insert, keys, lookup, singleton, size, unionWith, values) as O
-import Util (Endo, error)
+import Data.Set (empty, map, singleton, union, unions) as S
+import Data.Tuple (Tuple(..), fst, snd, swap)
+import Foreign.Object (Object, delete, fromFoldableWith, insert, lookup, singleton, size, toUnfoldable) as O
+import Util (Endo)
 
 
 class Graph g where
@@ -23,6 +20,8 @@ class Graph g where
 
 newtype Vertex = Vertex String
 
+unwrap :: Vertex -> String
+unwrap (Vertex string) = string    
 newtype AnnGraph = AnnGraph (O.Object (Tuple Vertex (Set Vertex)))
 
 newtype SimpleGraph = SimpleGraph (O.Object (Set Vertex)) 
@@ -39,13 +38,30 @@ instance Graph SimpleGraph where
                                             Just verts -> verts
                                             Nothing    -> S.empty
     singleton (Vertex key) neighbs = SimpleGraph (O.singleton key neighbs)
-    opp (SimpleGraph _) = error "todo"
+    opp s = fromEdges $ reverseEdges $ allEdges s
                         -- SimpleGraph $ foldrWithIndex combine obj verts
                         --   where
                         --   combine v edges = O.unionWith S.union (O.fromFoldable edges)
                         --   verts = O.keys obj
+allEdges :: SimpleGraph -> Set (Tuple Vertex Vertex)
+allEdges (SimpleGraph obj) =  let out = (map adjEdges (O.toUnfoldable obj :: Array (Tuple String (Set Vertex)))) in S.unions out
 
+adjEdges :: Tuple String (Set Vertex) -> Set (Tuple Vertex Vertex)
+adjEdges (Tuple id neighbs) = adjEdges' (Vertex id) neighbs
+
+adjEdges' :: Vertex -> Set Vertex -> Set (Tuple Vertex Vertex)
+adjEdges' v neighbs = S.map (\node -> (Tuple v node)) neighbs
+
+reverseEdges :: Endo (Set (Tuple Vertex Vertex))
+reverseEdges edges = S.map swap edges
+
+fromEdges :: Set (Tuple Vertex Vertex) -> SimpleGraph
+fromEdges edges = SimpleGraph $
+                    O.fromFoldableWith S.union (S.map (\pair -> Tuple (unwrap (fst pair)) (S.singleton $ snd pair)) edges)
 -- instance Foldable Graph where
 --     foldl f z (Graph o) = Graph (foldl f z (map fst (O.values o) :: ?_))
 --     foldr f z (Graph o) = Graph (foldr f z o)
 --     foldMap f (Graph o) = Graph (foldMap f o)
+
+derive instance Eq Vertex
+derive instance Ord Vertex
