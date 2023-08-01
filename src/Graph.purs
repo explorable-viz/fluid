efa5ba88515897a9e873851a61a006819dae82da
JosephBond <jecb1998@googlemail.com>
2023-08-01 16:34:52 +0100

    Fixed implementation of remove

--- src/Graph.purs
+++ src/Graph.purs
@@ -6,9 +6,9 @@ import Data.Foldable (foldl)
 import Data.Maybe (Maybe)
 import Data.Newtype (class Newtype)
 import Data.Set (Set)
-import Data.Set (empty, map, singleton, union, unions) as S
+import Data.Set (delete, empty, map, singleton, union, unions) as S
 import Data.Tuple (Tuple(..), swap)
-import Foreign.Object (Object, delete, lookup, singleton, size, toUnfoldable, unionWith) as O
+import Foreign.Object (Object, delete, fromFoldable, lookup, singleton, size, toUnfoldable, unionWith) as O
 import Util (Endo, (×), type (×))
 
 class Graph g where
@@ -33,8 +33,9 @@ instance Graph GraphImpl where
    allocate (GraphImpl (obj × _)) = Vertex α
       where
       α = show $ 1 + (O.size obj)
-   remove (Vertex α) (GraphImpl (obj1 × obj2)) = GraphImpl ((O.delete α obj1) × (O.delete α obj2))
-
+   remove (Vertex α) (GraphImpl (obj1 × obj2)) = let newObj1 = map (S.delete (Vertex α)) (O.delete α obj1)
+                                                     newObj2 = map (S.delete (Vertex α)) (O.delete α obj2)
+                                                   in GraphImpl (newObj1 × newObj2)  
    union α αs (GraphImpl (obj1 × obj2)) = (GraphImpl (newObj1 × newObj2))
       where
       newObj1 = O.unionWith S.union obj1 (outStar α αs)
@@ -46,12 +47,24 @@ instance Graph GraphImpl where
    singleton α αs = GraphImpl (outStar α αs × inStar α αs)
    opp (GraphImpl (obj1 × obj2)) = GraphImpl (obj2 × obj1)
 
--- these use folds to construct objects of the appropriate fields, with inverted entries, hence the mirror of the structure in each
+-- Initial attempts at making stargraphs, using foldl to construct intermediate objects
 outStar :: Vertex -> Set Vertex -> O.Object (Set Vertex)
 outStar (Vertex α) αs = foldl (O.unionWith S.union) (O.singleton α αs) (S.map (\(Vertex α') -> O.singleton α' S.empty) αs)
 
 inStar :: Vertex -> Set Vertex -> O.Object (Set Vertex)
 inStar (Vertex α) αs = foldl (O.unionWith S.union) (O.singleton α S.empty) (S.map (\(Vertex α') -> O.singleton α' (S.singleton (Vertex α))) αs)
+
+-- prototype attempts at more efficiently implementing the above operations
+outStar' :: Vertex -> Set Vertex -> O.Object (Set Vertex)
+outStar' v@(Vertex α) αs = O.unionWith S.union (O.singleton α αs) (star' v αs)
+
+star' :: Vertex -> Set Vertex -> O.Object (Set Vertex)
+star' (Vertex α) αs = O.fromFoldable $ S.map (\α' -> α × (S.singleton α')) αs
+star'' :: Vertex -> Set Vertex -> O.Object (Set Vertex)
+star'' α αs = O.fromFoldable $ S.map (\(Vertex α') -> α' × (S.singleton α)) αs
+
+inStar' :: Vertex -> Set Vertex -> O.Object (Set Vertex)
+inStar' v@(Vertex α) αs = O.unionWith S.union (O.singleton α S.empty) (star'' v αs)
 
 allEdges :: GraphImpl -> Set (Vertex × Vertex)
 allEdges (GraphImpl (obj × _)) = let out = (map adjEdges (O.toUnfoldable obj :: Array (String × (Set Vertex)))) in S.unions out
