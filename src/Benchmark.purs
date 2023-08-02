f953e27bdb9a09fd8919b67be2af14ed77eab3c4
JosephBond <jecb1998@googlemail.com>
2023-08-02 12:16:14 +0100

    Skeleton of benchmarking complete

--- src/Benchmark.purs
+++ src/Benchmark.purs
@@ -1,4 +1,33 @@
 module Benchmark where
   
 import Prelude
-import Benchotron.Core
+import Effect (Effect)
+import Benchotron.Core (Benchmark, benchFn', mkBenchmark)
+import Benchotron.UI.Console (runSuite)
+import Control.Monad.Gen.Common (genTuple)
+import Data.Set (fromFoldable, Set)
+import Data.String.Gen (genDigitString)
+import Data.Tuple (uncurry, Tuple(..))
+import Graph (outStar, outStar', Vertex(..))
+import Test.QuickCheck.Arbitrary (arbitrary)
+import Test.QuickCheck.Gen (vectorOf)
+
+
+main :: Effect Unit
+main = runSuite [benchOutStar]
+
+preProcessTuple :: Tuple Vertex (Array Vertex) -> Tuple Vertex (Set Vertex)
+preProcessTuple (Tuple α αs) = Tuple α (fromFoldable αs)
+
+benchOutStar :: Benchmark
+benchOutStar = mkBenchmark 
+    {   slug: "out-star"
+    ,   title: "comparing fold and fromFoldable"
+    ,   sizes: [10, 20, 40, 50, 100]
+    ,   sizeInterpretation: "Size of graph being created"
+    ,   inputsPerSize: 3
+    ,   gen: \n -> genTuple (Vertex <$> genDigitString) (vectorOf n (Vertex <$> arbitrary))
+    ,   functions: [ benchFn' "outStarFold" (uncurry outStar) preProcessTuple
+                   , benchFn' "outStarFromFoldable" (uncurry outStar') preProcessTuple
+                   ]
+    }
