79c47ad90d07aef31d862b10f4a772f292cce119
Roly Perera <roly.perera@dynamicaspects.org>
2023-08-03 11:40:30 +0100

    🧩 [remove-unused]: Sugar' newtype and associated wrapper/unwrapper.

--- src/Desugarable.purs
+++ src/Desugarable.purs
@@ -1,21 +1,8 @@
 module Desugarable where
 
 import Prelude
-import Unsafe.Coerce (unsafeCoerce)
 import Lattice (Raw, class JoinSemilattice, class BoundedJoinSemilattice)
-import Util (MayFail, type (×))
-
-wrapSugar :: forall s e. Desugarable s e => Raw s -> Sugar' e
-wrapSugar s = Sugar' (\k -> k s)
-
-unwrapSugar :: forall s e. Desugarable s e => Sugar' e -> Raw s
-unwrapSugar (Sugar' k) = k unsafeCoerce
-
-newtype Sugar' e = Sugar' (forall r. (forall s. Desugarable s e => Raw s -> r) -> r)
-
-class FromSugar e where
-   fromSug :: forall a. Sugar' e -> e a -> e a
-   toSug :: forall a. e a -> Sugar' e × e a
+import Util (MayFail)
 
 class (Functor s, Functor e) <= Desugarable s e | s -> e where
    desug :: forall a. JoinSemilattice a => s a -> MayFail (e a)
