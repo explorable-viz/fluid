8e87649a5f36759cc7f4594bbef0a172f769a198
JosephBond <jecb1998@googlemail.com>
2023-08-04 16:24:43 +0100

    Refactoring Graph.purs

--- src/Graph.purs
+++ src/Graph.purs
@@ -6,15 +6,15 @@ import Control.Monad.State (State, StateT, get, put)
 import Data.List (List(..), (:))
 import Data.List (fromFoldable, filter, elem, concat) as L
 import Data.Maybe (Maybe(..))
-import Data.Newtype (class Newtype, unwrap)
+import Data.Newtype (class Newtype)
 import Data.Set (Set)
-import Data.Set (delete, difference, empty, fromFoldable, map, member, singleton, subset, union) as S
+import Data.Set (delete, empty, map, singleton, union) as S
 import Data.Tuple (fst)
-import Foreign.Object (Object, delete, empty, filterKeys, fromFoldable, keys, lookup, singleton, size, unionWith) as SM
+import Foreign.Object (Object, delete, empty, fromFoldable, lookup, singleton, size, unionWith) as SM
 import Util (Endo, (×), type (×))
 
 type SMap = SM.Object
-
+type Edge = Vertex × Vertex
 class Graph g where
    union :: Vertex -> Set Vertex -> Endo g
    outN :: g -> Vertex -> Maybe (Set Vertex)
@@ -23,7 +23,8 @@ class Graph g where
    remove :: Vertex -> Endo g
    opp :: Endo g
    allocate :: g -> Vertex
-
+   emptyG :: g
+   discreteG :: Set Vertex -> g
 newtype Vertex = Vertex String
 
 type HeapT m a = StateT Int m a
@@ -35,6 +36,95 @@ fresh = do
    put (s + 1)
    pure (Vertex (show s))
 
+outE' :: forall g. Graph g => g -> Vertex -> List (Edge)
+outE' graph α = case outN graph α of
+   Just set -> L.fromFoldable $ S.map (\node -> α × node) set
+   Nothing -> Nil
+
+outE :: forall g. Graph g => Set Vertex -> g -> List (Edge)
+outE αs g =
+   let
+      allOut = L.concat (map (\α -> outE' g α) (L.fromFoldable αs))
+   in
+      L.filter (\(e1 × e2) -> (L.elem e1 αs || L.elem e2 αs)) allOut
+
+inE' :: forall g. Graph g => g -> Vertex -> List (Edge)
+inE' graph α = case inN graph α of
+   Just set -> L.fromFoldable $ S.map (\node -> node × α) set
+   Nothing -> Nil
+
+inE :: forall g. Graph g => Set Vertex -> g -> List (Edge)
+inE αs g =
+   let
+      allIn = L.concat (map (\α -> inE' g α) (L.fromFoldable αs))
+   in
+      L.filter (\(e1 × e2) -> L.elem e1 αs || L.elem e2 αs) allIn
+
+elem :: forall g. Graph g => g -> Vertex -> Boolean
+elem graph α =
+   case outN graph α of
+      Just _ -> true
+      Nothing -> false
+
+bwdSlice :: forall g. Graph g => Set Vertex -> g -> g
+bwdSlice αs parent = bwdSlice' parent startG edges
+   where
+   startG = discreteG αs
+   edges = outE αs parent
+
+bwdSlice' :: forall g. Graph g=> g -> g -> List (Edge) -> g
+bwdSlice' parent g ((s × t) : es) =
+   if elem g t then
+      let
+         newG = union s (S.singleton t) g
+      in
+         bwdSlice' parent newG es
+   else
+      let
+         newG = union s (S.singleton t) g
+         newEs = append es (L.fromFoldable (outE' parent t))
+      in
+         bwdSlice' parent newG newEs
+   where
+   append :: forall a. List a -> List a -> List a
+   append Nil xs = xs
+   append (y : ys) xs = append ys (y : xs)
+
+bwdSlice' _ g Nil = g
+
+fwdSlice :: forall g. Graph g => Set Vertex -> g -> g
+fwdSlice αs parent = fst $ fwdEdges parent startG emptyG edges
+   where
+   startG = discreteG αs
+   edges = inE αs parent
+
+fwdEdges :: forall g. Graph g => g -> g -> g -> List (Edge) -> g × g
+fwdEdges parent currSlice pending ((s × t) : es) =
+   let
+      (g' × h') = fwdVertex parent currSlice (union s (S.singleton t) pending) s
+   in
+      fwdEdges parent g' h' es
+fwdEdges _ currSlice pending Nil = currSlice × pending
+
+fwdVertex :: forall g. Graph g => g -> g -> g -> Vertex -> g × g
+fwdVertex parent currSlice pending α =
+   let
+      currNeighbors = outN pending α
+   in
+      if currNeighbors == (outN parent α) then
+         case currNeighbors of
+            Just αs -> fwdEdges parent (union α αs currSlice) (remove α pending) (inE' parent α)
+            Nothing -> fwdEdges parent (union α S.empty currSlice) pending (inE' parent α)
+      else currSlice × pending
+
+derive instance Eq Vertex
+derive instance Ord Vertex
+derive instance Newtype Vertex _
+
+instance Show Vertex where
+   show (Vertex α) = "Vertex " <> α
+
+-- GraphImpl Specifics
 newtype GraphImpl = GraphImpl ((SMap (Set Vertex)) × (SMap (Set Vertex)))
 
 instance Graph GraphImpl where
@@ -57,51 +147,10 @@ instance Graph GraphImpl where
 
    singleton α αs = GraphImpl (starInOut α αs × starInIn α αs)
    opp (GraphImpl (out × in_)) = GraphImpl (in_ × out)
-
-emptyG :: GraphImpl
-emptyG = GraphImpl (SM.empty × SM.empty)
-
-subgraph :: GraphImpl -> Set Vertex -> GraphImpl
-subgraph (GraphImpl (out × in_)) αs =
-   let
-      keys = S.fromFoldable $ SM.keys out
-      αNames = S.map unwrap αs
-   in
-      if S.subset αNames keys then
-         let
-            αs' = S.map Vertex (S.difference keys αNames)
-            filteredOut = SM.filterKeys (\k -> S.member k αNames) out
-            filteredIn = SM.filterKeys (\k -> S.member k αNames) in_
-            newOut = map (\set -> S.difference set αs') filteredOut
-            newIn = map (\set -> S.difference set αs') filteredIn
-         in
-            GraphImpl (newOut × newIn)
-      else
-         emptyG
-
-outE' :: forall g. Graph g => g -> Vertex -> List (Vertex × Vertex)
-outE' graph α = case outN graph α of
-   Just set -> L.fromFoldable $ S.map (\node -> α × node) set
-   Nothing -> Nil
-
-outE :: Set Vertex -> GraphImpl -> List (Vertex × Vertex)
-outE αs g =
-   let
-      allOut = L.concat (map (\α -> outE' g α) (L.fromFoldable αs))
-   in
-      L.filter (\(e1 × e2) -> (L.elem e1 αs || L.elem e2 αs)) allOut
-
-inE' :: forall g. Graph g => g -> Vertex -> List (Vertex × Vertex)
-inE' graph α = case inN graph α of
-   Just set -> L.fromFoldable $ S.map (\node -> node × α) set
-   Nothing -> Nil
-
-inE :: Set Vertex -> GraphImpl -> List (Vertex × Vertex)
-inE αs g =
-   let
-      allIn = L.concat (map (\α -> inE' g α) (L.fromFoldable αs))
-   in
-      L.filter (\(e1 × e2) -> L.elem e1 αs || L.elem e2 αs) allIn
+   emptyG = GraphImpl (SM.empty × SM.empty)
+   discreteG αs = let pairs = S.map (\(Vertex α) -> α × S.empty) αs
+                      discreteM = SM.fromFoldable pairs in
+                    GraphImpl (discreteM × discreteM)
 
 -- prototype attempts at more efficiently implementing the above operations
 starInOut :: Vertex -> Set Vertex -> SMap (Set Vertex)
@@ -121,70 +170,6 @@ inStar α αs = opp (outStar α αs)
 
 outStar :: Vertex -> Set Vertex -> GraphImpl
 outStar α αs = GraphImpl ((starInOut α αs) × (starInIn α αs))
-
-elem :: GraphImpl -> Vertex -> Boolean
-elem (GraphImpl (out × _)) (Vertex α) =
-   case SM.lookup α out of
-      Just _ -> true
-      Nothing -> false
-
-bwdSlice :: Set Vertex -> GraphImpl -> GraphImpl
-bwdSlice αs parent = bwdSlice' parent startG edges
-   where
-   startG = subgraph parent αs
-   edges = outE αs parent
-
-bwdSlice' :: GraphImpl -> GraphImpl -> List (Vertex × Vertex) -> GraphImpl
-bwdSlice' parent g ((s × t) : es) =
-   if elem g t then
-      let
-         newG = union s (S.singleton t) g
-      in
-         bwdSlice' parent newG es
-   else
-      let
-         newG = union s (S.singleton t) g
-         newEs = append es (L.fromFoldable (outE' parent t))
-      in
-         bwdSlice' parent newG newEs
-   where
-   append :: forall a. List a -> List a -> List a
-   append Nil xs = xs
-   append (y : ys) xs = append ys (y : xs)
-
-bwdSlice' _ g Nil = g
-
-fwdSlice :: Set Vertex -> GraphImpl -> GraphImpl
-fwdSlice αs parent = fst $ fwdEdges parent startG emptyG edges
-   where
-   startG = subgraph parent αs
-   edges = inE αs parent
-
-fwdEdges :: GraphImpl -> GraphImpl -> GraphImpl -> List (Vertex × Vertex) -> GraphImpl × GraphImpl
-fwdEdges parent currSlice pending ((s × t) : es) =
-   let
-      (g' × h') = fwdVertex parent currSlice (union s (S.singleton t) pending) s
-   in
-      fwdEdges parent g' h' es
-fwdEdges _ currSlice pending Nil = currSlice × pending
-
-fwdVertex :: GraphImpl -> GraphImpl -> GraphImpl -> Vertex -> GraphImpl × GraphImpl
-fwdVertex parent currSlice pending α =
-   let
-      currNeighbors = outN pending α
-   in
-      if currNeighbors == (outN parent α) then
-         case currNeighbors of
-            Just αs -> fwdEdges parent (union α αs currSlice) (remove α pending) (inE' parent α)
-            Nothing -> fwdEdges parent (union α S.empty currSlice) pending (inE' parent α)
-      else currSlice × pending
-
-derive instance Eq Vertex
-derive instance Ord Vertex
-derive instance Newtype Vertex _
-
-instance Show Vertex where
-   show (Vertex α) = "Vertex " <> α
 
 instance Show GraphImpl where
    show (GraphImpl (out × in_)) = "GraphImpl (" <> show out <> " × " <> show in_ <> ")"
