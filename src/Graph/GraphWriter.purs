b3c4c155d3df41d00df85b039054d21bc626bc16
Roly Perera <roly.perera@dynamicaspects.org>
2023-08-19 18:16:59 +0100

    🧩 [rename]: MonadGraphWriter -> MonadGraph (for consistency with names of data types, and it's not a writer anyway).

--- src/Graph/GraphWriter.purs
+++ src/Graph/GraphWriter.purs
@@ -4,7 +4,7 @@ module Graph.GraphWriter
    , WithGraph
    , WithGraphT
    , class MonadGraphAlloc
-   , class MonadGraphWriter
+   , class MonadGraph
    , alloc
    , extend
    , fresh
@@ -26,11 +26,11 @@ import Data.Traversable (class Traversable, traverse)
 import Graph (Vertex(..), class Graph, fromFoldable)
 import Util (MayFailT, MayFail, type (×), (×))
 
-class Monad m <= MonadGraphWriter s m | m -> s where
+class Monad m <= MonadGraph s m | m -> s where
    -- Extend graph with existing vertex pointing to set of existing vertices.
    extend :: Vertex -> s Vertex -> m Unit
 
-class MonadGraphWriter s m <= MonadGraphAlloc s m | m -> s where
+class MonadGraph s m <= MonadGraphAlloc s m | m -> s where
    fresh :: m Vertex
    -- Extend with a freshly allocated vertex.
    new :: s Vertex -> m Vertex
@@ -41,7 +41,7 @@ type WithGraphAllocT s m = MayFailT (StateT Int (WithGraphT s m))
 type WithGraphT s = StateT (AdjMapEntries s)
 type WithGraph s = WithGraphT s Identity
 
-instance (Monad m, MonadGraphWriter s (WithGraphAllocT s m)) => MonadGraphAlloc s (WithGraphAllocT s m) where
+instance (Monad m, MonadGraph s (WithGraphAllocT s m)) => MonadGraphAlloc s (WithGraphAllocT s m) where
    fresh = do
       n <- modify $ (+) 1
       pure (Vertex $ show n)
@@ -51,11 +51,11 @@ instance (Monad m, MonadGraphWriter s (WithGraphAllocT s m)) => MonadGraphAlloc 
       extend α αs
       pure α
 
-instance Monad m => MonadGraphWriter s (WithGraphT s m) where
+instance Monad m => MonadGraph s (WithGraphT s m) where
    extend α αs =
       void $ modify_ $ (:) (α × αs)
 
-instance Monad m => MonadGraphWriter s (WithGraphAllocT s m) where
+instance Monad m => MonadGraph s (WithGraphAllocT s m) where
    extend α αs = lift $ lift $ extend α αs
 
 alloc :: forall s m t a. Monad m => Traversable t => t a -> WithGraphAllocT s m (t Vertex)
