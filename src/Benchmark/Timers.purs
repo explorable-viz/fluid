7aa507ad4c9cbe04cb0f69a584df3fbc74369b77
JosephBond <jecb1998@googlemail.com>
2023-08-25 14:49:15 +0100

    Initial pass of timer functions, untested

--- src/Benchmark/Timers.purs
+++ src/Benchmark/Timers.purs
@@ -1,3 +1,19 @@
 module Benchmark.Timers where
 
---import Prelude
+import Prelude
+import Effect.Console (logShow)
+import Control.Monad.Trans.Class (lift)
+import Data.JSDate (JSDate, getTime, now)
+import Test.Util (Test)
+
+timeDiff :: JSDate -> JSDate -> Number
+timeDiff begin end =
+   getTime end - getTime begin
+
+liftTimer :: Test Unit -> Test Unit
+liftTimer test = do
+   begin <- lift now
+   out <- test
+   end <- lift now
+   lift $ logShow (timeDiff begin end)
+   pure out
