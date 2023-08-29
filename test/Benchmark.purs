049032bf0007365ed1f315a54d56b76972c45d2c
JosephBond <jecb1998@googlemail.com>
2023-08-29 12:57:29 +0100

    Set up separate benchmarking scripts, so can now separately benchmark and test

--- test/Benchmark.purs
+++ test/Benchmark.purs
@@ -1,4 +1,4 @@
-module Test.Main where
+module Test.Benchmark where
 
 import Prelude hiding (add)
 import App.Util (selectBarChart_data, selectMatrixElement, selectNth, selectNthCell, selectPair, selectSome, select_y)
@@ -17,7 +17,7 @@ import Graph.GraphImpl (GraphImpl)
 import Graph.Slice (fwdSlice)
 import Lattice (botOf, neg, topOf)
 import Module (File(..))
-import Test.Util (Test, run, testWithDatasetMany, testLinkMany, testMany, testBwdMany)
+import Test.Util (Test, run, testWithDatasetMany, testLinkMany, benchMany, benchBwdMany)
 import Util ((×))
 import Val (DictRep(..), Val(..))
 
@@ -40,12 +40,12 @@ tests = [ test_scratchpad ]
 -}
 
 test_scratchpad :: Test Unit
-test_scratchpad = testBwdMany
+test_scratchpad = benchBwdMany
    [ (File "filter") × (File "filter.expect") × (botOf >>> selectNthCell 0 neg) × "(_8_ _:_ (7 : []))"
    ]
 
 test_desugaring :: Test Unit
-test_desugaring = testMany
+test_desugaring = benchMany
    [ (File "desugar/list-comp-1") × "(14 : (12 : (10 : (13 : (11 : (9 : (12 : (10 : (8 : [])))))))))"
    , (File "desugar/list-comp-2") ×
         "(14 : (14 : (14 : (12 : (12 : (12 : (10 : (10 : (10 : (13 : (13 : (13 : (11 : (11 : (11 : (9 : \
@@ -59,7 +59,7 @@ test_desugaring = testMany
    ]
 
 test_misc :: Test Unit
-test_misc = testMany
+test_misc = benchMany
    [ (File "arithmetic") × "42"
    , (File "array") × "(1, (3, 3))"
    , (File "compose") × "5"
@@ -87,7 +87,7 @@ test_misc = testMany
    ]
 
 test_bwd :: Test Unit
-test_bwd = testBwdMany
+test_bwd = benchBwdMany
    [ (File "add") × (File "add.expect") × (const $ Int true 8) × "_8_"
    , (File "array/lookup") × (File "array/lookup.expect") × (const $ Int true 14) × "_14_"
    , (File "array/dims") × (File "array/dims.expect") × topOf × "_(_3_, _3_)_"
