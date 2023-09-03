f3754062c81cd2f8c3c63754304e5edc877b13f0
Roly Perera <roly.perera@dynamicaspects.org>
2023-09-03 10:53:49 +0100

    🧩 [consolidate]: TestLinkSpec type.

--- test/Util.purs
+++ test/Util.purs
@@ -55,7 +55,7 @@ import Set (subset)
 import Test.Spec (SpecT, before, beforeAll, beforeWith, it) -- class Example,evaluateExample,
 import Test.Spec.Assertions (fail)
 import Test.Spec.Mocha (runMocha)
-import Util (MayFailT, type (×), (×), error, successful)
+import Util (MayFailT, (×), error, successful)
 import Val (Val(..), class Ann, (<+>))
 
 type Test a = SpecT Aff Unit Effect a
@@ -209,6 +209,17 @@ type TestBwdSpec =
    , fwd_expect :: String
    }
 
+type TestWithDatasetSpec =
+   { dataset :: String
+   , file :: String
+   }
+
+type TestLinkSpec =
+   { spec :: LinkFigSpec
+   , δv1 :: Selector Val
+   , v2_expect :: String
+   }
+
 testMany :: Array TestSpec → Boolean -> Test Unit
 testMany fxs is_bench = withDefaultImports $ traverse_ test fxs
    where
@@ -231,11 +242,6 @@ testBwdMany fxs is_bench = withDefaultImports $ traverse_ testBwd fxs
                void $ testWithSetup is_bench s gconfig { δv, fwd_expect, bwd_expect }
    folder = File "slicing/"
 
-type TestWithDatasetSpec =
-   { dataset :: String
-   , file :: String
-   }
-
 testWithDatasetMany :: Array TestWithDatasetSpec -> Boolean -> Test Unit
 testWithDatasetMany fxs is_bench = withDefaultImports $ traverse_ testWithDataset fxs
    where
@@ -246,15 +252,16 @@ testWithDatasetMany fxs is_bench = withDefaultImports $ traverse_ testWithDatase
             \(gconfig × s) ->
                void $ testWithSetup is_bench s gconfig { δv: identity, fwd_expect: mempty, bwd_expect: mempty }
 
-testLinkMany :: Array (LinkFigSpec × Selector Val × String) -> Test Unit
+testLinkMany :: Array TestLinkSpec -> Test Unit
 testLinkMany fxs = traverse_ testLink fxs
    where
-   testLink (spec@{ x } × δv1 × v2_expect) =
+   testLink :: TestLinkSpec -> _
+   testLink { spec, δv1, v2_expect } =
       before (loadLinkFig spec) $
          it ("linking/" <> show spec.file1 <> " <-> " <> show spec.file2)
             \{ γ0, γ, e1, e2, t1, t2, v1 } ->
                let
-                  { v': v2' } = successful $ linkResult x γ0 γ e1 e2 t1 t2 (δv1 v1)
+                  { v': v2' } = successful $ linkResult spec.x γ0 γ e1 e2 t1 t2 (δv1 v1)
                in
                   checkPretty "Linked output" v2_expect v2'
 
