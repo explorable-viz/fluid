5b5ae95a91a20ac6151b43870f1e444e7f68811c
Roly Perera <roly.perera@dynamicaspects.org>
2023-09-08 07:08:32 +0100

    🧩 [doc] : Tweak.

--- src/Graph/GraphImpl.purs
+++ src/Graph/GraphImpl.purs
@@ -49,7 +49,7 @@ instance Set s Vertex => Graph (GraphImpl s) s where
       α_αs' = L.fromFoldable α_αs  -- doesn't seem to adversely affect performance
 
 -- Naive implementation based on Dict.filter fails with stack overflow on graphs with ~20k vertices.
--- This works but building the set is still slow if there are thousands of sinks.
+-- This works but still slow if there are thousands of sinks.
 sinks' :: forall s. Set s Vertex => AdjMap s -> s Vertex
 sinks' m = D.toArrayWithKey (×) m
    # A.filter (snd >>> Set.isEmpty)
