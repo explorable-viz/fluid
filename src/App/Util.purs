20889d362c9a4cc4a77cb940721fccbbc0f28692
Roly Perera <roly.perera@dynamicaspects.org>
2023-09-09 16:26:58 +0100

    🧩 [remove-unused]: Selector2.

--- src/App/Util.purs
+++ src/App/Util.purs
@@ -7,9 +7,9 @@ import Data.Array ((:)) as A
 import Data.Foldable (class Foldable)
 import Data.List (List(..), (:), (!!), updateAt)
 import Data.Maybe (Maybe(..))
-import Data.Newtype (class Newtype, unwrap, wrap)
+import Data.Newtype (class Newtype, wrap)
 import Data.Profunctor.Strong (first)
-import Data.Set (Set, difference, union)
+import Data.Set (Set)
 import Data.Set as S
 import Data.Tuple (fst)
 import DataType (Ctr, cBarChart, cCons, cNil, cSome, f_data)
@@ -18,7 +18,7 @@ import Effect (Effect)
 import Foreign.Object (update)
 import Graph (Vertex)
 import Graph.Slice (selectαs)
-import Lattice (class Neg, 𝔹, neg, topOf)
+import Lattice (𝔹, neg, topOf)
 import Partial.Unsafe (unsafePartial)
 import Primitive (as, intOrNumber)
 import Primitive (record) as P
@@ -30,39 +30,19 @@ import Web.Event.EventTarget (EventListener)
 type HTMLId = String
 type Renderer a = HTMLId -> Int -> a -> EventListener -> Effect Unit
 type Selector f = f 𝔹 -> f 𝔹 -- modifies selection state
-newtype Selector2 f = Selector2 (f Vertex -> Set Vertex) -- specifies selection
 type Sel f = f Vertex × Set Vertex
 newtype Sel2 f = Sel2 (f Vertex -> Set Vertex) -- specifies selection
 newtype Selector3 f = Selector3 (Sel f -> Sel f)
 type OnSel = Selector Val -> Effect Unit -- redraw based on modified output selection
-type OnSel2 = Selector2 Val -> Effect Unit -- redraw based on modified output selection
 type Handler = Event -> Selector Val
-type Handler2 = Event -> Selector2 Val
 
-derive instance Newtype (Selector2 f) _
 derive instance Newtype (Selector3 f) _
-
--- Can these be TopOf/BotOf instances?
-selectAll :: forall f. Apply f => Foldable f => Selector2 f
-selectAll = wrap $ \vα -> selectαs (topOf vα) vα
 
 selectAll' :: forall f. Apply f => Foldable f => Selector3 f
 selectAll' = wrap $ \(vα × _) -> vα × selectαs (topOf vα) vα
 
-selectNone :: forall f. Apply f => Foldable f => Selector2 f
-selectNone = wrap $ const S.empty
-
 selectNone' :: forall f. Apply f => Foldable f => Selector3 f
 selectNone' = wrap $ \(vα × _) -> vα × S.empty
-
-instance Semigroup (Selector2 f) where
-   append (Selector2 sel1) (Selector2 sel2) = Selector2 $ \x -> sel1 x `union` sel2 x
-
-instance Monoid (Selector2 f) where
-   mempty = Selector2 $ const S.empty
-
-instance (Apply f, Foldable f) => Neg (Selector2 f) where
-   neg (Selector2 sel) = Selector2 $ \x -> sel x `difference` unwrap selectAll x
 
 doNothing :: OnSel
 doNothing = const $ pure unit
