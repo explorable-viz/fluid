a0174077f7a5f95a41449afe5579af95e92317b0
JosephBond <jecb1998@googlemail.com>
2023-09-11 13:44:16 +0100

    Minimal pass and also minimal failing example

--- test/Benchmark.purs
+++ test/Benchmark.purs
@@ -1,11 +1,20 @@
 module Test.Benchmark where
 
 import Prelude hiding (add)
+
 import Effect (Effect)
+import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
-import Test.Util2 (run)
+import Test.Spec.Mocha2 (runMocha)
+import Test.Util2 (shouldSatisfy)
 
 main :: Effect Unit
 main = do
-   run (liftEffect (log "it worked!"))
+   runMocha [ minimalPass, minimalFail ]
+
+minimalPass :: Aff Unit
+minimalPass = liftEffect (log "it worked!")
+
+minimalFail :: Aff Unit
+minimalFail = shouldSatisfy "bool was false!" false (\x -> x)
