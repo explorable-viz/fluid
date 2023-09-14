e351e3d2dd69405dbf7ed0e7dda66ec8051da778
Roly Perera <roly.perera@dynamicaspects.org>
2023-09-14 10:55:10 +0100

    🧩 [remove-unused]: Benchmark.Util.

--- test/Benchmark/Util.purs
+++ test/Benchmark/Util.purs
@@ -2,20 +2,12 @@ module Benchmark.Util where
 
 import Prelude
 
-import Affjax.RequestBody (string) as RB
-import Affjax.ResponseFormat (string)
-import Affjax.Web (defaultRequest, printError, request)
 import Control.Monad.Writer (WriterT, runWriterT)
 import Data.Array (intersperse)
-import Data.Either (Either(..))
 import Data.Foldable (fold)
-import Data.HTTP.Method (Method(..))
 import Data.JSDate (JSDate, getTime, now)
-import Data.Maybe (Maybe(..))
-import Effect.Aff (Aff)
 import Effect.Class (class MonadEffect, liftEffect)
-import Effect.Console (logShow, log)
-import Util (MayFailT, error, type (×), (×))
+import Util (type (×), (×))
 
 data BenchRow = BenchRow TraceRow GraphRow
 
@@ -68,47 +60,6 @@ bench prog = do
    t' <- liftEffect $ now
    pure (r × timeDiff t t')
 
-getCurr :: forall m. MonadEffect m => m JSDate
-getCurr = liftEffect $ now
-
 timeDiff :: JSDate -> JSDate -> Number
 timeDiff begin end =
    getTime end - getTime begin
-
-logTime :: String -> JSDate -> JSDate -> Aff Unit
-logTime msg before after =
-   liftEffect $ log (msg <> show (timeDiff before after) <> "\n")
-
--- liftTimer :: Test Unit -> Test Unit
--- liftTimer test = do
---    (begin :: ?_ )<- lift now
---    out <- test
---    end <- lift now
---    lift $ logShow (timeDiff begin end)
---    pure out
-
-liftMayFail :: MayFailT Aff Unit -> MayFailT Aff Unit
-liftMayFail test = do
-   begin <- liftEffect $ now
-   test
-   end <- liftEffect $ now
-   liftEffect $ logShow (timeDiff begin end)
-
-loadBenches :: Aff String
-loadBenches = do
-   let url = "./Benchmark/benchmarks.csv"
-   result <- request (defaultRequest { url = url, method = Left GET, responseFormat = string })
-   case result of
-      Left err -> error (printError err)
-      Right response -> pure response.body
-
-appendLine :: String -> Aff Unit
-appendLine line = do
-   old <- loadBenches
-   let
-      url = ".Benchmark/benchmarks.csv"
-      new = old <> "\n" <> line
-   result <- request (defaultRequest { content = Just (RB.string new), url = url, method = Left PUT })
-   case result of
-      Left err -> error (printError err)
-      Right response -> pure response.body
