d6131f006c036da54cffaa4c4f333b57944e4e39
Roly Perera <roly.perera@dynamicaspects.org>
2023-09-27 07:36:59 +0100

    🧩 [modify-unused]: First attempt at row polymorphism.

--- src/GaloisConnection.purs
+++ src/GaloisConnection.purs
@@ -7,14 +7,15 @@ import Util (Endo)
 
 -- a and b are posets, but we don't enforce that here. Use record rather than type class so we can extend with
 -- explicit value-level representation of index (e.g. graph or trace) for families of GCs.
-type GaloisConnection a b =
+type GaloisConnection a b r =
    { fwd :: a -> b
    , bwd :: b -> a
+   | r
    }
 
 deMorgan :: forall a b. BoolAlg a -> BoolAlg b -> Endo (a -> b)
 deMorgan 𝒶 𝒷 = (𝒶.neg >>> _) >>> (_ >>> 𝒷.neg)
 
 -- Could unify deMorgan and dual but would need to reify notion of opposite category.
-dual :: forall a b. BoolAlg a -> BoolAlg b -> GaloisConnection a b -> GaloisConnection b a
-dual 𝒶 𝒷 { fwd, bwd } = { fwd: deMorgan 𝒷 𝒶 bwd, bwd: deMorgan 𝒶 𝒷 fwd }
+dual :: forall a b r. BoolAlg a -> BoolAlg b -> GaloisConnection a b r -> GaloisConnection b a r
+dual 𝒶 𝒷 gc@{ fwd, bwd } = gc{ fwd = deMorgan 𝒷 𝒶 bwd, bwd = deMorgan 𝒶 𝒷 fwd }
