79bba502842fe4548d271b334bc8e6e69f9c5e69
JosephBond <jecb1998@googlemail.com>
2023-09-28 09:55:18 +0100

    Added unsafe indexing for matrices, for use in optimal path extraction

--- example/Util/DTW.purs
+++ example/Util/DTW.purs
@@ -3,29 +3,31 @@ module Example.Util.DTW where
 import Prelude
 
 import Data.Array (concat, elemIndex, foldl, length, modifyAtIndices, range, replicate, sort, unsafeIndex, zip)
+import Data.List (List(..))
 import Data.Maybe (Maybe(..), fromMaybe)
-import Example.Util.BMA (IntInf(..), Matrix, bandMatrix, matIndex)
+import Example.Util.BMA (IntInf(..), Matrix, bandMatrix, matIndex, matOfInds)
+import Partial.Unsafe (unsafePartial)
 import Util (type (×), (×), error)
 
-distanceDTWWindow :: forall a. Partial => Array a -> Array a -> Int -> (a -> a -> IntInf) -> Matrix IntInf × (Matrix IntInf)
+distanceDTWWindow :: forall a. Partial => Array a -> Array a -> Int -> (a -> a -> IntInf) -> Matrix IntInf × (Matrix (Int × Int))
 distanceDTWWindow seq1 seq2 window cost = 
     let n = length seq1
         m = length seq2
         init = bandMatrix n m window
         mappedIndices = sort $ concat (map (\i -> indexIndices i (range (max 1 (i - window)) (min m (i + window)))) (range 1 n))
     in
-        foldl worker init mappedIndices × init
+        foldl worker (init × (matOfInds n m)) mappedIndices
     where
-      worker :: Matrix IntInf -> (Int × Int) -> Matrix IntInf
-      worker matrix (i' × j') = 
+      worker :: Matrix IntInf × Matrix (Int × Int) -> (Int × Int) -> Matrix IntInf × Matrix (Int × Int)
+      worker (dists × inds) (i' × j') = 
         let 
-            im1j   = indexInfty (i'-1) j' matrix
-            ijm1   = indexInfty i' (j'-1) matrix 
-            im1jm1 = indexInfty (i'-1) (j'-1) matrix
-            minim × _prev = prevDirection (i' × j') im1j ijm1 im1jm1 
+            im1j   = indexInfty (i'-1) j' dists
+            ijm1   = indexInfty i' (j'-1) dists 
+            im1jm1 = indexInfty (i'-1) (j'-1) dists
+            minim × prev = prevDirection (i' × j') im1j ijm1 im1jm1 
             costij = (cost (unsafeIndex seq1 (i'-1)) (unsafeIndex seq2 (j'-1))) + minim
         in 
-            updateAt i' j' matrix (\_ -> costij) 
+            (updateAt i' j' dists (\_ -> costij)) × (updateAt i' j' inds (\_ -> prev))
 
 prevDirection :: (Int × Int) -> IntInf -> IntInf -> IntInf -> IntInf × (Int × Int)
 prevDirection (i × j) im1j ijm1 im1jm1 = 
@@ -39,6 +41,21 @@ prevDirection (i × j) im1j ijm1 im1jm1 =
                    else if y == 2 then (im1jm1 × ((i-1)×(j-1)))
                    else error "cannot occur"
 
+extractPath :: Matrix (Int × Int) -> List (Int × Int)
+extractPath matrix = 
+    let i = length matrix
+        j = length (unsafePartial $ unsafeIndex matrix 1)
+    in 
+        traverser i j matrix Nil
+    where
+        traverser :: Int -> Int -> Matrix (Int × Int) -> List (Int × Int) -> List (Int × Int)
+        traverser x y mat accum = 
+            if x == y && y == 0 then 
+                accum 
+            else
+                let newPath = Cons (x × y) accum
+                    (nextX × nextY) = unsafeMatrixInd x y mat
+                in traverser nextX nextY mat newPath
 updateAt :: forall a. Partial => Int -> Int -> Matrix a -> (a -> a) -> Matrix a 
 updateAt i j matrix f = case matIndex matrix i j of
                         Nothing -> matrix
@@ -50,3 +67,6 @@ indexIndices i js = zip (replicate (length js) i) js
 
 distEuclid :: IntInf -> IntInf -> IntInf
 distEuclid x y = (x - y) * (x - y) 
+
+unsafeMatrixInd :: forall a. Int -> Int -> Matrix a -> a
+unsafeMatrixInd x y mat = unsafePartial $ if x < length mat then let xRow = unsafeIndex mat x in if y < length xRow then unsafeIndex xRow y else error "index out of bounds" else error "index out of bounds"
