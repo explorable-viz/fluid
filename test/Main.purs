2ac1b7f82c825af77fffee2208a5af2087ceef3f
Joe Bond <jecb1998@googlemail.com>
2023-10-02 16:29:44 +0100

    Tidied previous commit

--- test/Main.purs
+++ test/Main.purs
@@ -25,10 +25,13 @@ tests :: Array (String × Aff Unit)
 tests = test_scratchpad
 
 test_scratchpad :: Array (String × Aff Unit)
-test_scratchpad = second void <$> many [ { file: "dtw/min-prev"
+test_scratchpad = second void <$> many
+   [ { file: "dtw/min-prev"
      , fwd_expect:
           "((3, 5), 3)"
-     } ] 1
+     }
+   ]
+   1
 
 test_desugaring :: Array (String × Aff Unit)
 test_desugaring = second void <$> many desugar_cases 1
