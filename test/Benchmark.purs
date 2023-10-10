b282330c77c048cdb8a94cf1cd9589585f5bab3f
Roly Perera <roly.perera@dynamicaspects.org>
2023-10-10 09:20:16 +0100

    🧩 [consolidate]: Simplify.

--- test/Benchmark.purs
+++ test/Benchmark.purs
@@ -6,10 +6,8 @@ module Test.Benchmark
 import Prelude hiding (add)
 
 import Benchmark.Util (BenchAcc(..), BenchRow)
-import Control.Apply (lift2)
 import Data.Array (concat)
 import Data.Traversable (sequence)
-import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
 import Effect.Class.Console (logShow)
@@ -22,7 +20,7 @@ main = launchAff_ do
    let
       iter = 1
       arr = concat ([ bench_misc, bench_desugaring, bench_bwd, bench_graphics ] <#> ((#) (iter × true)))
-   outs <- sequence $ map (\(str × row) -> lift2 Tuple (pure str) row) arr
+   outs <- sequence $ (\(str × row) -> (str × _) <$> row) <$> arr
    logShow (BenchAcc outs)
 
 bench_desugaring :: (Int × Boolean) -> Array (String × Aff BenchRow)
