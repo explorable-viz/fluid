8638802982331ff15a5e09db87ae861fd6b97918
JosephBond <jecb1998@googlemail.com>
2023-10-19 14:11:11 +0100

    Changed inital benchmark script

--- plot_bench.py
+++ plot_bench.py
@@ -2,17 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def parse():
+def parse(test_names, column_order):
   # Read benchmark csv
   benchmarks = pd.read_csv('Benchmarks/benchmarks.csv', skipinitialspace=True, delimiter=',', index_col='Test-Name')
 
   # Extract test names of interest
-  test_names = ['convolution/edgeDetect', 'convolution/emboss', 'convolution/gaussian', 'graphics/grouped-bar-chart', 'graphics/line-chart', 'graphics/stacked-bar-chart']
   df = pd.DataFrame(benchmarks.loc[test_names])
   print(df)
 
   # Reorder benchmark columns
-  column_order = ['Trace-Eval', 'Trace-Bwd', 'Trace-Fwd', 'Graph-Eval', 'Graph-Bwd', 'Graph-BwdDual', 'Graph-BwdAll', 'Graph-Fwd', 'Graph-FwdDual', 'Graph-FwdAsDeMorgan']
   # column_colors = ['#0d6b12', '#93dbb5', '#3e3875', '#b8bef5', '#910303', '#e84d4d', '#e8bceb', '#5073a1']
 
   # Plot as bar chart
@@ -25,4 +23,7 @@ def parse():
   plt.gca().axhline(0, lw=0.3, color='blue', label="Zero accuracy")
 
   plt.show()
-parse()
+tests = ['convolution/edgeDetect', 'convolution/emboss', 'convolution/gaussian', 'graphics/grouped-bar-chart', 'graphics/line-chart', 'graphics/stacked-bar-chart']
+all_cols = ['Trace-Eval', 'Trace-Bwd', 'Trace-Fwd', 'Graph-Eval', 'Graph-Bwd', 'Graph-BwdDual', 'Graph-BwdAll', 'Graph-Fwd', 'Graph-FwdDual', 'Graph-FwdAsDeMorgan']
+
+parse(tests, all_cols)
