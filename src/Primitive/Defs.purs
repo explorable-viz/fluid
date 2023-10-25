97e49798241d199dd0b807f1254913f25fbda8fa
Roly Perera <roly.perera@dynamicaspects.org>
2023-10-25 15:25:37 +0100

    🧩 [consolidate]: matrixUpdate trace doesn't need original value, because we have the original matrix anyway.

--- src/Primitive/Defs.purs
+++ src/Primitive/Defs.purs
@@ -145,14 +145,14 @@ matrixUpdate = mkExists $ ForeignOp' { arity: 3, op': op, op: fwd, op_bwd: unsaf
       | c == cPair = Matrix <$> new empty <@> (matrixPut i j (const v) r)
    op _ = throw "Matrix, pair of ints and value expected"
 
-   fwd :: OpFwd (Raw MatrixRep × (Int × Int) × Raw Val)
+   fwd :: OpFwd (Raw MatrixRep × (Int × Int))
    fwd (Matrix _ r : Constr _ c (Int _ i : Int _ j : Nil) : v : Nil) | c == cPair =
-      pure $ (erase r × (i × j) × erase (matrixGet i j r)) × Matrix top (matrixPut i j (const v) r)
+      pure $ (erase r × (i × j)) × Matrix top (matrixPut i j (const v) r)
    fwd _ = throw "Matrix, pair of ints and value expected"
 
-   bwd :: Partial => OpBwd (Raw MatrixRep × (Int × Int) × Raw Val)
-   bwd (((r × (i × j) × oldV) × (Matrix _ r'))) =
-      Matrix bot (matrixPut i j (const (botOf oldV)) (botOf r))
+   bwd :: Partial => OpBwd (Raw MatrixRep × (Int × Int))
+   bwd (((r × (i × j)) × (Matrix _ r'))) =
+      Matrix bot (matrixPut i j identity (botOf r))
          : Constr bot cPair (Int bot i : Int bot j : Nil)
          : matrixGet i j r'
          : Nil
