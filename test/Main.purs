3a7be753f6e347169f2640b2c43260faa6c7a4e4
Roly Perera <roly.perera@dynamicaspects.org>
2023-10-26 16:58:35 +0100

    🧩 [new-test]: compute-dtw slicing test works.

--- test/Main.purs
+++ test/Main.purs
@@ -17,7 +17,6 @@ main :: Effect Unit
 main = run tests
 
 tests :: Array (String × Aff Unit)
-{-
 tests = concat
    [ test_desugaring
    , test_misc
@@ -25,9 +24,10 @@ tests = concat
    , test_graphics
    , test_linking
    ]
--}
 
+{-
 tests = concat [ test_scratchpad ]
+-}
 
 test_scratchpad :: Array (String × Aff Unit)
 test_scratchpad = second void <$> bwdMany
