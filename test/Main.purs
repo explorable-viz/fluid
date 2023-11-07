40240a47ff8968a4552769aa28c24758ffab1c63
Joe Bond <jecb1998@googlemail.com>
2023-11-07 13:00:10 +0000

    Changed fwd_expect to 570

--- test/Main.purs
+++ test/Main.purs
@@ -23,7 +23,7 @@ scratchpad :: TestSuite
 scratchpad = asTestSuite $ bwdMany
    [ { file: "motivating-example"
      , bwd_expect_file: "motivating-example.expect"
-     , fwd_expect: "⸨540⸩"
+     , fwd_expect: "⸨570⸩"
      , δv: neg
      }
    ]
