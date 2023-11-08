d9ac47f84f00768540c90cb12adfc9f349ff2c95
Roly Perera <roly.perera@dynamicaspects.org>
2023-11-08 09:08:50 +0000

    🧩 [rename]: one -> asTest; also remove unused drawing code from PLDI.

--- src/PLDI2024.purs
+++ src/PLDI2024.purs
@@ -2,10 +2,9 @@ module PLDI2024 where
 
 import Prelude hiding (absurd)
 
-import App.Fig (FigSpec, LinkedOutputsFigSpec, drawFiles, drawFigs, drawLinkedOutputsFigs, loadFig, loadLinkedOutputsFig)
+import App.Fig (LinkedOutputsFigSpec, drawLinkedOutputsFigs, loadLinkedOutputsFig)
 import Effect (Effect)
-import Module (File(..), Folder(..))
-import Util ((×))
+import Module (File(..))
 
 -- Will changes to PLDI figures but for now same as those on f.luid.org main page.
 linkedOutputsFig1 :: LinkedOutputsFigSpec
@@ -17,22 +16,6 @@ linkedOutputsFig1 =
    , x: "data"
    }
 
-fig1 :: FigSpec
-fig1 =
-   { divId: "fig-conv-1"
-   , file: File "slicing/convolution/emboss"
-   , xs: [ "image", "filter" ]
-   }
-
-fig2 :: FigSpec
-fig2 =
-   { divId: "fig-conv-2"
-   , file: File "slicing/convolution/emboss-wrap"
-   , xs: [ "image", "filter" ]
-   }
-
 main :: Effect Unit
 main = do
-   drawFiles [ Folder "fluid/lib" × File "convolution" ]
-   drawFigs [ loadFig fig1, loadFig fig2 ]
    drawLinkedOutputsFigs [ loadLinkedOutputsFig linkedOutputsFig1 ]
