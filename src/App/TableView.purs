2d0d9363934ff497564679633b8d6d20f978e711
Joe Bond <jecb1998@googlemail.com>
2023-11-09 11:43:26 +0000

    yarn tidy up

--- src/App/TableView.purs
+++ src/App/TableView.purs
@@ -18,33 +18,37 @@ newtype EnergyTable = EnergyTable { title :: String, table :: Array EnergyRecord
 newtype WaterRecord = WaterRecord { country :: String × 𝔹, cities :: Int × 𝔹, farms :: Int × 𝔹, industry :: Int × 𝔹, energy :: Int × 𝔹, popMil :: Int × 𝔹 }
 newtype WaterTable = WaterTable { title :: String, table :: Array WaterRecord }
 
-newtype Table r = Table { title :: String, table :: Array r}
+newtype Table r = Table { title :: String, table :: Array r }
 
 foreign import drawTable :: Renderer (Table (Dict (Val 𝔹)))
 
 class MakeRecord a where
    fromRec :: Dict (Val 𝔹) -> a
    toDict :: a -> Dict (Val 𝔹)
+
 instance MakeRecord WaterRecord where
-   fromRec r = 
-      WaterRecord { country: string.unpack (get "country" r)
-      , cities: int.unpack (get "cities" r)
-      , farms: int.unpack (get "farms" r)
-      , industry: int.unpack (get "industry" r)
-      , energy: int.unpack (get "energy" r)
-      , popMil: int.unpack (get "popMil" r)
-      }
-   toDict (WaterRecord { country, cities, farms, industry, energy, popMil}) =
-      Dict.fromFoldable (["country" × string.pack country, "cities" × (int.pack cities), "farms" × int.pack farms, "industry" × int.pack industry, "energy" × int.pack energy, "popMil" × int.pack popMil])
+   fromRec r =
+      WaterRecord
+         { country: string.unpack (get "country" r)
+         , cities: int.unpack (get "cities" r)
+         , farms: int.unpack (get "farms" r)
+         , industry: int.unpack (get "industry" r)
+         , energy: int.unpack (get "energy" r)
+         , popMil: int.unpack (get "popMil" r)
+         }
+   toDict (WaterRecord { country, cities, farms, industry, energy, popMil }) =
+      Dict.fromFoldable ([ "country" × string.pack country, "cities" × (int.pack cities), "farms" × int.pack farms, "industry" × int.pack industry, "energy" × int.pack energy, "popMil" × int.pack popMil ])
+
 instance MakeRecord EnergyRecord where
    fromRec r =
-      EnergyRecord { year: int.unpack (get "year" r)
-      , country: string.unpack (get "country" r)
-      , energyType: string.unpack (get "energyType" r)
-      , output: get_intOrNumber "output" r
-      }
-   toDict (EnergyRecord {year, country, energyType, output}) = 
-      Dict.fromFoldable (["year" × int.pack year, "country" × string.pack country, "energyType" × string.pack energyType, "output" × number.pack output])
+      EnergyRecord
+         { year: int.unpack (get "year" r)
+         , country: string.unpack (get "country" r)
+         , energyType: string.unpack (get "energyType" r)
+         , output: get_intOrNumber "output" r
+         }
+   toDict (EnergyRecord { year, country, energyType, output }) =
+      Dict.fromFoldable ([ "year" × int.pack year, "country" × string.pack country, "energyType" × string.pack energyType, "output" × number.pack output ])
 
 tableViewHandler :: Handler
 tableViewHandler = const identity
