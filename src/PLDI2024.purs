7a26b342ba9f011902426f5352613d9ac562e56c
Roly Perera <roly.perera@dynamicaspects.org>
2023-11-10 18:54:14 +0000

    🧩 [remove-unused]: fig1, fig2.

--- src/PLDI2024.purs
+++ src/PLDI2024.purs
@@ -1,7 +1,7 @@
 module PLDI2024 where
 
 import Prelude
-import App.Fig (LinkedOutputsFigSpec, drawLinkedOutputsFigs, loadLinkedOutputsFig, FigSpec)
+import App.Fig (LinkedOutputsFigSpec, drawLinkedOutputsFigs, loadLinkedOutputsFig)
 import Effect (Effect)
 import Module (File(..))
 
@@ -12,27 +12,6 @@ waterFig =
    , file2: File "water-ratio-chart"
    , dataFile: File "water-consumption-data"
    , x: "data"
-   }
-
---   { divId: "fig-1"
---   , file1: File "bar-chart"
---   , file2: File "line-chart"
---   , dataFile: File "renewables"
---   , x: "data"
---   }
-
-fig1 :: FigSpec
-fig1 =
-   { divId: "fig-conv-1"
-   , file: File "slicing/convolution/emboss"
-   , xs: [ "image", "filter" ]
-   }
-
-fig2 :: FigSpec
-fig2 =
-   { divId: "fig-conv-2"
-   , file: File "slicing/convolution/emboss-wrap"
-   , xs: [ "image", "filter" ]
    }
 
 main :: Effect Unit
