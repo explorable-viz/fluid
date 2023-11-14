6bdb3275c90ecf142648f9e92b3fc0fc3efd5858
Roly Perera <roly.perera@dynamicaspects.org>
2023-11-14 15:07:01 +0000

    🧩 [consolidate]: Combine app tests into tests.

--- test/App.purs
+++ test/App.purs
@@ -4,10 +4,8 @@ import Prelude
 
 import App (fig1, fig2)
 import App.Fig (FigSpec, LinkedOutputsFigSpec, loadFig, loadLinkedOutputsFig)
-import Effect (Effect)
 import Effect.Aff (Aff)
 import Test.Specs (linkedOutputs_spec1)
-import Test.Util.Mocha (run)
 import Util (type (×), (×))
 
 -- For now just exercise figure creation code; will test via UI at some point.
@@ -17,8 +15,5 @@ test_fig spec = spec.divId × void (loadFig spec)
 test_linkedOutputsFig :: LinkedOutputsFigSpec -> String × Aff Unit
 test_linkedOutputsFig spec = spec.divId × void (loadLinkedOutputsFig spec)
 
-tests :: Array (String × Aff Unit)
-tests = [ test_fig fig1, test_fig fig2, test_linkedOutputsFig linkedOutputs_spec1.spec ]
-
-main :: Effect Unit
-main = run tests
+app_tests :: Array (String × Aff Unit)
+app_tests = [ test_fig fig1, test_fig fig2, test_linkedOutputsFig linkedOutputs_spec1.spec ]
