ddeeed459719709bf1b1d893f752ab4d2fc84ac6
Roly Perera <roly.perera@dynamicaspects.org>
2023-11-28 16:39:31 +0000

    🧩 [add-unused]: relatedInputs/Outputs; not used yet.

--- src/GaloisConnection.purs
+++ src/GaloisConnection.purs
@@ -17,6 +17,12 @@ deMorgan = (neg >>> _) >>> (_ >>> neg)
 dual :: forall a b. Neg a => Neg b => GaloisConnection a b -> GaloisConnection b a
 dual (GC { fwd, bwd }) = GC { fwd: deMorgan bwd, bwd: deMorgan fwd }
 
+relatedInputs :: forall a b. Neg a => Neg b => GaloisConnection a b -> GaloisConnection a a
+relatedInputs gc = gc >>> dual gc
+
+relatedOutputs :: forall a b. Neg a => Neg b => GaloisConnection a b -> GaloisConnection b b
+relatedOutputs gc = gc <<< dual gc
+
 instance Semigroupoid GaloisConnection where
    compose (GC { fwd: fwd1, bwd: bwd1 }) (GC { fwd: fwd2, bwd: bwd2 }) =
       GC { fwd: fwd1 <<< fwd2, bwd: bwd1 >>> bwd2 }
