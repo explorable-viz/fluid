74532f180f1014fc4fce13b19a8c520d193a897d
Joe Bond <jecb1998@googlemail.com>
2023-12-07 11:57:49 +0000

    Modified energy data script so a user can generate the fluid datasets themselves from a csv for artifact evaluation

--- energy_data.py
+++ energy_data.py
@@ -1,42 +1,64 @@
 import pandas as pd
+import os
 
-data = pd.read_csv("~/Downloads/yearly_full_release_long_format.csv")
-data.fillna(0, inplace=True)
+energy_data = pd.read_csv("~/Downloads/yearly_full_release_long_format.csv")
+energy_data.fillna(0, inplace=True)
+gdp_data = pd.read_csv("~/Downloads/API_NY.GDP.PCAP.CD_DS2_en_csv_v2_5994720.csv", header=2)
+gdp_data.fillna(0, inplace=True)
 years = [2013, 2014, 2015, 2016, 2017, 2018]
 countries = ['Brazil', 'China', 'Germany', 'Egypt', 'United States of America']
+prefix = os.path.expanduser("~/Documents/fluid/fluid/example/linked-inputs/")
+non_renewables_str = "["
 
-print("Non-Renewables Data")
+print("Non-Renewables energy_data")
 for year in years:
+    non_renewables_str += "\n"
     for country in countries:
-        nuclear = data.loc[(data['Area'] == country) & (data['Category'] == 'Capacity') & (data['Year'] == year) & (data['Variable'] == 'Nuclear')]['Value'].values[0]
-        gas = data.loc[(data['Area'] == country) & (data['Category'] == 'Capacity') & (data['Year'] == year) & (data['Variable'] == 'Gas')]['Value'].values[0]
-        coal = data.loc[(data['Area'] == country) & (data['Category'] == 'Capacity') & (data['Year'] == year) & (data['Variable'] == 'Coal')]['Value'].values[0]
-        petrol = data.loc[(data['Area'] == country) & (data['Category'] == 'Capacity') & (data['Year'] == year) & (data['Variable'] == 'Other Fossil')]['Value'].values[0]
-        carbInt = data.loc[(data['Area'] == country) & (data['Subcategory'] == 'CO2 intensity') & (data['Year'] == year)]['Value'].values[0]
-        row = '{ country: \"' + country + '\", year: ' + str(year) +', nuclear: '+ str(nuclear) + ', gas: ' + str(gas) +', coal: ' + str(coal) + ', petrol: ' + str(petrol) + ', carbonInt: ' + str(carbInt) + '},'
+        nuclear = energy_data.loc[(energy_data['Area'] == country) & (energy_data['Category'] == 'Capacity') & (energy_data['Year'] == year) & (energy_data['Variable'] == 'Nuclear')]['Value'].values[0]
+        gas = energy_data.loc[(energy_data['Area'] == country) & (energy_data['Category'] == 'Capacity') & (energy_data['Year'] == year) & (energy_data['Variable'] == 'Gas')]['Value'].values[0]
+        coal = energy_data.loc[(energy_data['Area'] == country) & (energy_data['Category'] == 'Capacity') & (energy_data['Year'] == year) & (energy_data['Variable'] == 'Coal')]['Value'].values[0]
+        petrol = energy_data.loc[(energy_data['Area'] == country) & (energy_data['Category'] == 'Capacity') & (energy_data['Year'] == year) & (energy_data['Variable'] == 'Other Fossil')]['Value'].values[0]
+        carbInt = energy_data.loc[(energy_data['Area'] == country) & (energy_data['Subcategory'] == 'CO2 intensity') & (energy_data['Year'] == year)]['Value'].values[0]
+
+        # Horrible hack but we have to do it:
+        gdp_country = country
+        gdp_country = gdp_country.replace("United States of America", "United States")
+        gdp_country = gdp_country.replace("Egypt", "Egypt, Arab Rep.")
+        gdp = '%.3f'%(gdp_data.loc[(gdp_data['Country Name'] == gdp_country)][str(year)].values[0] / 1000)
+        row = '   { country: \"' + country + '\", year: ' + str(year) +', nuclear: '+ str(nuclear) + ', gas: ' + str(gas) +', coal: ' + str(coal) + ', petrol: ' + str(petrol) + ', carbonInt: ' + str(carbInt) + ', gdpPerCap: ' + gdp + '},'
         row = row.replace("United States of America", "USA")
-        print(row)
-    print()
+        non_renewables_str += row
+        non_renewables_str += "\n"
+non_renewables_str += "]"
+print(non_renewables_str)
 
-print("Renewables Data")
+with open(prefix + 'non-renewables.fld', 'w') as f:
+    f.write(non_renewables_str)
 
+print("Renewables energy_energy_data")
+renewables_str = "["
 for year in years:
     for country in countries:
-        bio = data.loc[(data['Area'] == country) & (data['Category'] == 'Capacity') & (data['Year'] == year) & (data['Variable'] == 'Bioenergy')]['Value'].values[0]
-        hydro = data.loc[(data['Area'] == country) & (data['Category'] == 'Capacity') & (data['Year'] == year) & (data['Variable'] == 'Hydro')]['Value'].values[0]
-        solar = data.loc[(data['Area'] == country) & (data['Category'] == 'Capacity') & (data['Year'] == year) & (data['Variable'] == 'Solar')]['Value'].values[0]
-        wind = data.loc[(data['Area'] == country) & (data['Category'] == 'Capacity') & (data['Year'] == year) & (data['Variable'] == 'Wind')]['Value'].values[0]
-        biorow = '{ year: ' + str(year) + ', country: \"' + country +'\", energyType: \"Bio\", output: ' + str(bio) + '},'
+        renewables_str += "\n"
+        bio = energy_data.loc[(energy_data['Area'] == country) & (energy_data['Category'] == 'Capacity') & (energy_data['Year'] == year) & (energy_data['Variable'] == 'Bioenergy')]['Value'].values[0]
+        hydro = energy_data.loc[(energy_data['Area'] == country) & (energy_data['Category'] == 'Capacity') & (energy_data['Year'] == year) & (energy_data['Variable'] == 'Hydro')]['Value'].values[0]
+        solar = energy_data.loc[(energy_data['Area'] == country) & (energy_data['Category'] == 'Capacity') & (energy_data['Year'] == year) & (energy_data['Variable'] == 'Solar')]['Value'].values[0]
+        wind = energy_data.loc[(energy_data['Area'] == country) & (energy_data['Category'] == 'Capacity') & (energy_data['Year'] == year) & (energy_data['Variable'] == 'Wind')]['Value'].values[0]
+        biorow = '   { year: ' + str(year) + ', country: \"' + country +'\", energyType: \"Bio\", output: ' + str(bio) + '},\n'
         biorow = biorow.replace("United States of America", "USA")
-        hydrorow = '{ year: ' + str(year) + ', country: \"' + country +'\", energyType: \"Hydro\", output: ' + str(hydro) + '},'
+        hydrorow = '   { year: ' + str(year) + ', country: \"' + country +'\", energyType: \"Hydro\", output: ' + str(hydro) + '},\n'
         hydrorow = hydrorow.replace("United States of America", "USA")
-        solarrow = '{ year: ' + str(year) + ', country: \"' + country +'\", energyType: \"Solar\", output: ' + str(solar) + '},'
+        solarrow = '   { year: ' + str(year) + ', country: \"' + country +'\", energyType: \"Solar\", output: ' + str(solar) + '},\n'
         solarrow = solarrow.replace("United States of America", "USA")
-        windrow = '{ year: ' + str(year) + ', country: \"' + country +'\", energyType: \"Wind\", output: ' + str(wind) + '},'
+        windrow = '   { year: ' + str(year) + ', country: \"' + country +'\", energyType: \"Wind\", output: ' + str(wind) + '},\n'
         windrow = windrow.replace("United States of America", "USA")
         
-        print(biorow)
-        print(hydrorow)
-        print(solarrow)
-        print(windrow)
-        print()
+        renewables_str += biorow
+        renewables_str += hydrorow
+        renewables_str += solarrow
+        renewables_str += windrow
+renewables_str += "]"
+
+print(renewables_str)
+with open(prefix + 'renewables.fld', 'w') as f:
+    f.write(renewables_str)
