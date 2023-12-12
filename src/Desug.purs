3967f9878e9541b2b42df8346fd451c8d5e05201
Roly Perera <roly.perera@dynamicaspects.org>
2023-12-12 13:02:20 +0000

    🧩 [refactor]: Push 'expand' behaviour into desugar/bwd for now.

--- src/Desug.purs
+++ src/Desug.purs
@@ -7,18 +7,24 @@ import Desugarable (desug, desugBwd)
 import Effect.Exception (Error)
 import Expr (Expr)
 import GaloisConnection (GaloisConnection(..))
-import Lattice (class BoundedLattice, Raw)
+import Lattice (class BoundedLattice, Raw, expand)
 import SExpr (Expr) as S
 import Util (successful)
+
+-- Core-language slicing can produce "partial" slices, but these are not (yet) tolerated by desugaring.
+type Desugaring a =
+   { gc :: GaloisConnection (S.Expr a) (Expr a)
+   , e :: Raw Expr -- original (non-partial) desugared expression
+   }
 
 desugGC
    :: forall a m
     . MonadError Error m
    => BoundedLattice a
    => Raw S.Expr
-   -> m (GaloisConnection (S.Expr a) (Expr a))
-desugGC s0 = do
-   let
-      fwd s = successful $ desug s
-      bwd e = desugBwd e s0
-   pure $ GC { fwd, bwd }
+   -> m (Desugaring a)
+desugGC s = pure $ { gc: GC { fwd, bwd }, e }
+   where
+   e = successful $ desug s
+   fwd s' = successful $ desug s'
+   bwd e' = desugBwd (expand e' e) s
