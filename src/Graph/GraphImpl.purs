d6eb60f999669817acb2786475f5b547a85f4ff7
Roly Perera <roly.perera@dynamicaspects.org>
2023-12-14 12:04:18 +0000

    🧩 [refactor]: Verify that, although there _are_ in fact duplicate inetries in the edge list, they are always equivalent.

--- src/Graph/GraphImpl.purs
+++ src/Graph/GraphImpl.purs
@@ -89,13 +89,14 @@ outMap α_αs = do
    addEdges :: List (Vertex × NonEmptySet Vertex) × MutableAdjMap _ -> ST _ _
    addEdges (Nil × acc) = pure $ Done acc
    addEdges (((Vertex α × βs) : rest) × acc) = do
-      _ <- OST.peek α acc <#> case _ of
-         Nothing -> true
-         Just βs' -> S.isEmpty βs'
-      if true then do
+      βs' <- OST.peek α acc <#> case _ of
+         Nothing -> S.empty
+         Just βs' -> βs' \\ toSet βs
+      if S.isEmpty βs' then do
          acc' <- OST.poke α (toSet βs) acc >>= flip (foldM addIfMissing) βs
          pure $ Loop (rest × acc')
-      else error $ "Duplicate key " <> α
+      else
+         error $ "Losing edges to " <> show βs'
 
 inMap :: List (Vertex × NonEmptySet Vertex) -> forall r. ST r (MutableAdjMap r)
 inMap α_αs = do
