e434a2865eff182964a34bd0ad4c85705e82879c
Roly Perera <roly.perera@dynamicaspects.org>
2023-12-14 13:37:36 +0000

    ❗ [incomplete]: Focus on 'range' test to see if I can get insight into duplicate edge info.

--- test/Test.purs
+++ test/Test.purs
@@ -2,33 +2,25 @@ module Test.Test where
 
 import Prelude hiding (add)
 
-import App.Util.Select (constr)
 import Data.Array (concat)
 import Data.Profunctor.Strong (second)
-import DataType (cSome)
 import Effect (Effect)
 import Effect.Aff (Aff)
-import Lattice (neg)
 import Test.App (app_tests)
 import Test.Benchmark (benchmarks)
 import Test.Specs (linkedInputs_cases, linkedOutputs_cases)
 import Test.Util.Mocha (run)
-import Test.Util.Suite (BenchSuite, bwdSuite, linkedInputsSuite, linkedOutputsSuite)
+import Test.Util.Suite (BenchSuite, linkedInputsSuite, linkedOutputsSuite, suite)
 import Util (type (×), (×))
 
 main :: Effect Unit
-main = run tests
+--main = run tests
 
---main = run scratchpad
+main = run scratchpad
 
 scratchpad :: TestSuite
-scratchpad = asTestSuite $ bwdSuite
-   [ { file: "lookup"
-     , imports: []
-     , bwd_expect_file: "lookup.expect"
-     , δv: constr cSome neg
-     , fwd_expect: "⸨Some \"Germany\"⸩"
-     }
+scratchpad = asTestSuite $ suite
+   [ { file: "range", imports: [], fwd_expect: "((0, 0) : ((0, 1) : ((1, 0) : ((1, 1) : []))))" }
    ]
 
 type TestSuite = Array (String × Aff Unit)
