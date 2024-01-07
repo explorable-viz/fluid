64f6f55518e1919becd7b829e5736256e4c6b5d4
Roly Perera <roly.perera@dynamicaspects.org>
2024-01-07 11:02:35 +0000

    🧩 [consolidate]: Simplify initialConfig.

--- src/Module.purs
+++ src/Module.purs
@@ -82,11 +82,11 @@ datasetAs file x (ProgCxt r@{ datasets }) = do
 initialConfig :: forall m a. MonadError Error m => FV a => a -> Raw ProgCxt -> m GraphConfig
 initialConfig e progCxt = do
    when checking.allocRoundTrip $ alloc_check "progCxt" (alloc progCxt)
-   n' × _ × progCxt' <- runAllocT (alloc progCxt) 0
-   n × _ × γ <- flip runAllocT n' do
+   n × _ × progCxt' × γ <- flip runAllocT 0 do
+      progCxt' <- alloc progCxt
       let αs = vertices progCxt'
       _ × γ <- runWithGraphT (eval_progCxt progCxt') αs :: AllocT m (GraphImpl × _)
       -- Restrict γ derived from prog cxt to free vars for managability, although this precludes mapping back
       -- to surface syntax for now, and no easy way to similarly restrict inputs of corresponding graph.
-      pure (γ `restrict` (fv e))
+      pure (progCxt' × γ `restrict` (fv e))
    pure { n, progCxt: progCxt', γ }
