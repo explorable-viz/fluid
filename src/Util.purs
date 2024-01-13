77cd0c0b6faac69cd9dcba5c5b3129877028cfea
Roly Perera <roly.perera@dynamicaspects.org>
2024-01-13 15:10:22 +0000

    🔧 [meta]: Fiddle with logging settings.

--- src/Util.purs
+++ src/Util.purs
@@ -39,8 +39,8 @@ debug
       }
 
 debug =
-   { logging: false
-   , tracing: false
+   { logging: true
+   , tracing: true
    }
 
 type Thunk a = Unit -> a -- similar to Lazy but without datatype
