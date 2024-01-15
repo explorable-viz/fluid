ff555a0e0e2cb78e49b9b45cf1ae7cab7acf4c3a
Roly Perera <roly.perera@dynamicaspects.org>
2024-01-15 16:31:50 +0000

    ❗ [incomplete]: More fixes to paths/variables.

--- test/Specs.purs
+++ test/Specs.purs
@@ -307,8 +307,8 @@ linkedOutputs_spec1' =
         { divId: "fig-1"
         , datasets: [ "renewables" ↦ "example/linked-outputs/renewables" ]
         , imports: []
-        , file: File "bar-chart-line-chart"
-        , inputs: [ "renewables_data" ]
+        , file: File "linked-outputs/bar-chart-line-chart"
+        , inputs: [ "renewables" ]
         }
    , δ_out: constrArg cMultiPlot 0
         (dictVal "bar chart" (constrArg cBarChart 0 (field f_data (listElement 1 (field f_y neg)))))
