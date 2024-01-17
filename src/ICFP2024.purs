5b875025dd172e050e6982e3cc899e9520d724f8
JosephBond <jecb1998@googlemail.com>
2024-01-17 13:49:24 +0000

    Added selectionless spec for testing/diagrams

--- src/ICFP2024.purs
+++ src/ICFP2024.purs
@@ -4,12 +4,12 @@ import Prelude
 
 import App.Fig (drawFig, runAffs_)
 import Effect (Effect)
-import Test.Specs (linkedInputs_spec3, linkedInputs_spec4, linkedInputs_spec5)
+import Test.Specs (linkedInputs_spec4, linkedInputs_spec5, linkedInputs_spec_no_sel)
 import Test.Util.Suite (linkedInputsTest)
 
 main :: Effect Unit
 main = runAffs_ drawFig $ linkedInputsTest <$>
-   [ linkedInputs_spec3
+   [ linkedInputs_spec_no_sel
    , linkedInputs_spec4
    , linkedInputs_spec5
    ]
