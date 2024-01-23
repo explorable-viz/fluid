6d9ddc49c5c4beb8ed92d857ccab7bc5417030b6
Roly Perera <roly.perera@dynamicaspects.org>
2024-01-23 15:12:29 +0000

    Gah, don't know where to set timeout...

--- karma.conf.tests.js
+++ karma.conf.tests.js
@@ -3,7 +3,7 @@ module.exports = function (config) {
       autoWatch: true,
       basePath: "",
       browsers: ["ChromeHeadlessNoSandbox"],
-      browserDisconnectTimeout: 60000,
+      browserDisconnectTimeout: 120000,
       customLaunchers: {
          ChromeHeadlessNoSandbox: {
            base: 'ChromeHeadless',
@@ -15,7 +15,7 @@ module.exports = function (config) {
       },
       client: {
          mocha: {
-            timeout: 60000
+            timeout: 120000
          }
       },
       colors: true,
