f01cbe8f2902cce4c3cad030181b1d30ce46253e
JosephBond <jecb1998@googlemail.com>
2024-02-22 15:48:06 +0000

    Added a second linked outputs example to the performance testing

--- test/Util/Suite.purs
+++ test/Util/Suite.purs
@@ -66,8 +66,8 @@ bwdSuite specs (n × is_bench) = specs <#> ((_.file >>> ("slicing/" <> _)) &&& a
    folder = File "slicing/"
 
    asTest :: TestBwdSpec -> Aff BenchRow
-   asTest { imports, file, bwd_expect_file, δv, fwd_expect } = do
-      gconfig <- loadProgCxt imports []
+   asTest { imports, file, bwd_expect_file, δv, fwd_expect, datasets } = do
+      gconfig <- loadProgCxt imports datasets
       bwd_expect <- loadFile (Folder "fluid/example") (folder <> File bwd_expect_file)
       test (folder <> File file) gconfig { δv, fwd_expect, bwd_expect } (n × is_bench)
 
