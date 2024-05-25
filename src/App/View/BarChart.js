8a121767733f767c565a0c9d486f493f3b750dcc
Roly Perera <roly.perera@dynamicaspects.org>
2024-05-25 08:54:17 +0100

    ❗ [modify]: Update selection state on existing chart rather than re-rendering each time -- fixes problem with spurious mouseevents. Mouseover enabled for bar charts so far.

--- src/App/View/BarChart.js
+++ src/App/View/BarChart.js
@@ -10,12 +10,11 @@ function tickEvery (n) {
       : 10 ** m
 }
 
-function setSelectionState ({ bar_fill, bar_stroke }, stacks, selData) {
-   // Be careful. Arrow functions don't have their own "this".
+function setSelectionState ({ bar_fill, bar_stroke }, chart, selData) {
    const color = d3.scaleOrdinal(d3.schemeAccent)
-   stacks.selectAll('rect').each(function (d) {
+   chart.selectAll('.bar').each(function (d) {
       const sel = selData[d.i][d.j]
-      d3.select(this)
+      d3.select(this) // won't work inside arrow function :/
          .attr('fill', bar => { return bar_fill(sel)(color(bar.j)) })
          .attr('stroke', bar => { return bar_stroke(sel)(color(bar.j)) })
    })
@@ -37,133 +36,134 @@ function drawBarChart_ (
    listener
 ) {
    return () => {
-//      const { bar_fill, bar_stroke } = barChartHelpers
       const childId = divId + '-' + suffix
       const margin = {top: 15, right: 75, bottom: 40, left: 40},
             width = 275 - margin.left - margin.right,
             height = 185 - margin.top - margin.bottom
       const div = d3.select('#' + divId)
+      const chart = div.selectAll('#' + childId)
 
-      div.selectAll('#' + childId).remove()
+      if (!chart.empty()) {
+         setSelectionState(barChartHelpers, chart, selData)
+      } else {
+         const chart = div
+            .append('svg')
+               .attr('width', width + margin.left + margin.right)
+               .attr('height', height + margin.top + margin.bottom)
+            .attr('id', childId)
 
-      const svg = div
-         .append('svg')
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom)
-         .attr('id', childId)
-         .append('g')
+         chart
+            .append('g')
             .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
-      // x-axis
-      const x = d3.scaleBand()
-         .range([0, width])
-         .domain(data.map(d => val(d.x)))
-         .padding(0.2)
-      svg.append('g')
-         .attr('transform', "translate(0," + height + ")")
-         .call(d3.axisBottom(x))
-         .selectAll('text')
-            .style('text-anchor', 'middle')
+         // x-axis
+         const x = d3.scaleBand()
+            .range([0, width])
+            .domain(data.map(d => val(d.x)))
+            .padding(0.2)
+         chart.append('g')
+            .attr('transform', "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
+            .selectAll('text')
+               .style('text-anchor', 'middle')
 
-      function barHeight (bars) {
-         return bars.reduce((acc, bar) => { return val(bar.z) + acc }, 0)
-      }
-      // y-axis
-      const nearest = 100,
-            y_max = Math.ceil(Math.max(...data.map(d => barHeight(d.bars))) / nearest) * nearest
-      const y = d3.scaleLinear()
-         .domain([0, y_max])
-         .range([height, 0])
-      const tickEvery_n = tickEvery(y_max),
-            ticks = Array.from(Array(Math.ceil(y_max / tickEvery_n + 1)).keys()).map(n => n * tickEvery_n)
-      const yAxis = d3.axisLeft(y)
-         .tickValues(ticks)
-      svg.append('g')
-         .call(yAxis)
+         function barHeight (bars) {
+            return bars.reduce((acc, bar) => { return val(bar.z) + acc }, 0)
+         }
+         // y-axis
+         const nearest = 100,
+               y_max = Math.ceil(Math.max(...data.map(d => barHeight(d.bars))) / nearest) * nearest
+         const y = d3.scaleLinear()
+            .domain([0, y_max])
+            .range([height, 0])
+         const tickEvery_n = tickEvery(y_max),
+               ticks = Array.from(Array(Math.ceil(y_max / tickEvery_n + 1)).keys()).map(n => n * tickEvery_n)
+         const yAxis = d3.axisLeft(y)
+            .tickValues(ticks)
+         chart.append('g')
+            .call(yAxis)
 
-      // bars
-      const stacks = svg.selectAll('.stack')
-         .data([...data.entries()])
-         .enter()
-         .append('g')
-      const color = d3.scaleOrdinal(d3.schemeAccent)
-      const strokeWidth = 1
+         // bars
+         const stacks = chart.selectAll('.stack')
+            .data([...data.entries()])
+            .enter()
+            .append('g')
+         const color = d3.scaleOrdinal(d3.schemeAccent)
+         const strokeWidth = 1
 
-      mouseenterEnabled = true
-      stacks.selectAll('.bar')
-         .data(([i, {x, bars}]) => bars.slice(1).reduce((acc, bar) => {
-            const prev = acc[acc.length - 1]
-            const y = prev.y + prev.height
-            acc.push({i, j: prev.j + 1, x: val(x), y, height: val(bar.z), sel: selState(bar.z)})
-            return acc
-         }, [{i, j: 0, x: val(x), y: 0, height: val(bars[0].z), sel: selState(bars[0].z)}]))
-         .enter()
-         .append('rect')
-            .attr('x', bar => { return x(bar.x) })
-            .attr('y', bar => { return y(bar.y + bar.height) })
-            .attr('width', x.bandwidth())
-            .attr('height', bar => { return height - y(bar.height) - strokeWidth }) // stop bars overplotting
-//            .attr('fill', bar => { return bar_fill(bar.sel)(color(bar.j)) })
-            .attr('stroke-width', _ => strokeWidth)
-//            .attr('stroke', bar => { return bar_stroke(bar.sel)(color(bar.j)) })
-            .on('mousedown', (e, d) => { listener(e) })
-            .on('mouseleave', (e, d) => {
-               console.log(`${e.type}`)
-               listener(e)
-            })
-            .on('mouseenter', (e, d) => {
-               if (mouseenterEnabled) {
+         stacks.selectAll('.bar')
+            .data(([i, {x, bars}]) => bars.slice(1).reduce((acc, bar) => {
+               const prev = acc[acc.length - 1]
+               const y = prev.y + prev.height
+               acc.push({i, j: prev.j + 1, x: val(x), y, height: val(bar.z)})
+               return acc
+            }, [{i, j: 0, x: val(x), y: 0, height: val(bars[0].z)}]))
+            .enter()
+            .append('rect')
+               .attr('class', 'bar')
+               .attr('x', bar => { return x(bar.x) })
+               .attr('y', bar => { return y(bar.y + bar.height) })
+               .attr('width', x.bandwidth())
+               .attr('height', bar => { return height - y(bar.height) - strokeWidth }) // stop bars overplotting
+               .attr('stroke-width', _ => strokeWidth)
+               .on('mousedown', (e, d) => { listener(e) })
+               .on('mouseleave', (e, d) => {
                   console.log(`${e.type}`)
                   listener(e)
-               }
-               mouseenterEnabled = !mouseenterEnabled
-            })
+               })
+               .on('mouseenter', (e, d) => {
+                  console.log(`${e.type}`)
+                  listener(e)
+               })
 
-      setSelectionState(barChartHelpers, stacks, selData)
+         // TODO: enforce that all stacked bars have same set of segments
+         const legendLineHeight = 15,
+               legendStart = width + margin.left / 2
+               names = data[0].bars.map(bar => val(bar.y))
+         chart
+            .append('rect')
+            .attr('transform', `translate(${legendStart}, ${height / 2 - margin.top - 2})`)
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('stroke', 'lightgray')
+            .attr('fill', 'none')
+            .attr('height', legendLineHeight * names.length)
+            .attr('width', margin.right - 22)
 
-      // TODO: enforce that all stacked bars have same set of segments
-      const legendLineHeight = 15,
-            legendStart = width + margin.left / 2
-            names = data[0].bars.map(bar => val(bar.y))
-      svg.append('rect')
-         .attr('transform', `translate(${legendStart}, ${height / 2 - margin.top - 2})`)
-         .attr('x', 0)
-         .attr('y', 0)
-         .attr('stroke', 'lightgray')
-         .attr('fill', 'none')
-         .attr('height', legendLineHeight * names.length)
-         .attr('width', margin.right - 22)
+         const legend = chart.selectAll('legend')
+            .data(names)
+            .enter()
+            .append('g')
+            .attr('class', 'legend')
+            .attr('transform', (d, i) =>
+               `translate(${legendStart}, ${height / 2 - margin.top + i * legendLineHeight})`
+            )
 
-      const legend = svg.selectAll('legend')
-         .data(names)
-         .enter()
-         .append('g')
-         .attr('class', 'legend')
-         .attr('transform', (d, i) =>
-            `translate(${legendStart}, ${height / 2 - margin.top + i * legendLineHeight})`
-         )
+         legend.append('text')
+            .text(d => d)
+            .attr('font-size', 11)
+            .attr('transform', 'translate(15, 9)') // align text with boxes
 
-      legend.append('text')
-         .text(d => d)
-         .attr('font-size', 11)
-         .attr('transform', 'translate(15, 9)') // align text with boxes
+         const legendSquareSize = 4
 
-      const legendSquareSize = 4
+         legend.append('rect')
+            .attr('fill', d => color(names.indexOf(d)))
+            .attr('width', legendSquareSize)
+            .attr('height', legendSquareSize)
+            .attr('x', legendLineHeight / 2 - legendSquareSize / 2)
+            .attr('y', legendLineHeight / 2 - legendSquareSize)
 
-      legend.append('rect')
-         .attr('fill', d => color(names.indexOf(d)))
-         .attr('width', legendSquareSize)
-         .attr('height', legendSquareSize)
-         .attr('x', legendLineHeight / 2 - legendSquareSize / 2)
-         .attr('y', legendLineHeight / 2 - legendSquareSize)
+         chart
+            .append('text')
+            .text(val(caption))
+            .attr('x', width / 2)
+            .attr('y', height + 35)
+            .attr('class', 'title-text')
+            .attr('dominant-baseline', 'bottom')
+            .attr('text-anchor', 'middle')
 
-      svg.append('text')
-         .text(val(caption))
-         .attr('x', width / 2)
-         .attr('y', height + 35)
-         .attr('class', 'title-text')
-         .attr('dominant-baseline', 'bottom')
-         .attr('text-anchor', 'middle')
+         setSelectionState(barChartHelpers, chart, selData)
+      }
    }
 }
 
