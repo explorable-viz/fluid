dc58302d7985a7c050ac6af05fd6670c4eaf201e
Roly Perera <roly.perera@dynamicaspects.org>
2024-06-19 08:19:13 +0100

    🧩 [refactor]: Load URLs relative to root of server.

--- script/publish.sh
+++ script/publish.sh
@@ -8,4 +8,5 @@ yarn build-app
 
 # hardcode to renewables examples first of all
 # will this pick up CSS (probably not) and Fluid files (maybe)?
-rm -rf dist/renewables && mkdir -p dist/renewables && cp web/template.html dist/renewables/index.html
+# depends on app being compiled to dist/app
+rm -rf dist/app/renewables && mkdir -p dist/app/renewables && cp web/template.html dist/app/renewables/index.html
