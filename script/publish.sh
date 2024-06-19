0af3da6e563b7d13f935ef40083d2a304b6983a7
Roly Perera <roly.perera@dynamicaspects.org>
2024-06-19 11:12:16 +0100

    🧩 [refactor]: Generalise to arbitrary Publish module.

--- script/publish.sh
+++ script/publish.sh
@@ -5,18 +5,18 @@ set -e
 
 # hardcode to renewables examples first of all
 
-NAME="HereIsAName"
+NAME="Renewables"
 NAME_LISP_CASE=$(echo $NAME | sed 's/\([A-Z]\)/-\1/g' | tr 'A-Z' 'a-z' | sed 's/^-//')
 echo $NAME_LISP_CASE
 
 # turn into npm script?
-rm -rf dist/app/renewables
-mkdir -p dist/app/renewables
+rm -rf dist/app/$NAME_LISP_CASE
+mkdir -p dist/app/$NAME_LISP_CASE
 yarn spago build --purs-args '--strict --censor-codes=UserDefinedWarning'
-yarn purs-backend-es bundle-app --main Publish.Renewables --to dist/app/renewables/app.js
+yarn purs-backend-es bundle-app --main Publish.$NAME --to dist/app/$NAME_LISP_CASE/app.js
 
 # shared by all standalone figures
 cp -r fluid dist/app
 cp -r web/css dist/app
 
-cp web/template.html dist/app/renewables/index.html
+cp web/template.html dist/app/$NAME_LISP_CASE/index.html
