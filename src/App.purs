84d6f0c5038b51b7218b5416032c117254d7b4f3
Joe Bond <jecb1998@googlemail.com>
2024-07-19 14:53:55 +0100

    working out an example program to help illustrate the idea of the project

--- src/App.purs
+++ src/App.purs
@@ -4,6 +4,7 @@ import Prelude hiding (absurd)
 
 import App.Fig (FigSpec, drawFig, drawFile, loadFig)
 import App.Util (runAffs_)
+import Bind ((↦))
 import Data.Tuple (uncurry)
 import Effect (Effect)
 import Module (File(..), Folder(..), loadFile')
@@ -35,16 +36,25 @@ fig3 =
    , inputs: [ "input_image" ]
    }
 
+fig4 :: FigSpec
+fig4 = { datasets: [ "renewables" ↦ "example/linked-outputs/renewables" ]
+       , imports: []
+       , file: File "graphics/small-barchart"
+       , inputs: [ "renewables" ]
+       }
+
 main :: Effect Unit
 main = do
    runAffs_ drawFile
-      [ loadFile' (Folder "fluid/example/linked-outputs") (File "bar-chart-line-chart")
+      [ loadFile' (Folder "fluid/example/graphics") (File "small-barchart")
+      , loadFile' (Folder "fluid/example/linked-outputs") (File "bar-chart-line-chart")
       , loadFile' (Folder "fluid/example/linked-outputs") (File "renewables")
       , loadFile' (Folder "fluid/lib") (File "convolution")
       , loadFile' (Folder "fluid/example/slicing/convolution") (File "emboss-wrap")
       ]
    runAffs_ (uncurry drawFig)
-      [ ("fig-4" × _) <$> loadFig energyScatter
+      [ ("fig-small-barchart" × _) <$> loadFig fig4
+      , ("fig-4" × _) <$> loadFig energyScatter
       , ("fig-conv-2" × _) <$> loadFig fig2
       , ("fig-1" × _) <$> loadFig linkedOutputs_spec1.spec
       ]
