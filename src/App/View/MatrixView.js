d946022db2849cd6b0876b03988d68a391a90243
hafniz <42380178+hafniz@users.noreply.github.com>
2024-07-22 12:32:40 +0100

    Restructure groups in svg matrix; add highlight borders for each cell

--- src/App/View/MatrixView.js
+++ src/App/View/MatrixView.js
@@ -46,6 +46,8 @@ function drawMatrix_ (
       const { val } = uiHelpers
       const childId = divId + '-' + suffix
       const strokeWidth = 0.5
+      const highlightStrokeWidth = 1.5
+      const highlightStrokeColor = 'blue'
       const w = 30, h = 30
 
       const div = d3.select('#' + divId)
@@ -54,7 +56,7 @@ function drawMatrix_ (
          return
       }
 
-      const [width, height] = [w * matrix.j + strokeWidth, h * matrix.i + strokeWidth]
+      const [width, height] = [w * matrix.j + highlightStrokeWidth, h * matrix.i + highlightStrokeWidth]
       const hMargin = w / 2
       const vMargin = h / 2
 
@@ -68,45 +70,82 @@ function drawMatrix_ (
          rootElement
             .attr('width', width + hMargin)
             .attr('height', height + vMargin)
-
-         // group for each row
-         const grp = rootElement
-            .selectAll('g')
-            .data([...matrix.cells.entries()].map(([i, ns]) => { return { i: i + 1, ns } }))
-            .enter()
+         
+         // outer group containing all rows
+         const outerGrp = rootElement
             .append('g')
-            .attr(
-               'transform',
-               _ => `translate(${strokeWidth / 2 + hMargin / 2}, ${strokeWidth / 2 + vMargin})`
-            )
+            .attr('transform', `translate(${strokeWidth / 2 + hMargin / 2}, ${strokeWidth / 2 + vMargin})`)            
             // these will be inherited by text elements
             .attr('fill', 'currentColor')
             .attr('stroke', 'currentColor')
             .attr('stroke-width', '.25') // otherwise setting stroke makes it bold
+         
+         // group for each row
+         const grp = outerGrp
+            .selectAll('g')
+            .data([...matrix.cells.entries()].map(([i, ns]) => { return { i: i + 1, ns } }))
+            .enter()
+            .append('g')
 
          const cells = grp
             .selectAll('rect')
             .data(({ i, ns }) => [...ns.entries()].map(([j, n]) => { return { i, j: j + 1, n } }))
             .enter()
 
-         cells
-            .append('rect')
-            .classed('matrix-cell', true)
-            .attr('x', ({j}) => w * (j - 1))
-            .attr('y', ({i}) => h * (i - 1))
-            .attr('width', w)
-            .attr('height', h)
-            .attr('stroke-width', strokeWidth)
+         cells.each(function ({i, j, n}) {
+            const cell = d3.select(this);
+            const x = w * (j - 1);
+            const y = h * (i - 1);
 
-         cells
-            .append('text')
-            .text(({ n }) => val(n))
-            .attr('x', ({j}) => (j - 0.5) * w)
-            .attr('y', ({i}) => (i - 0.5) * h)
-            .attr('class', 'matrix-cell-text')
-            .attr('text-anchor', 'middle')
-            .attr('dominant-baseline', 'middle')
-            .attr('pointer-events', 'none')
+            cell.append('rect')
+               .classed('matrix-cell', true)
+               .attr('x', x)
+               .attr('y', y)
+               .attr('width', w)
+               .attr('height', h)
+               .attr('stroke-width', strokeWidth)
+            
+            cell.append('text')
+               .text(val(n))
+               .attr('x', (j - 0.5) * w)
+               .attr('y', (i - 0.5) * h)
+               .attr('class', 'matrix-cell-text')
+               .attr('text-anchor', 'middle')
+               .attr('dominant-baseline', 'middle')
+               .attr('pointer-events', 'none')
+
+            cell.append('line') // top border
+               .attr('x1', x)
+               .attr('y1', y)
+               .attr('x2', x + w)
+               .attr('y2', y)
+               .attr('stroke-width', highlightStrokeWidth)
+               .attr('stroke', highlightStrokeColor);
+
+            cell.append('line') // right border
+               .attr('x1', x + w)
+               .attr('y1', y)
+               .attr('x2', x + w)
+               .attr('y2', y + h)
+               .attr('stroke-width', highlightStrokeWidth)
+               .attr('stroke', highlightStrokeColor);
+
+            cell.append('line') // bottom border
+               .attr('x1', x + w)
+               .attr('y1', y + h)
+               .attr('x2', x)
+               .attr('y2', y + h)
+               .attr('stroke-width', highlightStrokeWidth)
+               .attr('stroke', highlightStrokeColor);
+
+            cell.append('line') // left border
+               .attr('x1', x)
+               .attr('y1', y + h)
+               .attr('x2', x)
+               .attr('y2', y)
+               .attr('stroke-width', highlightStrokeWidth)
+               .attr('stroke', highlightStrokeColor);
+         })
 
          rootElement.append('text')
             .text(title)
