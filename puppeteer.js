812a84dc5549b4b0696ce490481871e4b32ec5f7
hanaizakim <hi266@cam.ac.uk>
2024-07-31 14:04:00 +0100

    Added js-promise-aff to packages.dhall

--- puppeteer.js
+++ puppeteer.js
@@ -10,15 +10,35 @@ app.use(serve(__dirname + '/dist/app'));
 
 const server = app.listen(8080, function(){
   console.log("Server running");
-}).withShutdown();
-
-/*
-server.listen(8080, () => {
-  console.log('Server started');
-});
-*/
+}).withShutdown();    
 
 (async () => {
+  try {
+    await import('./output-es/Test.Puppeteer/index.js').then(({ main }) => {
+      main();
+    }).catch(err => {
+      console.error("Failed to load PureScript output:", err);
+    });
+  } catch (error) {
+    console.error('Error:', error);
+  }
+
+  console.log('Shutting down server')
+  await server.shutdown(function(err) {
+    if (err) {
+      return console.log('shutdown failed', err.message);
+    }
+    console.log('Everything is cleanly shutdown.');
+  });
+  
+})();
+
+    
+
+      
+    /*
+
+  (async () => {
   try {
     console.log('Launching browser')
     const browser = await puppeteer.launch();
@@ -28,21 +48,18 @@ server.listen(8080, () => {
     console.log(content);
     await browser.close();
     console.log("Browser closed");
+    
+
   } catch (error) {
     console.error('Error:', error);
   }
   console.log('Shutting down server')
-  server.shutdown(function(err) {
+  await server.shutdown(function(err) {
     if (err) {
       return console.log('shutdown failed', err.message);
     }
     console.log('Everything is cleanly shutdown.');
   });
 })();
-
-
-
-
-
-
+*/
   
