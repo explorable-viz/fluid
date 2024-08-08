627bf2bfda27969361b09a788a2178342e1a2ad7
hanaizakim <hi266@cam.ac.uk>
2024-08-08 15:01:10 +0100

    Removed yarn spago install aff-promise from pre-push

--- puppeteer.js
+++ puppeteer.js
@@ -35,28 +35,3 @@ function serverDown()
   });
   
 }
-/*
-(async () => {
-  try {
-    console.log('Launching browser')
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://127.0.0.1:8080');
-    const content = await page.content();
-    console.log(content);
-    await browser.close();
-    console.log("Browser closed");
-    
-
-  } catch (error) {
-    console.error('Error:', error);
-  }
-  console.log('Shutting down server')
-  await server.shutdown(function(err) {
-    if (err) {
-      return console.log('shutdown failed', err.message);
-    }
-    console.log('Everything is cleanly shutdown.');
-  });
-})();
-*/
