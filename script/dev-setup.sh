c15ca879f64fac7f91ccc21735356a54eb5bb6b5
Roly Perera <roly.perera@dynamicaspects.org>
2024-08-12 10:39:48 +0100

    🔧 [meta]: pre-push: abort if locally modified files.

--- script/dev-setup.sh
+++ script/dev-setup.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 # run from project root
-
-# This will install:
-#   - various [Git aliases](/.gitconfig.include) to classify different kinds of commit
-#   - pre-push hook that ensures files are formatted
-
 # set -x
 set -e
 
-git config --local include.path "../.gitconfig.include"
-cp script/git/hooks/pre-push .git/hooks/
+git config --local include.path "../.gitconfig.include" # install Git aliases
+./script/install-hooks.sh
