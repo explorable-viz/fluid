54d2d7045328080e5a937e1710c10145cd0350a7
hafniz <42380178+hafniz@users.noreply.github.com>
2024-08-13 16:34:17 +0100

    code cleanup

--- src/App/View/MatrixView.purs
+++ src/App/View/MatrixView.purs
@@ -24,31 +24,31 @@ matrixBorderStyles West = "filter: drop-shadow(-1px 0px 1px blue);"
 matrixBorderStyles None = "visibility: hidden;"
 
 hBorderStyles :: IntMatrix -> MatrixBorderCoordinate -> String
-hBorderStyles m c = matrixBorderStyles $ hBorderShadowDirection m c
+hBorderStyles m = matrixBorderStyles <<< hBorderShadowDirection m
 
 hBorderShadowDirection :: IntMatrix -> MatrixBorderCoordinate -> ShadowDirection
-hBorderShadowDirection { cells, i: matrixH, j: _ } { i, j } = case i of
-   0 -> if isCellTransient cells { i, j: j - 1 } then South else None
-   _ | i == matrixH -> if isCellTransient cells { i: i - 1, j: j - 1 } then North else None
-   _ ->
-      if isCellTransient cells { i, j: j - 1 } && (not isCellTransient cells { i: i - 1, j: j - 1 }) then South
-      else if (not isCellTransient cells { i, j: j - 1 }) && isCellTransient cells { i: i - 1, j: j - 1 } then North
-      else None
+hBorderShadowDirection { cells, i: height } { i, j }
+   | i == 0 = if isCellTransient cells { i, j: j - 1 } then South else None
+   | i == height = if isCellTransient cells { i: i - 1, j: j - 1 } then North else None
+   | otherwise =
+        if isCellTransient cells { i, j: j - 1 } && (not isCellTransient cells { i: i - 1, j: j - 1 }) then South
+        else if (not isCellTransient cells { i, j: j - 1 }) && isCellTransient cells { i: i - 1, j: j - 1 } then North
+        else None
 
 vBorderStyles :: IntMatrix -> MatrixBorderCoordinate -> String
-vBorderStyles m c = matrixBorderStyles $ vBorderShadowDirection m c
+vBorderStyles m = matrixBorderStyles <<< vBorderShadowDirection m
 
 vBorderShadowDirection :: IntMatrix -> MatrixBorderCoordinate -> ShadowDirection
-vBorderShadowDirection { cells, i: _, j: matrixW } { i, j } = case j of
-   0 -> if isCellTransient cells { i: i - 1, j } then East else None
-   _ | j == matrixW -> if isCellTransient cells { i: i - 1, j: j - 1 } then West else None
-   _ ->
-      if isCellTransient cells { i: i - 1, j } && (not isCellTransient cells { i: i - 1, j: j - 1 }) then East
-      else if (not isCellTransient cells { i: i - 1, j }) && isCellTransient cells { i: i - 1, j: j - 1 } then West
-      else None
+vBorderShadowDirection { cells, j: width } { i, j }
+   | j == 0 = if isCellTransient cells { i: i - 1, j } then East else None
+   | j == width = if isCellTransient cells { i: i - 1, j: j - 1 } then West else None
+   | otherwise =
+        if isCellTransient cells { i: i - 1, j } && not isCellTransient cells { i: i - 1, j: j - 1 } then East
+        else if not isCellTransient cells { i: i - 1, j } && isCellTransient cells { i: i - 1, j: j - 1 } then West
+        else None
 
 isCellTransient :: forall a. Array2 (Selectable a) -> MatrixCellCoordinate -> Boolean
-isCellTransient arr2d { i, j } = isTransient $ snd $ (arr2d ! i) ! j
+isCellTransient arr2d { i, j } = isTransient $ snd $ arr2d ! i ! j
 
 foreign import drawMatrix :: MatrixHelpers -> Renderer MatrixView Unit
 
