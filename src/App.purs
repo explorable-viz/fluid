8e90cdb0238b3a3df7caf7a50a3d9c7b4d795298
Joe Bond <jecb1998@googlemail.com>
2024-08-21 10:51:09 +0100

    Think this has fixed the puppeteer stuff

--- src/App.purs
+++ src/App.purs
@@ -11,7 +11,6 @@ import Module (File(..), Folder(..), loadFile')
 import Test.Specs.LinkedInputs (energyScatter)
 import Test.Specs.LinkedOutputs (linkedOutputs_spec1)
 import Util ((×))
-import Bind ((↦))
 
 fig2 :: FigSpec
 fig2 =
@@ -37,26 +36,16 @@ fig3 =
    , inputs: [ "input_image" ]
    }
 
-fig4 :: FigSpec
-fig4 =
-   { datasets: [ "renewables" ↦ "example/linked-outputs/renewables" ]
-   , imports: []
-   , file: File "text"
-   , inputs: [ "renewables" ]
-   }
-
 main :: Effect Unit
 main = do
    runAffs_ drawFile
-      [ loadFile' (Folder "fluid/example/graphics") (File "small-barchart")
-      , loadFile' (Folder "fluid/example/linked-outputs") (File "bar-chart-line-chart")
+      [ loadFile' (Folder "fluid/example/linked-outputs") (File "bar-chart-line-chart")
       , loadFile' (Folder "fluid/example/linked-outputs") (File "renewables")
       , loadFile' (Folder "fluid/lib") (File "convolution")
       , loadFile' (Folder "fluid/example/slicing/convolution") (File "emboss-wrap")
       ]
    runAffs_ (uncurry drawFig)
-      [ ("fig-text" × _) <$> loadFig fig4
-      , ("fig-4" × _) <$> loadFig energyScatter
+      [ ("fig-4" × _) <$> loadFig energyScatter
       , ("fig-conv-2" × _) <$> loadFig fig2
       , ("fig-1" × _) <$> loadFig linkedOutputs_spec1.spec
       ]
