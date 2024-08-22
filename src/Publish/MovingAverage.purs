45498b5fd56b5fdc4e8a71564b2fcb7f77890c5e
Joe Bond <jecb1998@googlemail.com>
2024-08-22 13:05:54 +0100

    Skeleton of new moving average example. Had to add a slice function to the prelude.

---
+++ src/Publish/MovingAverage.purs
@@ -0,0 +1,21 @@
+module Publish.MovingAverage where
+
+import Prelude hiding (absurd)
+
+import App.Fig (drawFig, loadFig)
+import App.Util (runAffs_)
+import App.View.Util (FigSpec)
+import Data.Tuple (uncurry)
+import Effect (Effect)
+import Module (File(..))
+import Util ((×))
+
+fig :: FigSpec
+fig = { datasets: [ ]
+      , imports: [ ]
+      , file: File "/linked-outputs/movingaverage"
+      , inputs: [ ]   
+}
+
+main :: Effect Unit
+main = runAffs_ (uncurry drawFig) [ ("fig" × _) <$> loadFig fig]
