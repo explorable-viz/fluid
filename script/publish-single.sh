cd65f899cbf8fb6176bb9470dd43b2f83c25b37c
Roly Perera <roly.perera@dynamicaspects.org>
2024-08-24 14:48:57 +0100

    🧩 [doc] : Bit tricky to factor publish-single.sh through build.sh/clean.sh and retain the shared CSS files, so leave that for now.

--- script/publish-single.sh
+++ script/publish-single.sh
@@ -3,9 +3,11 @@
 # set -x
 set -e
 
-# shared by all standalone figures
+# Could factor this through build.sh but currently these are shared by all standalone figures
 cp -r fluid dist/app
 cp -r web/css dist/app
+
+cp web/template.html dist/app/$NAME_LISP_CASE/index.html
 
 echo $1
 NAME=$(basename $1 .purs)
@@ -16,5 +18,3 @@ rm -rf dist/app/$NAME_LISP_CASE
 mkdir -p dist/app/$NAME_LISP_CASE
 yarn spago build --purs-args '--strict --censor-codes=UserDefinedWarning'
 yarn purs-backend-es bundle-app --main Publish.$NAME --to dist/app/$NAME_LISP_CASE/app.js
-
-cp web/template.html dist/app/$NAME_LISP_CASE/index.html
