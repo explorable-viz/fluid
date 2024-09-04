0ad2b60164f456ad05f4aaf1bfba11ad141494c6
Roly Perera <roly.perera@dynamicaspects.org>
2024-09-04 10:10:27 +0100

    🔧 [meta]: waitFor always reports.

--- test/Puppeteer.purs
+++ test/Puppeteer.purs
@@ -2,63 +2,24 @@ module Test.Puppeteer where
 
 import Prelude
 
-import Control.Promise (Promise, fromAff, toAffE)
+import Control.Promise (Promise, fromAff)
 import Data.Foldable (sequence_)
 import Effect (Effect)
-import Effect.Aff (Aff, catchError)
-import Effect.Class (class MonadEffect)
+import Effect.Aff (Aff)
 import Effect.Class.Console (log)
 import Foreign (unsafeFromForeign)
 import Test.Util (testCondition)
+import Test.Util.Puppeteer (click, goto, launchFirefox, show', waitFor)
 import Toppokki as T
-import Util (debug)
-
-foreign import _launchFirefox :: Effect (Promise T.Browser)
-
-launchFirefox :: Aff T.Browser
-launchFirefox = toAffE _launchFirefox
-
-show' :: T.Selector -> String
-show' (T.Selector sel) = sel
-
-timeout :: Int
-timeout = 60000
-
-waitFor :: T.Selector -> T.Page -> Aff Unit
-waitFor selector page = do
-   log' ("Waiting for " <> show' selector)
-   catchError
-      ( do
-           void $ T.pageWaitForSelector selector { timeout, visible: true } page
-           log' "-> found"
-      )
-      \e ->
-         testCondition (show' selector) false (show e)
-
-waitForHidden :: T.Selector -> T.Page -> Aff Unit
-waitForHidden selector page = do
-   log' ("Waiting for " <> show' selector)
-   void $ T.pageWaitForSelector selector { timeout, visible: false } page
-   log' "-> found"
-
-puppeteerLogging :: Boolean
-puppeteerLogging = false
-
-log' :: forall m. MonadEffect m => String -> m Unit
-log' msg = when (debug.logging || puppeteerLogging) (log msg)
 
 main :: Effect (Promise Unit)
 main = fromAff $ sequence_ tests
 
 tests :: Array (Aff Unit)
 tests =
-   [ browserTests "firefox" (launchFirefox)
+   [ browserTests "chrome" (T.launch {})
+   , browserTests "firefox" (launchFirefox)
    ]
-
-goto :: T.URL -> T.Page -> Aff Unit
-goto (T.URL url) page = do
-   log' ("Going to " <> show url)
-   T.goto (T.URL url) page
 
 -- Test each fig on a fresh page, else earlier tests seem to interfere with element visibility (on Firefox)
 browserTests :: String -> Aff T.Browser -> Aff Unit
@@ -88,13 +49,14 @@ testScatterPlot page = do
 
    clickScatterPlotPoint :: Aff Unit
    clickScatterPlotPoint = do
-      let selector = T.Selector ("div#" <> fig <> " .scatterplot-point")
-      waitFor selector page
-      void $ T.click selector page
-      className <- getAttributeValue page selector "class"
-      radius <- getAttributeValue page selector "r"
+      let point = T.Selector ("div#" <> fig <> " .scatterplot-point")
+      waitFor point page
+      click point page
+      className <- getAttributeValue page point "class"
       let expectedClass = "scatterplot-point selected-primary-persistent selected-primary-transient"
-      testCondition (show' selector) (className == expectedClass && radius == "3.2") "class and radius"
+      testCondition (show' point) (className == expectedClass) "class"
+      radius <- getAttributeValue page point "r"
+      testCondition (show' point) (radius == "3.2") "radius"
       let caption = T.Selector ("table#" <> fig <> "-input-renewables > caption.table-caption")
       checkTextContent page caption "renewables (4 of 240)"
 
@@ -116,7 +78,7 @@ testBarChartLineChart page = do
    clickBarChart = do
       let bar = T.Selector ("svg#" <> barChart <> " rect.bar")
       waitFor bar page
-      void $ T.click bar page
+      click bar page
       fill <- getAttributeValue page bar "fill"
       testCondition (show' bar) (fill == "#57a157") "fill"
 
@@ -136,14 +98,12 @@ waitForFigure :: T.Page -> String -> Aff Unit
 waitForFigure page id = do
    let selector = T.Selector ("svg#" <> id)
    waitFor selector page
-   testCondition (show' selector) true "exists"
 
 clickToggle :: T.Page -> String -> Aff Unit
 clickToggle page id = do
-   let selector = T.Selector ("div#" <> id <> " + div > div > span.toggle-button")
-   waitFor selector page
-   log' ("Clicking " <> show' selector)
-   void $ T.click selector page
+   let toggle = T.Selector ("div#" <> id <> " + div > div > span.toggle-button")
+   waitFor toggle page
+   click toggle page
 
 checkTextContent :: T.Page -> T.Selector -> String -> Aff Unit
 checkTextContent page selector expected = do
