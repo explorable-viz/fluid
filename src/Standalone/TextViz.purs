3bc0409312fb4274c05578a310cdfb884aad9046
Joe Bond <jecb1998@googlemail.com>
2024-09-06 13:48:28 +0100

    advanced text first explanation function

--- src/Standalone/TextViz.purs
+++ src/Standalone/TextViz.purs
@@ -5,7 +5,7 @@ import Prelude hiding (absurd)
 import App.Fig (drawFig, loadFig)
 import App.Util (runAffs_)
 import App.View.Util (FigSpec)
-import Bind ((↦))
+-- import Bind ((↦))
 import Data.Tuple (uncurry)
 import Effect (Effect)
 import Module (File(..))
@@ -13,10 +13,10 @@ import Util ((×))
 
 fig :: FigSpec
 fig =
-   { datasets: [ "renewables" ↦ "example/linked-outputs/renewables" ]
-   , imports: [ "lib/nombre" ]
-   , file: File "text"
-   , inputs: [ "renewables" ]
+   { datasets: [ ]
+   , imports: [  ]
+   , file: File "advanced-text"
+   , inputs: [ ]
    }
 
 main :: Effect Unit
