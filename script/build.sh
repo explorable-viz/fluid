ecd92d128acf86432a4d7d765f43e74be8e5367c
Roly Perera <roly.perera@dynamicaspects.org>
2024-09-19 10:27:15 +0100

    🔧 [meta]: Explicit 'yarn'.

--- script/build.sh
+++ script/build.sh
@@ -3,5 +3,5 @@ set -xe
 
 yarn tidy
 . script/clean.sh $1
-spago build --purs-args '--strict --censor-codes=UserDefinedWarning'
-purs-backend-es bundle-app --main $2 --to dist/$1/app.js ${@:3}
+yarn spago build --purs-args '--strict --censor-codes=UserDefinedWarning'
+yarn purs-backend-es bundle-app --main $2 --to dist/$1/app.js ${@:3}
