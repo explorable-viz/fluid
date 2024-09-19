c37460d592550929968078ee85fe1352e0d04230
ColinC-UoE <s2092346@ed.ac.uk>
2024-09-19 05:02:56 +0100

    Resolved ENEEDAUTH from yarn npm-publish

--- script/npm-publish.sh
+++ script/npm-publish.sh
@@ -3,4 +3,5 @@
 set -xe #propagate errors to parent script
 
 yarn build-article
+yarn config set registry "https://registry.npmjs.org/" #yarn command sets registry to https://registry.yarnpkg.com; re-set to npm
 npm publish --access public
