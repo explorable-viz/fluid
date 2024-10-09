dae94e4b0ea36be22199ce46d53371df55f75531
Roly Perera <roly.perera@dynamicaspects.org>
2024-10-09 10:16:19 +0100

    ❗ [modify]: Better idea: switch to pastel scheme for now.

--- src/App/View/BarChart.js
+++ src/App/View/BarChart.js
@@ -77,9 +77,8 @@ function drawBarChart_ (
          return
       }
 
-      // const color = d3.scaleOrdinal(d3.schemeAccent)
-      // color(-1) // has side-effect of removing first color (green) from palette, which clashes with table highlighting
-      const color = d3.scaleOrdinal(d3.schemePastel1)
+      // const color = d3.scaleOrdinal(d3.schemeAccent) // has a green which clashes a bit with table highlighting
+      const color = d3.scaleOrdinal(d3.schemePastel2)
       let rootElement = div.selectAll('#' + childId)
 
       if (rootElement.empty()) {
