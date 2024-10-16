547b5e798b5405030d60d43451ac56709760e03b
Roly Perera <roly.perera@dynamicaspects.org>
2024-10-16 13:59:46 +0100

    ❗ [fix]: Quotes but all 'rest'.

--- script/build.sh
+++ script/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -xe
 
-echo $2
 . script/compile.sh
 . script/clean.sh $1
-. script/bundle.sh "$1" "$2" # quotes because $1 might be empty
+. script/bundle.sh "${@:1}" # quotes because $1 might be empty
