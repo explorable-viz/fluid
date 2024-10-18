2ce81340e02c0750b6e74bff61c05dfb37e8339b
Roly Perera <roly.perera@dynamicaspects.org>
2024-10-18 13:00:20 +0100

    🧩 [doc] : Record why puppeteer tests don't need separate build step.

--- script/test-standalone.sh
+++ script/test-standalone.sh
@@ -3,4 +3,7 @@ set -e
 
 yarn puppeteer browsers install chrome
 yarn puppeteer browsers install firefox
+
+# don't need to have "deployed" this to dist/
+# instead the following just picks up from output-es/
 node puppeteer.js Standalone.Test.$1
