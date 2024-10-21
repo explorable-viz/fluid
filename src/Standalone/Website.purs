471cb98bdb218190e5fdce24f9d9e20d64a79389
Roly Perera <roly.perera@dynamicaspects.org>
2024-10-21 16:18:54 +0100

    ❗ [modify]: Disable EnergyScatter test for now -- does seem to want to run locally or in GH Actions.

--- src/Standalone/Website.purs
+++ src/Standalone/Website.purs
@@ -2,14 +2,12 @@ module Standalone.Website where
 
 import Prelude hiding (absurd)
 
-import App.Fig (drawFig, loadFig)
+import App.Fig (drawFig)
 import App.Util (runAffs_)
 import Data.Tuple (uncurry)
 import Effect (Effect)
-import Standalone.EnergyScatter as EnergyScatter
-import Util ((×))
 
 main :: Effect Unit
 main = runAffs_ (uncurry drawFig)
-   [ ("fig-4" × _) <$> loadFig EnergyScatter.fig
+   [
    ]
