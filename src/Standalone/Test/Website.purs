a956abf057dfce731995253797f6ca2094e9bedd
Roly Perera <roly.perera@dynamicaspects.org>
2024-10-21 15:57:46 +0100

    🧩 [new-test]: Move website test to EnergyScatter (but doesn't run on my machine and seems to regularly fail in GitHub Actions).

--- src/Standalone/Test/Website.purs
+++ src/Standalone/Test/Website.purs
@@ -6,7 +6,7 @@ import Control.Promise (Promise, fromAff)
 import Data.Foldable (sequence_)
 import Effect (Effect)
 import Effect.Aff (Aff)
-import Test.Util.Puppeteer (checkAttribute, checkAttributeContains, checkTextContent, click, clickToggle, testURL, waitFor, waitForFigure)
+import Test.Util.Puppeteer (testURL)
 import Toppokki as T
 
 main :: Effect (Promise Unit)
@@ -15,22 +15,4 @@ main = fromAff $ sequence_ $ testURL "/"
    ]
 
 testFig :: T.Page -> Aff Unit
-testFig page = do
-   waitForFigure page (fig <> "-output")
-   let toggle = fig <> "-input"
-   clickToggle page toggle
-   waitFor (T.Selector ("div#" <> toggle)) page
-   clickScatterPlotPoint
-
-   where
-   fig = "fig-4"
-
-   clickScatterPlotPoint :: Aff Unit
-   clickScatterPlotPoint = do
-      let point = T.Selector ("div#" <> fig <> " .scatterplot-point")
-      waitFor point page
-      click point page
-      checkAttributeContains page point "class" "selected-primary-persistent"
-      checkAttribute page point "r" "3.2"
-      let caption = T.Selector ("table#" <> fig <> "-input-renewables > caption.table-caption")
-      checkTextContent page caption "renewables (40 of 240)"
+testFig _ = pure unit
