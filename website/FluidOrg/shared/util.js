9b96f5fac02d714ca9d1f4de21403a1b439645e8
Roly Perera <roly.perera@dynamicaspects.org>
2024-11-11 12:27:23 +0000

    🧩 [move]: Extract 'shared' folder for common headers/nav content.

--- website/FluidOrg/shared/util.js
+++ website/FluidOrg/shared/util.js
@@ -1,5 +1,18 @@
+function loadHeader () {
+   fetch('/shared/header.html')
+      .then(response => response.text())
+      .then(data => {
+         const header = document.createElement('div')
+         header.innerHTML = data
+         const divElement = header.children[0]
+         const grid = document.getElementById('grid')
+         grid.parentNode.insertBefore(divElement, grid)
+      })
+      .catch(error => console.error('Error loading shared HTML:', error))
+}
+
 function loadSubHeader (n) {
-   fetch('/sub-header.html')
+   fetch('/shared/sub-header.html')
       .then(response => response.text())
       .then(data => {
          const header = document.createElement('div')
@@ -15,25 +28,12 @@ function loadSubHeader (n) {
       .catch(error => console.error('Error loading shared HTML:', error))
 }
 
-function loadHeader () {
-   fetch('/header.html')
-      .then(response => response.text())
-      .then(data => {
-         const header = document.createElement('div')
-         header.innerHTML = data
-         const divElement = header.children[0]
-         const grid = document.getElementById('grid')
-         grid.parentNode.insertBefore(divElement, grid)
-      })
-      .catch(error => console.error('Error loading shared HTML:', error))
-}
-
 function toggleDataPane(gridId) {
    const grid = document.getElementById(gridId)
 
-   if (grid.classList.contains('data-pane-hidden')) {
-      grid.classList.remove('data-pane-hidden');
+   if (grid.classList.contains('data-pane-visible')) {
+      grid.classList.remove('data-pane-visible');
    } else {
-      grid.classList.add('data-pane-hidden');
+      grid.classList.add('data-pane-visible');
    }
 }
