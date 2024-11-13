001bb5f7839e87dc8a84c6cd0b0e9faae9cba324
Roly Perera <roly.perera@dynamicaspects.org>
2024-11-13 11:53:47 +0000

    🧩 [consolidate]: loadSubHeader no longer needs index of subheader page to consider 'active', computes it directly from URL.

--- website/FluidOrg/shared/util.js
+++ website/FluidOrg/shared/util.js
@@ -11,14 +11,28 @@ function loadHeader () {
       .catch(error => console.error('Error loading shared HTML:', error))
 }
 
+function eqPaths(path1, path2) {
+  return path1.replace(/\/+$/, '') === path2.replace(/\/+$/, '')
+}
+
 function loadSubHeader (n) {
    fetch('/shared/sub-header.html')
       .then(response => response.text())
       .then(data => {
          const header = document.createElement('div')
          header.innerHTML = data
+
          const listItems = header.querySelectorAll('li')
-         listItems[n].classList.add('active-page')
+
+         const n_ = Array.from(listItems).findIndex(li => {
+            const link = li.querySelector('a')
+            return link && eqPaths(link.getAttribute('href'), window.location.pathname)
+         })
+
+         if (n_ !== -1) {
+            listItems[n_].classList.add('active-page')
+         }
+
          const divElements = header.children
          const grid = document.getElementById('grid')
          for (let i = Math.min(3, divElements.length - 1); i >= 0; --i) {
