543580f29d1941a34ed26c878a96a4638dc0f6ca
Tia Shah <ts6311@kent.ac.uk>
2024-11-14 16:29:55 +0000

    Extend check and synth

--- src/TypeChecking.purs
+++ src/TypeChecking.purs
@@ -2,7 +2,7 @@ module TypeChecking where
 
 import Prelude
 
-import Data.Array (fromFoldable, singleton, foldl, find, elem)
+import Data.Array (fromFoldable, singleton, foldl, find, elem, findMap)
 import Data.List.NonEmpty (NonEmptyList(..), cons)
 import Data.NonEmpty (NonEmpty(..), (:|))
 import Data.Tuple (Tuple(..))
@@ -63,30 +63,18 @@ isValidType (TList ty) = isValidType ty
 
 -- LOOKUP TABLE FOR OPERATOR TYPES
 type OperatorType = {opTy :: Types, argTy :: Array Types}
-operatorTypes :: Var -> Maybe OperatorType
+operatorTypes :: Var -> Maybe (Array OperatorType)
 operatorTypes op = case op of
-      -- Int
-      "+" -> Just { opTy: TCons "Int", argTy: [TCons "Int", TCons "Int"] }
-      "-" -> Just { opTy: TCons "Int", argTy: [TCons "Int", TCons "Int"] }
-      "*" -> Just { opTy: TCons "Int", argTy: [TCons "Int", TCons "Int"] }
-      "/" -> Just { opTy: TCons "Int", argTy: [TCons "Int", TCons "Int"] }
-      ">" -> Just { opTy: TCons "Bool", argTy: [TCons "Int", TCons "Int"] }
-      ">=" -> Just { opTy: TCons "Bool", argTy: [TCons "Int", TCons "Int"] }
-      "<=" -> Just { opTy: TCons "Bool", argTy: [TCons "Int", TCons "Int"] }
-      "<" -> Just { opTy: TCons "Bool", argTy: [TCons "Int", TCons "Int"] }
-      "==" -> Just { opTy: TCons "Bool", argTy: [TCons "Int", TCons "Int"] }
-      -- Float
-      "+" -> Just { opTy: TCons "Float", argTy: [TCons "Float", TCons "Float"] }
-      "-" -> Just { opTy: TCons "Float", argTy: [TCons "Float", TCons "Float"] }
-      "*" -> Just { opTy: TCons "Float", argTy: [TCons "Float", TCons "Float"] }
-      "/" -> Just { opTy: TCons "Float", argTy: [TCons "Float", TCons "Float"] }
-      ">" -> Just { opTy: TCons "Bool", argTy: [TCons "Float", TCons "Float"] }
-      ">=" -> Just { opTy: TCons "Bool", argTy: [TCons "Float", TCons "Float"] }
-      "<=" -> Just { opTy: TCons "Bool", argTy: [TCons "Float", TCons "Float"] }
-      "<" -> Just { opTy: TCons "Bool", argTy: [TCons "Float", TCons "Float"] }
-      "==" -> Just { opTy: TCons "Bool", argTy: [TCons "Float", TCons "Float"] }
-      -- Str
-      "==" -> Just { opTy: TCons "Bool", argTy: [TCons "Str", TCons "Str"] }
+      -- Int, Float and Str
+      "+" -> Just [{ opTy: TCons "Int", argTy: [TCons "Int", TCons "Int"] }, { opTy: TCons "Float", argTy: [TCons "Float", TCons "Float"] }]
+      "-" -> Just [{ opTy: TCons "Int", argTy: [TCons "Int", TCons "Int"] }, { opTy: TCons "Float", argTy: [TCons "Float", TCons "Float"] }]
+      "*" -> Just [{ opTy: TCons "Int", argTy: [TCons "Int", TCons "Int"] }, { opTy: TCons "Float", argTy: [TCons "Float", TCons "Float"] }]
+      "/" -> Just [{ opTy: TCons "Int", argTy: [TCons "Int", TCons "Int"] }, { opTy: TCons "Float", argTy: [TCons "Float", TCons "Float"] }]
+      ">" -> Just [{ opTy: TCons "Bool", argTy: [TCons "Int", TCons "Int"] }, { opTy: TCons "Bool", argTy: [TCons "Float", TCons "Float"] }]
+      ">=" -> Just [{ opTy: TCons "Bool", argTy: [TCons "Int", TCons "Int"] }, { opTy: TCons "Bool", argTy: [TCons "Float", TCons "Float"] }]
+      "<=" -> Just [{ opTy: TCons "Bool", argTy: [TCons "Int", TCons "Int"] }, { opTy: TCons "Bool", argTy: [TCons "Float", TCons "Float"] }]
+      "<" -> Just [{ opTy: TCons "Bool", argTy: [TCons "Int", TCons "Int"] }, { opTy: TCons "Bool", argTy: [TCons "Float", TCons "Float"] }]
+      "==" -> Just [{ opTy: TCons "Bool", argTy: [TCons "Int", TCons "Int"] }, { opTy: TCons "Bool", argTy: [TCons "Float", TCons "Float"] }, { opTy: TCons "Bool", argTy: [TCons "Str", TCons "Str"] }]
       -- Undefined
       _ -> Nothing
 
@@ -97,23 +85,24 @@ allEqual t1 arr = foldl (\acc x -> acc && (x == t1)) true arr
 type Identifier = String
 type Context = Array (Tuple Identifier Types)
 
+-- data Expr a
+--    | Constr a Ctr (List (Expr a))
+--    | Record a (List (Bind (Expr a)))
+--    | Dictionary a (List (Pair (Expr a)))
+--    | Matrix a (Expr a) (Var × Var) (Expr a)
+--    | Lambda (Clauses a)
+--    | Project (Expr a) Var
+--    | App (Expr a) (Expr a)
+--    | MatchAs (Expr a) (NonEmptyList (Pattern × Expr a))
+--    | ListEnum (Expr a) (Expr a)
+--    | ListComp a (Expr a) (List (Qualifier a))
+--    | LetRec (RecDefs a) (Expr a)
+
 check :: forall a. Context -> Expr a -> Types -> Boolean
 check g (Int u n) (TCons "Int") = true
 check g (Str u s) (TCons "Str") = true
 check g (Float u n) (TCons "Float") = true
--- BinaryApp needs to check e1 and e2 are of the same type
-check g (BinaryApp e1 op e2) (TCons "Int") = 
-      case check g e1 (TCons "Int") of
-            true -> case check g e2 (TCons "Int") of
-                  true -> true
-                  _ -> false
-            _ -> false
-check g (BinaryApp e1 op e2) (TCons "Bool") = 
-      case check g e1 (TCons "Bool") of
-            true -> case check g e2 (TCons "Bool") of
-                  true -> true
-                  _ -> false
-            _ -> false
+check g (BinaryApp e1 op e2) ty = (synth g (BinaryApp e1 op e2)) == Just ty
 
 -- Var needs a lookup to see if it's in the context
 check g (Var varName) ty = case find (\(Tuple n t) -> n == varName) g of
@@ -124,7 +113,7 @@ check g (Var varName) ty = case find (\(Tuple n t) -> n == varName) g of
 check g (Let varDefs expr) ty = case varDefs of
       NonEmptyList (NonEmpty (VarDef pattern ty' val) Nil) -> case check g val ty' of
             true -> 
-                  if isValidType ty && ty == ty' then
+                  if isValidType ty then
                         case pattern of 
                               PVar varName ->
                                     let updatedG = pushVarDef g varName ty'
@@ -140,59 +129,88 @@ check g (Let varDefs expr) ty = case varDefs of
 
 -- The empty list
 check g (ListEmpty u) (TList _) = true 
-check _ _ _ = false
+-- NonEmpty List
+check g (ListNonEmpty u expr rest) (TList ty) = case check g expr ty of
+      true -> checkNonEmptyList rest ty
+      false -> false
+-- If else
+-- First expr needs to be Bool, the other 2 need to be the same type
+check g (IfElse e1 e2 e3) ty = 
+      if (check g e1 (TCons "Bool")) then
+            (synth g e2) == (synth g e3)
+      else
+            false
+-- Record data structures can have different types
+check g (Record _ exprs) = true
+check g expr ty = (synth g expr) == Just ty
 
-checkListNonEmpty :: forall a. Types -> Expr a -> Types -> Boolean
-checkListNonEmpty givenTy (ListNonEmpty _ expr rest) expectedTy = 
-      check [] expr givenTy && check [] expr expectedTy && checkRest rest givenTy expectedTy
-checkListNonEmpty _ (ListEmpty _) _ = true
-checkListNonEmpty _ _ _ = false
 
-checkRest :: forall a. ListRest a -> Types -> Types -> Boolean
-checkRest (End _) _ _ = true
-checkRest (Next _ nextElem rest) givenTy expectedTy =
-      check [] nextElem givenTy && check [] nextElem expectedTy && checkRest rest givenTy expectedTy
+checkNonEmptyList :: forall a. ListRest a -> Types -> Boolean
+checkNonEmptyList (End _) ty = true
+checkNonEmptyList (Next _ nextElem rest) ty = check [] nextElem ty && checkNonEmptyList rest ty
+
+
+synthRest :: forall a. Context -> ListRest a -> Types -> Maybe Types
+synthRest g (End _) expectedType = Just (TList expectedType)
+synthRest g (Next _ exp rest) expectedType = do
+  nextType <- synth g exp
+  if nextType == expectedType
+    then synthRest g rest expectedType
+    else Nothing
 
 
 pushVarDef :: Context -> String -> Types -> Context
 pushVarDef g varName varType = g <> singleton (Tuple varName varType)
 
-
 synth :: forall a. Context -> Expr a -> Maybe Types
 synth g (Int _ _) = Just (TCons "Int")
 synth g (Str _ _) = Just (TCons "Str")
 synth g (Float _ _) = Just (TCons "Float")
-synth g (BinaryApp e1 op e2) = 
-      case synth g e1 of
+synth g (BinaryApp e1 op e2) = case synth g e1 of
+      Nothing -> Nothing
+      Just t1 -> case synth g e2 of
             Nothing -> Nothing
-            Just t1 -> case synth g e2 of
+            Just t2 -> case operatorTypes op of
+                  Just operatorTypesArray -> 
+                        let 
+                              checkOperatorType :: OperatorType -> Maybe Types
+                              checkOperatorType { opTy, argTy } = if t1 == t2 && allEqual t1 argTy then Just opTy else Nothing
+                        in
+                              findMap checkOperatorType operatorTypesArray
                   Nothing -> Nothing
-                  Just t2 -> case operatorTypes op of
-                        Just {opTy, argTy} -> 
-                              if t1 == t2 && allEqual t1 argTy then
-                                    Just opTy
-                              else
-                                    Nothing
-                        Nothing -> Nothing
 synth g (Var varName) = case find (\(Tuple n t) -> n == varName) g of
       Just (Tuple _ t) -> Just t
       _ -> Nothing
 synth g (Let varDefs expr) = case varDefs of
-      NonEmptyList (NonEmpty (VarDef pattern ty' val) Nil) -> case synth g val of
-            Just valTy -> 
-                  if valTy == ty' then 
-                        case pattern of
-                              PVar varName -> 
-                                    let updatedG = pushVarDef g varName ty'
-                                    in synth updatedG expr
-                              PListEmpty -> Just ty'
-                              PConstr ctr _ -> Just ty'
-                              _ -> Nothing
-                  else
-                        Nothing
-            Nothing -> Nothing
+      NonEmptyList (NonEmpty (VarDef pattern ty' val) Nil) -> case check g val ty' of
+            true -> 
+                  case pattern of
+                        PVar varName -> 
+                              let updatedG = pushVarDef g varName ty'
+                              in synth updatedG expr
+                        PListEmpty -> synth g expr
+                        -- implement a function that does typing for pattern matches
+                        -- check pattern match has that type & binds variables in the process
+                        PConstr ctr _ -> synth g expr 
+                        _ -> Nothing
+            false -> Nothing
       _ -> Nothing
 synth g (ListEmpty _) = Just (TList (TCons "unknown"))
+synth g (ListNonEmpty _ exp rest) = do
+      headTy <- synth g exp
+      restTy <- synthRest g rest headTy
+      Just (TList (headTy))
+synth g (IfElse e1 e2 e3) =
+      if ((synth g e1) == Just (TCons "Bool")) then
+            do
+                  e2' <- synth g e2
+                  e3' <- synth g e3
+                  if (e2' == e3') then 
+                        Just e2'
+                  else
+                        Nothing
+      else
+            Nothing
 synth _ _ = Nothing
 
 
@@ -217,7 +235,6 @@ resultSynthInvalid = synth context'' (Var "x")
 
 exampleLet = check (singleton (Tuple "x" (TCons "Int"))) (Let (NonEmptyList (NonEmpty (VarDef (PVar "x") (TCons "Int") (Int unit 20)) Nil)) (Var "x")) (TCons "Int")
 exampleLetInvalid = check (singleton (Tuple "x" (TCons "Int"))) (Let (NonEmptyList (NonEmpty (VarDef (PVar "x") (TCons "Int") (Str unit "20")) Nil)) (Var "x")) (TCons "Int")
-
 
 -- Test function to check that the context is updated
 runTest :: Effect Unit
