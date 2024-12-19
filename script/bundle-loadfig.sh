14b13664cf55189cd2be3f314bf699ca89962e7d
Roly Perera <roly.perera@dynamicaspects.org>
2024-12-19 12:40:44 +0000

    🧩 [consolidate]: Let target directory be implicit in bundle-loadfig.sh.

--- script/bundle-loadfig.sh
+++ script/bundle-loadfig.sh
@@ -3,10 +3,10 @@
 set -xe
 
 echo "Processing shared files:"
-cp -r website/shared dist/fluid/shared
-cp -r website/font dist/fluid/font
-cp -r website/css dist/fluid/css
-cp -r website/image dist/fluid/image
-cp website/favicon.ico dist/fluid/favicon.ico
+cp -r website/shared dist/fluid
+cp -r website/font dist/fluid
+cp -r website/css dist/fluid
+cp -r website/image dist/fluid
+cp website/favicon.ico dist/fluid
 
 yarn purs-backend-es bundle-module -m Website.LoadFigure --to dist/fluid/load-fig.js
