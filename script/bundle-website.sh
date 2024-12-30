ce6a106bfe1e202082efb9b0320c6a8d5404437c
Roly Perera <roly.perera@dynamicaspects.org>
2024-12-30 17:04:08 +0000

    🧩 [remove-unused]: Unnecessary 'Processing shared files' step in bundle-load-figure.sh.

--- script/bundle-website.sh
+++ script/bundle-website.sh
@@ -47,10 +47,7 @@ for CHILD in "${TO_COPY[@]}"; do
 done
 
 echo "Processing load-figure.js:"
-ls -ld dist/$WEBSITE_LISP_CASE/shared
-mkdir -p dist/$WEBSITE_LISP_CASE/shared
 cp ${PREFIX}dist/fluid/load-figure.js dist/$WEBSITE_LISP_CASE/shared
-# or just bundle load-figure.js to $DIST/fluid/shared instead?
 
 cp -r fluid dist/$WEBSITE_LISP_CASE
 echo "Bundled website $WEBSITE"
