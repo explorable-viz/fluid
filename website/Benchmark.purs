aba64ec828814890f6ed7f9c00f53d3bd0ed0f43
Joe Bond <jecb1998@googlemail.com>
2025-01-08 11:53:13 +0000

    Moved LoadFigure to src/App, verified websites still work

--- website/Benchmark.purs
+++ website/Benchmark.purs
@@ -5,24 +5,30 @@ import Prelude
 import Data.Array (concat)
 import Data.Array.NonEmpty (fromArray)
 import Data.Traversable (sequence)
+import Data.Tuple (Tuple)
 import Effect (Effect)
-import Effect.Aff (launchAff_)
-import Effect.Class.Console (logShow)
-import Test.Benchmark.Util (BenchAcc(..))
+import Effect.Aff (Aff, launchAff_)
+import Effect.Class.Console (log, logShow)
+import Test.Benchmark.Util (BenchAcc(..), BenchRow)
 import Test.Specs.Bwd (bwd_cases)
 import Test.Specs.Desugar (desugar_cases)
 import Test.Specs.Graphics (graphics_cases)
 import Test.Specs.Misc (misc_cases)
 import Test.Util.Suite (BenchSuite, bwdSuite, suite, withDatasetSuite)
-import Util (definitely, (×))
+import Util (definitely, (×), type (×))
 
 -- Runs as webpage; would be nicer to use Mocha but that doesn't currently support returning values from tests.
 
 main :: Effect Unit
 main = launchAff_ do
    -- Mocha.run doesn't allow values to be returned from tests, so must run via HTML entrypoint
-   outs <- sequence $ (\(str × row) -> (str × _) <$> row) <$> (concat (benchmarks <@> (10 × true)))
+   outs <- sequence $ reportBench <$> (concat (benchmarks <@> (10 × true)))
    logShow $ BenchAcc $ definitely "More than one benchmark" $ fromArray outs
+
+reportBench :: String × (Aff BenchRow) -> Aff (Tuple String BenchRow)
+reportBench (str × row) = do
+   log $ "Benchmarking " <> str
+   (str × _) <$> row
 
 benchmarks :: Array BenchSuite
 benchmarks =
