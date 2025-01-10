a30ffeb075e7a866b6bd6b7d8b294f1970fd4fbb
Joe Bond <jecb1998@googlemail.com>
2025-01-10 12:45:55 +0000

    Dockerfile update regarding file permissions, rebuild_benchmark script works inside docker container

--- artifact/script/rebuild_benchmark.sh
+++ artifact/script/rebuild_benchmark.sh
@@ -1,7 +1,9 @@
-#!/bin bash
+#!/usr/env/bin bash
 set -xe
 
 sudo /usr/src/python/bin/python3 script/python/plot_bench.py -t paper -b table-one
 sudo /usr/src/python/bin/python3 script/python/plot_bench.py -t paper -b table-two
 sudo /usr/src/python/bin/python3 script/python/plot_bench.py -t paper -b table-two
-pdflatex benchmark/tex/benchmarks.tex
+pushd benchmark
+pushd tex
+sudo pdflatex benchmarks.tex
