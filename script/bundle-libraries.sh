c387295d5d1ae9f94a99138e20bb6b4a4fa7abcf
Joe Bond <jecb1998@googlemail.com>
2025-01-13 16:45:36 +0000

    bundle-libraries.sh needs --platform=node

--- script/bundle-libraries.sh
+++ script/bundle-libraries.sh
@@ -2,3 +2,4 @@
 set -xe
 
 yarn purs-backend-es bundle-module -m App.LoadFigure --to dist/fluid/load-figure.js
+yarn purs-backend-es bundle-module -m Test.Util.Puppeteer --to dist/fluid/puppeteer-lib.js --platform=node
