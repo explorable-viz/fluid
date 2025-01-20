238e36038735f970fedb2f5defcad3690326578a
Joe Bond <jecb1998@googlemail.com>
2025-01-20 14:09:10 +0000

    Documentation on the esbuild fix

--- script/bundle-libraries.sh
+++ script/bundle-libraries.sh
@@ -2,6 +2,9 @@
 set -xe
 
 ./script/util/bundle-module.sh load-figure.js App.LoadFigure
+# Comes from https://discourse.purescript.org/t/purescript-0-15-spago-affjax/3045/3
+# Using bundle-module.sh results in mixed module, which does not work well, therefore force puppeteer
+# to bundle to CommonJS module
 esbuild ./output-es/Test.Util.Puppeteer/index.js --bundle --platform=node > dist/fluid/shared/webtest-lib.js
 
 
