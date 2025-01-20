2cef6ea0640116c5fe1e5f1464eee43265b449e4
Joe Bond <jecb1998@googlemail.com>
2025-01-20 15:22:01 +0000

    Publishing website now copies test script to appropriate location

--- script/bundle-page.sh
+++ script/bundle-page.sh
@@ -28,3 +28,7 @@ if [[ -e "website/$SRC_PATH.json" ]]; then
    cp website/$SRC_PATH.html dist/$SRC_PATH_LISP_CASE/index.html
    cp website/$SRC_PATH.json dist/$SRC_PATH_LISP_CASE/spec.json
 fi
+
+if [[ -e "website/$SRC_PATH/test.mjs"]]; then
+   cp website/$SRC_PATH/test.mjs dist/SRC_PATH_LISP_CASE/test.mjs
+fi
