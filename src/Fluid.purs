c895c1f02a5aa7f720a51fb77b9845973e5edfc7
Roly Perera <roly.perera@dynamicaspects.org>
2025-01-22 13:50:44 +0000

    🧩 [refactor]: Simplify parseDatasets' to parsePair and inline.

--- src/Fluid.purs
+++ src/Fluid.purs
@@ -40,15 +40,10 @@ between p1 p2 f s =
       Nothing -> Left ("Expected " <> show p1 <> "..." <> show p2 <> " but got ...")
 
 parsePair :: String -> Either String (Bind String)
-parsePair = between (Pattern "(") (Pattern ")") $ \s -> do
+parsePair = between (Pattern "(") (Pattern ")") $ \s ->
    case split (Pattern ",") s of
       [ k, v ] -> Right (trim k ↦ trim v)
       _ -> Left $ "Expected a pair but got " <> s
-
-parseDataset' :: String -> Either String (Bind String)
-parseDataset' = \s -> do
-   dataset <- parsePair s
-   Right (dataset :: (Bind String))
 
 parseImports' :: Pattern -> Pattern -> (String -> Either String (Array String))
 parseImports' open close = between open close $ \s -> do
@@ -56,10 +51,10 @@ parseImports' open close = between open close $ \s -> do
 
 parseDatasets :: Parser (List (Bind String))
 parseDatasets =
-   many $ option (eitherReader $ parseDataset')
+   many $ option (eitherReader parsePair)
       ( long "datasets"
            <> short 'd'
-           <> help "A comma separated list of datasets"
+           <> help "Comma-separated list of datasets"
       )
 
 parseImports :: Parser (List String)
@@ -67,7 +62,7 @@ parseImports =
    many $ strOption
       ( long "imports"
            <> short 'i'
-           <> help "A comma separated list of import file locations"
+           <> help "Comma-separated list of files to import"
       )
 
 program :: Parser Program
