4ccfbbd51a405797b490c6e2a55839a035a07eb0
Roly Perera <roly.perera@dynamicaspects.org>
2025-02-05 15:19:13 +0000

    ❗ [fix]: Use of lisp-case with PREFIX_.

--- script/bundle-website.sh
+++ script/bundle-website.sh
@@ -11,8 +11,9 @@ while getopts "w:r:" opt; do
 done
 
 PREFIX_=${PREFIX:+$PREFIX/}
-
-WEBSITE_LISP_CASE=$(./$PREFIX/script/util/lisp-case.sh "$WEBSITE")
+pwd
+ls -la script/util/lisp-case.sh
+WEBSITE_LISP_CASE=$(. "${PREFIX_}script/util/lisp-case.sh" "$WEBSITE")
 echo "$WEBSITE -> $WEBSITE_LISP_CASE"
 echo "Cleaning dist/$WEBSITE_LISP_CASE"
 . "${PREFIX_}script/util/clean.sh" $WEBSITE_LISP_CASE
