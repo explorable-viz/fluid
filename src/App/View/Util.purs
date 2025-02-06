fd3d9afd45ea7bc2920c737318adf9287bb2c24a
Joe Bond <jecb1998@googlemail.com>
2025-02-06 10:56:15 +0000

    Don't expect trace in the FigSpec, but in the Fig itself

--- src/App/View/Util.purs
+++ src/App/View/Util.purs
@@ -16,7 +16,7 @@ import GaloisConnection (GaloisConnection)
 import Lattice (𝔹, Raw, (∨))
 import Module.Web (File, Folder)
 import SExpr as S
-import Trace (Trace, TraceTransformer)
+import Trace (TraceTransformer)
 import Util (type (×), Endo, Setter, check)
 import Val (Env, Val)
 import Web.Event.Event (EventType(..))
@@ -98,7 +98,7 @@ type FigSpec =
    , datasets :: Array (Bind String)
    , file :: File
    , inputs :: Array Var
-   , traceTransform :: Maybe (Trace × TraceTransformer 𝔹)
+   , traceTransform :: Maybe (TraceTransformer 𝔹)
    }
 
 data Direction = LinkedInputs | LinkedOutputs
