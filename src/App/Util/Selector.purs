6ac03da777c7ab5cfc1cddf72b9b0e7d48ef3bcd
Roly Perera <roly.perera@dynamicaspects.org>
2025-03-26 17:17:15 +0000

    Better constrArg'.

--- src/App/Util/Selector.purs
+++ src/App/Util/Selector.purs
@@ -135,10 +135,7 @@ constrArg c n δv = unsafePartial $ case _ of
 constrArg' :: Ctr -> Int -> SelSetter' Val Val
 constrArg' c n δv = unsafePartial $ case _ of
    Val α (Constr c' us) | c == c' ->
-      Val α (Constr c' us') × selType
-      where
-      us' × selType = definitely "constrArg out of bounds" $
-         first (\u' -> fromJust (updateAt n u' us)) <$> δv <$> (us !! n)
+      first (\u' -> Val α (Constr c' $ fromJust (updateAt n u' us))) (definitely "constrArg out of bounds" $ δv <$> (us !! n))
 
 constr :: Ctr -> Setter (Val (SelStates 𝔹)) 𝔹
 constr c' δα = unsafePartial $ case _ of
