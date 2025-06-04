43bfc26a07bb629faeff42f0dc532ac9718d1794
Roly Perera <roly.perera@dynamicaspects.org>
2025-06-04 11:32:09 +0100

    🧩 [consolidate]: Plug in new implementation.

--- src/App/View/BarChart.purs
+++ src/App/View/BarChart.purs
@@ -8,7 +8,7 @@ import Prelude hiding (absurd)
 
 import App.Util (class Reflect, SelStates, Selectable, 𝕊(..), colorShade, dict, from, getPersistent, getTransient, get_intOrNumber)
 import App.Util.Selector (ViewSelSetter, barChart, barSegment)
-import App.View.Util (class Drawable, class Drawable2, Renderer, UIHelpers, selListener, uiHelpers)
+import App.View.Util (class Drawable, class Drawable2, Renderer, UIHelpers, draw', selListener, uiHelpers)
 import App.View.Util.D3 as D3
 import Bind ((↦))
 import Data.Int (floor, pow, toNumber)
@@ -44,7 +44,8 @@ type BarChartHelpers =
    , tickEvery :: Int -> Int
    }
 
-foreign import createRootElement3 :: BarChartHelpers -> UIHelpers -> BarChart -> D3.Selection -> String -> Effect D3.Selection
+foreign import createRootElement2 :: BarChartHelpers -> UIHelpers -> BarChart -> D3.Selection -> String -> Effect D3.Selection
+foreign import setSelStates2 :: BarChartHelpers -> BarChart -> EventListener -> D3.Selection -> Effect Unit
 foreign import drawBarChart :: BarChartHelpers -> Renderer BarChart
 
 barChartHelpers :: BarChartHelpers
@@ -85,17 +86,14 @@ barChartHelpers =
       where
       m = floor (log (toNumber n) / log 10.0)
 
-setSelStates2 :: BarChart -> EventListener -> D3.Selection -> Effect Unit
-setSelStates2 _ _ _ =
-   pure unit
-
 instance Drawable2 BarChart where
-   createRootElement = createRootElement3 barChartHelpers uiHelpers
-   setSelStates = setSelStates2
+   createRootElement = createRootElement2 barChartHelpers uiHelpers
+   setSelStates = setSelStates2 barChartHelpers
 
 instance Drawable BarChart where
    draw rSpec figVal _ redraw =
-      drawBarChart barChartHelpers uiHelpers rSpec =<< selListener figVal redraw barSegment'
+      -- drawBarChart barChartHelpers uiHelpers rSpec =<< selListener figVal redraw barSegment'
+      draw' uiHelpers rSpec =<< selListener figVal redraw barSegment'
       where
       barSegment' :: ViewSelSetter BarSegmentCoordinate
       barSegment' { i, j } = barSegment i j >>> barChart
