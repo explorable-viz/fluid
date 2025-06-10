764e1763d633d9f4a0f3da272c7217d065216389
Joe Bond <jecb1998@googlemail.com>
2025-06-10 16:11:17 +0100

    Tidy up

--- src/App/View/Link.purs
+++ src/App/View/Link.purs
@@ -31,8 +31,8 @@ instance Drawable (Link (SelStates 𝕊)) where
 
 selLink :: ViewSelSetter (Link (SelStates 𝕊))
 selLink _ δv = unsafePartial $ case _ of
-      (Val α doc (Constr c (v1 : v2 : Nil))) | c == cLink ->
-         first (\v1' -> Val α doc (Constr c (v1' : v2 : Nil))) (δv v1)
+   (Val α doc (Constr c (v1 : v2 : Nil))) | c == cLink ->
+      first (\v1' -> Val α doc (Constr c (v1' : v2 : Nil))) (δv v1)
 
 createRootElement :: Link (SelStates 𝕊) -> D3.Selection -> Effect D3.Selection
 createRootElement link parent = do
--- src/App/View/MultiView.purs
+++ src/App/View/MultiView.purs
@@ -30,5 +30,5 @@ setSelStates' :: MultiView -> Select -> D3.Selection -> Effect Unit
 setSelStates' (MultiView views) select rootElement = do
    sequence_ $
       flip mapWithIndex (toUnfoldable views :: Array (String × View)) \i (x × view) -> do
-           elem <- rootElement # D3.select ("svg" <> D3.nthChild (i + 1))
-           void $ unpack view \v -> setSelStates v (multiViewEntry x >>> select) elem
+         elem <- rootElement # D3.select ("svg" <> D3.nthChild (i + 1))
+         void $ unpack view \v -> setSelStates v (multiViewEntry x >>> select) elem
