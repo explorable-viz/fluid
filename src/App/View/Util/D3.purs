41495d58cda469f441182e7f22855997a2515032
Joe Bond <jecb1998@googlemail.com>
2025-06-17 15:57:56 +0100

    BarChart cleanup

--- src/App/View/Util/D3.purs
+++ src/App/View/Util/D3.purs
@@ -4,7 +4,7 @@ module App.View.Util.D3
    , Margin
    , Selection
    , attrs
-   , bandWidth
+   , bandwidth
    , classed
    , colorScale
    , colorScale2
@@ -112,7 +112,7 @@ foreign import colorScale :: forall a. String -> a -> Array a -> String
 foreign import colorScale2 :: forall a. String -> a -> String
 foreign import scaleLinear :: { min :: Number, max :: Number } -> { min :: Number, max :: Number } -> Endo Number
 foreign import scaleBand :: forall r. Int -> (Selectable String -> String) -> Array ({ x :: Selectable String | r }) -> String -> Number
-foreign import bandWidth :: (String -> Number) -> Number
+foreign import bandwidth :: (String -> Number) -> Number
 -- Currently two different protocols for x and y axis; will subsume into something more general
 foreign import xAxis :: forall a r. { x :: a -> Number | r } -> NonEmptyArray a -> Selection -> Effect Selection
 foreign import yAxis :: forall a r. { y :: a -> Number | r } -> Number -> Selection -> Effect Selection
