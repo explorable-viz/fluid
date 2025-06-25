25b905770ea2cefdb670bbc118925c9cb3631e4e
Roly Perera <roly.perera@dynamicaspects.org>
2025-06-25 15:43:56 +0100

    🧩 [consolidate]: Slightly more robust impl of MultiView.

--- src/App/View/MultiView.purs
+++ src/App/View/MultiView.purs
@@ -22,13 +22,20 @@ instance Drawable MultiView Unit Unit where
 createRootElement' :: MultiView -> D3.Selection -> Effect D3.Selection
 createRootElement' (MultiView views) parent = do
    rootElement <- parent # create D3.G []
-   sequence_ $ flip map views \view -> do
+   sequence_ $ views' <#> \(_ × view) ->
       unpack view \v -> createRootElement (const const) v rootElement
    pure rootElement
+   where
+   -- create views in fixed order so we can access positionally in setSelStates and map back to keys
+   views' :: Array (String × View)
+   views' = toUnfoldable views
 
 setSelStates' :: MultiView -> Select -> D3.Selection -> Effect Unit
-setSelStates' (MultiView views) select rootElement = do
+setSelStates' (MultiView views) select rootElement =
    sequence_ $
-      flip mapWithIndex (toUnfoldable views :: Array (String × View)) \i (x × view) -> do
+      flip mapWithIndex views' \i (x × view) -> do
          child <- rootElement # D3.select ("svg" <> D3.nthChild (i + 1)) -- TODO: remove 'svg'
          void $ unpack view \v -> setSelStates v (multiViewEntry x >>> select) child
+   where
+   views' :: Array (String × View)
+   views' = toUnfoldable views
