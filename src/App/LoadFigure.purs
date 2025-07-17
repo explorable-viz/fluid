9fa3c9e6f6beb9cfd7f14736d63e656ab80bfda4
Jessy Sodimu <jessysodimu@gmail.com>
2025-07-17 16:27:52 +0100

    Created dummy version of loadFigure accepting FigSpec and string inputs

--- src/App/LoadFigure.purs
+++ src/App/LoadFigure.purs
@@ -14,6 +14,7 @@ import Data.Maybe (Maybe(..))
 import Data.Tuple (uncurry)
 import Doc (DocOpt(..))
 import Effect (Effect)
+--import Effect.Aff (Aff, launchAff, launchAff_)
 import File (File(..), FileCxt(..), Folder(..))
 import Graph (DVertex'(..))
 import Module.Web (loadFile', runWebT)
@@ -46,10 +47,48 @@ figSpecFromJson spec@{ datasets, file, imports, inputs, query, linking } =
    , linking
    }
 
-loadFigure :: String -> Effect Unit
+loadFigure :: String -> Effect Unit -- TO DELETE
 loadFigure fileName = runAffs_ (uncurry drawFig)
    [ do
         -- TODO: simplify
+        result <- get json fileName
+        case result of
+           Left err -> error ("Json fetching failed with " <> printError err)
+           Right response ->
+              case decodeJson response.body of
+                 Left err -> error ("JSON decoding failed with " <> show err)
+                 Right spec -> do
+                    let spec'@{ fluidSrcPaths } = figSpecFromJson spec
+                    ("fig" × _) <$> runWebT (FileCxt { fluidSrcPaths }) (loadFig spec')
+   ]
+
+{-
+-- TRYING TO MAKE THIS WORK
+loadSpec :: String -> JsonSpec
+loadSpec fileName = launchAff_ do
+      result <- get json fileName
+      case result of
+         Left err -> error ("Json fetching failed with " <> printError err)
+         Right response -> case decodeJson response.body of
+            Left err -> error ("JSON decoding failed with " <> show err)
+            Right decodedSpec  -> pure $ decodedSpec
+
+loadFigureOLD :: String -> Effect Unit --OLD WAY OF DOING IT, SHOULD STILL WORK
+loadFigureOLD filename = do
+   jsonSpec <- loadSpec filename
+   figSpec <- figSpecFromJson filename
+   loadFigureFromFigSpec figSpec
+-}
+
+loadFigureFromFigSpec :: FigSpec -> Effect Unit
+loadFigureFromFigSpec spec@{ fluidSrcPaths } = runAffs_ (uncurry drawFig)
+   [ do
+        ("fig" × _) <$> runWebT (FileCxt { fluidSrcPaths }) (loadFig spec)
+   ]
+
+loadFigureFromFilename :: String -> Effect Unit -- ORIGINAL loadFigure (just renamed) 
+loadFigureFromFilename fileName = runAffs_ (uncurry drawFig)
+   [ do
         result <- get json fileName
         case result of
            Left err -> error ("Json fetching failed with " <> printError err)
