48fd742a6b8fa2e9b2a137440b641b4746e7714f
Jessy Sodimu <jessysodimu@gmail.com>
2025-07-24 14:36:38 +0100

    Remove loadFigureFromJsonSpec

--- src/App/LoadFigure.purs
+++ src/App/LoadFigure.purs
@@ -50,33 +50,27 @@ figSpecFromJson spec@{ datasets, file, imports, inputs, query, linking } =
    , linking
    }
 
-loadFigureFromJsonSpec :: JsonSpec -> Effect Unit
-loadFigureFromJsonSpec spec = runAffs_ (uncurry drawFig)
-   [ let
-        figSpec@{ fluidSrcPaths } = figSpecFromJson spec
-     in
-        ("fig" × _) <$> runWebT (FileCxt { fluidSrcPaths }) (loadFig figSpec)
-   ]
 
-loadSpec :: String -> Aff JsonSpec -- TO DO: Make exportable
+loadSpec :: String -> Aff Json -- TO DO: Make exportable
 loadSpec filename = do
    result <- get json filename
    case result of
       Left err -> throwError (error ("Json fetching failed with " <> printError err))
-      Right response ->
-         case decodeJson response.body of
-            Left err -> throwError (error ("JSON decoding failed with " <> show err))
-            Right spec -> pure $ spec
+      Right response -> pure $ response.body
 
-loadFigureFromSpec :: Json -> Effect Unit
-loadFigureFromSpec encodedJsonSpec = case decodeJson encodedJsonSpec :: Either JsonDecodeError JsonSpec of
-   Left err -> throwError (error ("JSON decoding failed with " <> show err))
-   Right jsonSpec -> liftEffect $ loadFigureFromJsonSpec jsonSpec
+loadFigureFromJson :: Json -> Effect Unit
+loadFigureFromJson json = runAffs_ (uncurry drawFig)
+   [ case decodeJson json :: Either JsonDecodeError JsonSpec of
+        Left err -> throwError (error ("JSON decoding failed with " <> show err))
+        Right spec -> ("fig" × _) <$> runWebT (FileCxt { fluidSrcPaths }) (loadFig figSpec)
+           where
+           figSpec@{ fluidSrcPaths } = figSpecFromJson spec
+   ]
 
 loadFigure :: String -> Effect Unit
 loadFigure filename = launchAff_ do
    jsonSpec <- loadSpec filename
-   liftEffect $ loadFigureFromJsonSpec jsonSpec
+   liftEffect $ loadFigureFromJson jsonSpec
 
 drawCode :: String -> String -> Effect Unit
 drawCode folder file = runAffs_ drawFile
