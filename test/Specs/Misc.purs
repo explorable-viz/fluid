e6e5b420108e9088fad570567170a73fdbc79eb9
Jacob Pake <github@pake.dev>
2025-07-29 08:45:36 +0100

    test cases for basic import from source file

--- test/Specs/Misc.purs
+++ test/Specs/Misc.purs
@@ -44,4 +44,7 @@ misc_cases =
    , { file: "records", imports: [], fwd_expect: "{[\"a\"] : 2, [\"b\"] : 6, [\"c\"] : 7, [\"d\"] : (5 : []), [\"e\"] : 7}" }
    , { file: "record-lookup", imports: [], fwd_expect: "True" }
    , { file: "reverse", imports: [], fwd_expect: "(2 : (1 : []))" }
+
+   , { file: "module/import-simple", imports: [], fwd_expect: "84" }
+   , { file: "module/import-simple-unused", imports: [], fwd_expect: "84" }
    ]
