afef732eef6fc23b1e91f1dd420470bbddbaff90
Ali6461 <ali20042004@hotmail.co.uk>
2025-08-05 14:58:31 +0100

    test

--- script/bundle-website.sh
+++ script/bundle-website.sh
@@ -16,16 +16,12 @@ fi
 
 if [[ "${BASH_SOURCE[0]}" == *"/node_modules/"* ]]; then
    PREFIX="node_modules/@explorable-viz/fluid/"
+else
+   PREFIX=""
 fi
 
-PREFIX_=${PREFIX:+$PREFIX/}
 echo "Cleaning dist/$WEBSITE"
-. "${PREFIX_}script/util/clean.sh" $WEBSITE
-
-for DIR in css font shared; do
-  echo "Processing $DIR files:"
-  cp -rL "website/$WEBSITE/$DIR" "dist/$WEBSITE/$DIR"
-done
+. "${PREFIX}script/util/clean.sh" $WEBSITE
 
 shopt -s nullglob
 
@@ -33,7 +29,7 @@ echo "Processing other static files:"
 set +xu  # try to remove +u
 TO_COPY=()
 shopt -s dotglob extglob
-for CHILD in ${PREFIX_}website/$WEBSITE/!(.|..); do
+for CHILD in ${PREFIX}website/$WEBSITE/!(.|..); do
    BASENAME="$(basename "$CHILD")"
    if [[ "$BASENAME" =~ ^[a-z.] ]]; then
       TO_COPY+=("$CHILD")
@@ -47,10 +43,10 @@ for CHILD in "${TO_COPY[@]}"; do
 done
 
 echo "Processing Fluid source files:"
-cp -r "${PREFIX_}dist/fluid/fluid" dist/$WEBSITE
+cp -r "${PREFIX}dist/fluid/fluid" dist/$WEBSITE
 [ -d "website/$WEBSITE/fluid" ] && cp -r "website/$WEBSITE/fluid" dist/$WEBSITE
 
 echo "Processing shared JavaScript files:"
-cp -r "${PREFIX_}dist/fluid/shared" dist/$WEBSITE
+cp -r "${PREFIX}dist/fluid/shared" dist/$WEBSITE
 
 echo "Bundled website $WEBSITE"
