8733ae037786a5b7c1dfc10dbedfcdc581d7afb0
Jacob Pake <github@pake.dev>
2025-08-05 10:17:26 +0100

    fix typo

--- src/Module.purs
+++ src/Module.purs
@@ -124,7 +124,7 @@ loadModuleGraph roots = do
                (Set.insert mod visited)
                (Map.insert mod imports' graph)
                (Map.insert mod mod' modules)
-               (imports <> rest)
+               (imports' <> rest)
 
    loadModule :: ModuleName -> m (Raw Module × List ModuleName)
    loadModule name = do
