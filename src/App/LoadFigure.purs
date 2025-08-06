770c1e8359e31db999719c3768dc839c92270a2c
Jessy Sodimu <jessysodimu@gmail.com>
2025-08-06 17:20:16 +0100

    Made fluidExtension implicit in loadFigure versions

--- src/App/LoadFigure.purs
+++ src/App/LoadFigure.purs
@@ -18,10 +18,10 @@ import Doc (DocOpt(..))
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
 import Effect.Class (liftEffect)
-import File (File(..), FileCxt(..), Folder(..), loadFileFromPath)
+import File (File(..), FileCxt(..), Folder(..), fluidExtension, loadFileFromPath)
 import Graph (DVertex'(..))
 import Module.Web (loadFile_, runWebT)
-import Util (error, (×))
+import Util (definitely', error, (×))
 import Val (Val(..), asVal)
 
 type JsonSpec =
@@ -55,31 +55,25 @@ loadSpec filename = do
       Left err -> error ("Json fetching failed with " <> printError err)
       Right response -> pure $ response.body
 
---loadFigure -- specFile (String) & srcFile (String)
+--loadFigure -- specFilename (String) & srcFilename (String)
 loadFigure :: String -> String -> Effect Unit
 loadFigure specFile srcFile = launchAff_ do
    jsonSpec <- loadSpec specFile
-   fluidSrc_ <- loadFileFromPath (File srcFile)
-   case fluidSrc_ of
-      Nothing -> error ("File not found: " <> show srcFile)
-      Just fluidSrc -> liftEffect $ loadFigureSpecSrc jsonSpec fluidSrc
+   liftEffect $ loadFigureSpec jsonSpec srcFile
 
---loadFigureSrc -- specFile (String) & fluidSrc (String)
+--loadFigureSrc -- specFilename (String) & fluidSrc (String)
 loadFigureSrc :: String -> String -> Effect Unit
 loadFigureSrc specFile fluidSrc = launchAff_ do
    jsonSpec <- loadSpec specFile
    liftEffect $ loadFigureSpecSrc jsonSpec fluidSrc
 
---loadFigureSpec -- jsonSpec (Json) & srcFile (String)
+--loadFigureSpec -- jsonSpec (Json) & srcFilename (String)
 loadFigureSpec :: Json -> String -> Effect Unit
 loadFigureSpec jsonSpec srcFile = launchAff_ do
-   fluidSrc_ <- loadFileFromPath (File srcFile)
-   case fluidSrc_ of
-      Nothing -> error ("File not found: " <> show srcFile)
-      Just fluidSrc -> liftEffect $ loadFigureSpecSrc jsonSpec fluidSrc
+   fluidSrc <- loadFileFromPath (File (srcFile <> fluidExtension))
+   liftEffect $ loadFigureSpecSrc jsonSpec (definitely' fluidSrc)
 
 --loadFigureSpecSrc -- jsonSpec (Json) & fluidSrc (String)
---PS: all 4 versions pass the parameters to loadFigureSpecSrc
 loadFigureSpecSrc :: Json -> String -> Effect Unit
 loadFigureSpecSrc jsonSpec fluidSrc = runAffs_ (uncurry drawFig)
    [ case decodeJson jsonSpec :: Either JsonDecodeError JsonSpec of
