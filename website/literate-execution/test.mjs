e150af5fd65fe7af1c78219179582e2b7eca0a6a
Roly Perera <roly.perera@dynamicaspects.org>
2025-08-24 10:05:38 +0100

    🧩 [new-test]: ar6-wg1-spm test for literate-execution.

---
+++ website/literate-execution/test.mjs
@@ -0,0 +1,13 @@
+import { runTests, testURL, waitForFigure, waitFor } from "./shared/webtest-lib.js"
+
+export const main = async () => {
+    await runTests(testURL("figure-spm-4b")([
+        page => waitFor("#fig-output > svg:nth-of-type(1)")(page),
+        page => waitFor("#fig-output > svg:nth-of-type(2)")(page),
+    ]))()
+    await runTests(testURL("table-spm-1")([
+        page => waitFor("#fig-output > div.para-text:nth-of-type(1)")(page),
+        page => waitFor("#fig-output > div.para-text:nth-of-type(2)")(page)
+    ]))()
+    console.log("Success!")
+}
