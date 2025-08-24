f904feeb0bc7680d34b4d75fb16dbf5bc1e94bd4
Roly Perera <roly.perera@dynamicaspects.org>
2025-08-24 13:13:24 +0100

    🧩 [doc] : Link to #1385.

--- script/test-website.sh
+++ script/test-website.sh
@@ -20,10 +20,7 @@ else
    echo "No test.mjs found for $WEBSITE in dist/$WEBSITE"
 fi
 
-# $WEBSITE is now in Lisp-case, so this is only happens to do anything when $WEBSITE doesn't contain hyphens
-# and this script is running on a case-insensitive file system like MacOS. Otherwise PAGES will be empty.
-# Best option would probably be to migrate the .purs tests to .js but that isn't trivial either because the
-# current Puppeteer helpers return Aff, which doesn't play well with JS -- may need to migrate these to Promise.
+# Borked -- see #1385
 if [[ -e "website/Test/$WEBSITE" ]]; then
    PAGES=($(for FILE in website/Test/$WEBSITE/*.purs; do
       basename "$FILE" | sed 's/\.[^.]*$//'
