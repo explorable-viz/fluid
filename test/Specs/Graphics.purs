61fa47376004d53a37b129b085a286bdb189326f
Ali6461 <ali20042004@hotmail.co.uk>
2025-08-28 12:25:32 +0100

    added new files

--- test/Specs/Graphics.purs
+++ test/Specs/Graphics.purs
@@ -4,12 +4,16 @@ import Test.Util.Suite (TestWithDatasetSpec)
 
 graphics_cases :: Array TestWithDatasetSpec
 graphics_cases =
-   [ { file: "graphics/background.fld" }
-
-   , { file: "graphics/grouped-bar-chart.fld"
-     }
-   , { file: "graphics/line-chart.fld"
-     }
-   , { file: "graphics/stacked-bar-chart.fld"
-     }
+   [ {
+        file: "graphics/background.fld"
+    },
+    {
+        file: "graphics/grouped-bar-chart.fld"
+    },
+    {
+        file: "graphics/line-chart.fld"
+    },
+    {
+        file: "graphics/stacked-bar-chart.fld"
+    }
    ]
