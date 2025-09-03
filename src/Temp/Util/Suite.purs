6f22382e20f594e0f63876d40246bdeb22c4c26a
Jacob Pake <github@pake.dev>
2025-09-03 13:18:48 +0100

    test for parse and pretty separately

--- src/Temp/Util/Suite.purs
+++ src/Temp/Util/Suite.purs
@@ -2,8 +2,9 @@ module Temp.Util.Suite where
 
 import Prelude
 
-import Data.Array (drop, elem, filter, null)
+import Data.Array (drop, elem, filter, index, null)
 import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
 import Data.String (Pattern(..))
 import Data.String.CodeUnits (takeRight)
 import Data.String.Common (replace)
@@ -12,30 +13,34 @@ import Data.Traversable (foldl, traverse)
 import Data.Tuple (snd)
 import Effect (Effect)
 import Effect.Console (log)
-import Effect.Exception (try)
+import Effect.Exception (throw, try)
 import Module (parse)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (exists, readTextFile, readdir, writeTextFile)
 import Node.Process (argv)
 import Parse as P
+import Pretty (prettyP)
+import Temp.Parse (parsePy)
 import Temp.Pretty (prettyPy)
 
-dir :: String
-dir = "test/golden/pretty"
+type TestFn = String -> Effect (Either String String)
+
+parseArgs :: Effect {fn :: TestFn, dir :: String, files :: Array String}
+parseArgs = do
+   args <- argv
+   case (index args 2) of
+      Nothing -> throw "missing"
+      Just "pretty" -> pure $ {fn: testPretty, dir: "test/golden/pretty", files: drop 3 args}
+      Just "parse" -> pure $ {fn: testParse, dir: "test/golden/parse", files: drop 3 args}
+      Just _ -> throw "unsupported"
 
 main :: Effect Unit
 main = do
-
-   args <- argv
-   let specified = drop 2 args
-
-   files <- filter (\s -> takeRight 4 s == ".fld") <$> readdir dir
-   let files' = if null specified then files else filter (\s -> elem s specified) files
-
-   results <- traverse (process true dir) files'
-
+   {fn, dir, files} <- parseArgs
+   files' <- filter (\s -> takeRight 4 s == ".fld") <$> readdir dir
+   let files'' = if null files then files' else filter (\s -> elem s files) files'
+   results <- traverse (test fn dir) files''
    let { passes, fails, missing } = tally results
-
    let
       colour =
          if fails == 0 && missing == 0 then green
@@ -58,39 +63,46 @@ tally = foldl tally' { passes: 0, fails: 0, missing: 0 }
    tally' acc Missing = acc { missing = acc.missing + 1 }
    tally' acc _ = acc -- ignore Error cases
 
-process :: Boolean -> String -> String -> Effect Result
-process create srcDir srcFile = do
+
+
+
+testPretty :: TestFn
+testPretty src = do
+   parsed <- try $ snd <$> parse src P.program
+   case parsed of
+      Left error -> pure $ Left (show error)
+      Right expr -> pure $ Right (prettyPy expr <> "\n")
+
+testParse :: TestFn
+testParse src = do
+   parsed <- try $ parsePy src
+   case parsed of
+      Left error -> pure $ Left (show error)
+      Right expr -> pure $ Right (prettyP expr)
+
+test :: TestFn -> String -> String -> Effect Result
+test f srcDir srcFile = do
    let srcPath = srcDir <> "/" <> srcFile
    let expectPath = replace (Pattern ".fld") (Replacement ".expect") srcPath
-
    src <- readTextFile UTF8 srcPath
-   parsed <- try $ snd <$> parse src P.program
-
-   case parsed of
+   result <- f src
+   case result of
       Left error -> do
          log $ red ("✘ " <> srcFile)
          log $ show error
-
          pure Fail
-      Right parsed' -> do
-
-         let pretty = prettyPy parsed' <> "\n"
-
+      Right out -> do
          hasExpect <- exists expectPath
-
          if hasExpect then do
             expect <- readTextFile UTF8 expectPath
-            if pretty == expect then do
-               -- log $ (green "✔ ") <> srcFile
+            if out == expect then
                pure Pass
             else do
                log $ red ("✘ " <> srcFile)
-               log $ pretty
+               log $ out
                pure Fail
          else do
-            when create do
-               writeTextFile UTF8 expectPath pretty
-
+            writeTextFile UTF8 expectPath out
             log $ yellow ("~ " <> srcFile)
             pure Missing
 
