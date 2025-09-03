bc84f40e881772507644728152c956204d29c4c5
Jacob Pake <github@pake.dev>
2025-09-03 17:01:02 +0100

    rethink grammar structure

--- src/Temp/Parse.purs
+++ src/Temp/Parse.purs
@@ -3,27 +3,24 @@ module Temp.Parse (parsePy) where
 import Prelude
 
 import Control.Alt ((<|>))
-import Control.Lazy (fix)
 import Control.Monad.Error.Class (liftEither)
 import Control.Monad.Except (class MonadError)
 import Control.Monad.State (StateT)
 import Data.Bifunctor (lmap)
 import Data.Identity (Identity)
-import Data.Int (toNumber)
 import Data.List (List(..), (:))
 import Data.List.NonEmpty (NonEmptyList)
-import Data.Maybe (Maybe(..))
 import Data.Traversable (foldl)
 import Doc (DocOpt(..))
 import Effect.Exception (Error, error)
 import Lattice (Raw)
 import Parsing (ParseError(..), Position(..), runParserT)
-import Parsing.Combinators (optionMaybe, sepBy, sepBy1, try)
+import Parsing.Combinators (sepBy, sepBy1, try)
 import Parsing.Expr (Assoc(..), Operator(..), buildExprParser)
 import Parsing.Indent (runIndent, withPos)
 import Parsing.String (char, eof, string)
 import SExpr (Clause(..), Expr(..), Pattern(..), VarDef(..))
-import Temp.Parse.Parser (Parser, align, block, delim, floating, integer, lexeme, lines, number, parens, reserved, sign, unreserved, whitespace)
+import Temp.Parse.Parser (Parser, align, block, delim, floating, integer, lexeme, lines, parens, reserved, unreserved, whitespace)
 import Temp.Util.UnsafeDebug (exitUnsafe, logErrorUnsafe)
 import Util (nonEmpty, (×))
 
@@ -72,67 +69,76 @@ opdefs =
      ]
    ]
 
-expression :: Parser (Raw Expr)
-expression =
-   fix (opTreeLeaf >>> buildExprParser opdefs)
+-- expr :=
+--    | opTree
+--    | Let expr expr
+--    | LetRec expr expr
+--    | MatchAs opTree [expr]
+--    | IfElse opTree expr expr
+--
+-- opTree :=
+--    | opTreeLeaf
+--    | BinaryApp opTreeLeaf opTreeLeaf
+--
+-- opTreeLeaf :=
+--    | (opTree)
+--    | ... everything else, all child expressions are opTrees ...
 
+
+blocky :: Parser (Raw Expr)
+blocky = ifElse <|> try funDef <|> valDef <|>  opTree
    where
+      funDef :: Parser (Raw Expr)
+      funDef = do
+         reserved "def"
+         name <- unreserved
+         ps <- params
+         e <- block blocky
+         e' <- align blocky
+         pure $ LetRec (nonEmpty ((name × Clause (ps × e)) : Nil)) e'
+         where
+         params :: Parser (NonEmptyList Pattern)
+         params = parens $ sepBy1 pvar (lexeme $ char ',')
 
-   opTreeLeaf :: Parser (Raw Expr) -> Parser (Raw Expr)
-   opTreeLeaf expr' = conditional <|> def <|> appChain
-      where
+      valDef :: Parser (Raw Expr)
+      valDef = do
+         reserved "def"
+         name <- pvar
+         e <- block blocky
+         e' <- align blocky
+         pure $ Let (nonEmpty ((VarDef name e) : Nil)) e'
 
-      conditional :: Parser (Raw Expr)
-      conditional = do
+      ifElse :: Parser (Raw Expr)
+      ifElse = do
          reserved "if"
-         c <- expr'
-         t <- block expr'
+         c <- opTree
+         t <- block blocky
          align (reserved "else")
-         e <- block expr'
+         e <- block blocky
          pure $ IfElse c t e
 
-      def :: Parser (Raw Expr)
-      def = reserved "def" *> (try fun <|> val)
+      opTree :: Parser (Raw Expr)
+      opTree =  (buildExprParser opdefs simple)
 
+      simple :: Parser (Raw Expr)
+      simple = appChain
          where
-
-         fun :: Parser (Raw Expr)
-         fun = do
-            name <- unreserved
-            ps <- params
-            e <- block expr'
-            e' <- align expr'
-            pure $ LetRec (nonEmpty ((name × Clause (ps × e)) : Nil)) e'
+         appChain :: Parser (Raw Expr)
+         appChain = simple' >>= \e -> app e
             where
-            params :: Parser (NonEmptyList Pattern)
-            params = parens $ sepBy1 pvar (lexeme $ char ',')
+            app :: Raw Expr -> Parser (Raw Expr)
+            app e = args e <|> pure e
 
-         val :: Parser (Raw Expr)
-         val = do
-            name <- pvar
-            e <- block expr'
-            e' <- align expr'
-            pure $ Let (nonEmpty ((VarDef name e) : Nil)) e'
+            args :: Raw Expr -> Parser (Raw Expr)
+            args e = do
+               ps <- parens $ sepBy simple (lexeme $ char ',')
+               app (foldl (App None) e ps)
 
-      appChain :: Parser (Raw Expr)
-      appChain = simple >>= \e -> app e
-
-         where
-
-         app :: Raw Expr -> Parser (Raw Expr)
-         app e = args e <|> pure e
-
-         args :: Raw Expr -> Parser (Raw Expr)
-         args e = do
-            ps <- parens $ sepBy expr' (lexeme $ char ',')
-            app (foldl (App None) e ps)
-
-         simple :: Parser (Raw Expr)
-         simple = try float <|> int <|> variable
-
+            simple' :: Parser (Raw Expr)
+            simple' = try float <|> int <|> variable
 
 program :: Parser (Raw Expr)
-program = lines *> withPos expression <* whitespace <* eof
+program = lines *> withPos blocky <* whitespace <* eof
 
 parsePy :: forall m. MonadError Error m => String -> m (Raw Expr)
 parsePy input = liftEither $ lmap (evil input) $ runIndent $ runParserT input program
