5a43fc0ef3070f76ead32a21f04f2271c3ca0b98
Roly Perera <roly.perera@dynamicaspects.org>
2025-09-08 18:04:00 +0100

    🧩 [modify-unused]: Check NPM component has source website installed.

--- script/install-website.sh
+++ script/install-website.sh
@@ -4,14 +4,21 @@ set -xeu
 # Run from root of repo
 
 WEBSITE=$1
+NPM_ROOT=node_modules/@explorable-viz/fluid
 
 if [ -e "website/$WEBSITE" ]; then
   echo "Error: website/$WEBSITE already exists" >&2
   exit 1
 fi
 
+if [ -e "website/$WEBSITE" ]; then
+  echo "Error: $NPM_ROOT/website/$WEBSITE does not exist" >&2
+  exit 1
+fi
+
 mkdir -p website/$WEBSITE
-cp -r node_modules/@explorable-viz/fluid/website/{css,font,shared} website
+cp -r $NPM_ROOT/website/{css,font,shared} website
+cp -r $NPM_ROOT/website/$1 website
 
 ln -s website/css website/$WEBSITE/css
 ln -s website/font website/$WEBSITE/font
