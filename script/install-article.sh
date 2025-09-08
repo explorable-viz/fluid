49cf23ad15e799dc7cd12b010c106c84188e43f4
Roly Perera <roly.perera@dynamicaspects.org>
2025-09-08 17:34:28 +0100

    🧩 [unconsolidate]: Generalise install-article. Move fluid-org specific stuff out of 'shared'.

--- script/install-article.sh
+++ script/install-article.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 set -xeu
 
-rm -rf website
-mkdir -p website
-cp -r node_modules/@explorable-viz/fluid/website/{article,css,font,shared} website
+# Run from root of repo
 
-ln -s ../css website/article/css
-ln -s ../font website/article/font
-ln -s ../shared website/article/shared
+WEBSITE=$1
+
+if [ -e "website/$WEBSITE" ]; then
+  echo "Error: website/$WEBSITE already exists" >&2
+  exit 1
+fi
+
+mkdir -p website/$WEBSITE
+cp -r node_modules/@explorable-viz/fluid/website/{css,font,shared} website
+
+ln -s website/css website/$WEBSITE/css
+ln -s website/font website/$WEBSITE/font
+ln -s website/shared website/$WEBSITE/shared
