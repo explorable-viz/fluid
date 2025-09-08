1868f197c9fbffd567b7fdf10c4b3143d35ea488
Roly Perera <roly.perera@dynamicaspects.org>
2025-09-08 18:07:38 +0100

    🧩 [consolidate]: Simplify install-website.

--- script/install-website.sh
+++ script/install-website.sh
@@ -11,15 +11,5 @@ if [ -e "website/$WEBSITE" ]; then
   exit 1
 fi
 
-if [ -e "website/$WEBSITE" ]; then
-  echo "Error: $NPM_ROOT/website/$WEBSITE does not exist" >&2
-  exit 1
-fi
-
 mkdir -p website/$WEBSITE
-cp -r $NPM_ROOT/website/{css,font,shared} website
 cp -r $NPM_ROOT/website/$1 website
-
-ln -s website/css website/$WEBSITE/css
-ln -s website/font website/$WEBSITE/font
-ln -s website/shared website/$WEBSITE/shared
