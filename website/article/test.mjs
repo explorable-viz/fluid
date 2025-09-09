4ee201d93cb6d09b04cc7fa36cb8cc2a8f89648a
Roly Perera <roly.perera@dynamicaspects.org>
2025-09-09 20:12:39 +0100

    🧩 [new-test]: scigen test.

--- website/article/test.mjs
+++ website/article/test.mjs
@@ -11,4 +11,5 @@ export const main = async () => {
       page => waitFor("#fig-output > svg:nth-child(1)")(page),
       page => waitFor("#fig-output > svg:nth-child(2)")(page),
    ]))()
+   await runTests(testURL("scigen-1805.02474v1-10")([ page => waitFor("div#fig-output")(page) ]))()
 }
