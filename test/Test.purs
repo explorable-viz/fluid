c4f62084bdcd8d16e314eaf9bf979302033e51bd
Roly Perera <roly.perera@dynamicaspects.org>
2025-09-14 13:24:28 +0100

    ❗ [fix]: Ditto paragraph/basic test. Also triple quotes obviate need for escaping string quotes.

--- test/Test.purs
+++ test/Test.purs
@@ -25,7 +25,7 @@ import Test.Util.Suite (BenchSuite, bwdSuite, linkedInputsSuite, linkedOutputsSu
 import Util ((×))
 
 main :: Effect Unit
-main = run (second (runWebT (FileCxt { fluidSrcPaths })) <$> selectedCommentsTests)
+main = run (second (runWebT (FileCxt { fluidSrcPaths })) <$> allTests)
 
 scratchpad :: forall m. MonadAff m => MonadError Error m => MonadReader FileCxt m => LoadFile m => TestSuite m
 scratchpad = second void <$> suite paragraph_cases (1 × false)
