fa64d80c962fdbad5945896298bc9ad5751cceb9
Roly Perera <roly.perera@dynamicaspects.org>
2025-10-03 13:09:00 +0900

    ❗ [incomplete]: loadJson should use loadFileFromPath not (loadFile + fluidSrcPaths).

--- website/fluid-org/test.mjs
+++ website/fluid-org/test.mjs
@@ -1,5 +1,10 @@
 import { runTests, testURL, waitFor } from "./shared/webtest-lib.js"
 
 export const main = async () => {
+   await runTests(testURL("")([
+      page => waitFor("#fig-output > svg:nth-child(1)")(page),
+      page => waitFor("#fig-output > svg:nth-child(2)")(page),
+   ]))()
    await runTests(testURL("convolution")([ page => waitFor("svg#fig-output")(page) ]))()
+   await runTests(testURL("moving-average")([ page => waitFor("svg")(page) ]))()
 }
