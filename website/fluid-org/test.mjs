74686bbe213848ffc132b4fb6ff2e438cc7f860e
Roly Perera <roly.perera@dynamicaspects.org>
2025-10-03 13:52:30 +0900

    🧩 [new-test]: Reinstate but as orthogonal test, which does work (and in this case is semantically equivalent).

--- website/fluid-org/test.mjs
+++ website/fluid-org/test.mjs
@@ -1,11 +1,8 @@
 import { runTests, testURL, waitFor } from "./shared/webtest-lib.js"
 
 export const main = async () => {
-   await runTests(testURL("")([
-      page => waitFor("#fig-output > svg:nth-child(1)")(page),
-      // navigating to localhost second time times out on Firefox, manually looks ok though
-//      page => waitFor("#fig-output > svg:nth-child(2)")(page),
-   ]))()
+   await runTests(testURL("")([ page => waitFor("#fig-output > svg:nth-child(1)")(page) ]))()
+   await runTests(testURL("")([ page => waitFor("#fig-output > svg:nth-child(2)")(page) ]))()
    await runTests(testURL("convolution")([ page => waitFor("svg#fig-output")(page) ]))()
    await runTests(testURL("moving-average")([ page => waitFor("svg")(page) ]))()
 }
