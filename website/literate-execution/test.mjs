7b21635e89b576a9accf8926feb7e15e372d3f36
Roly Perera <roly.perera@dynamicaspects.org>
2025-10-03 16:05:15 +0900

    🧩 [move]: More structural simplification.

--- website/literate-execution/test.mjs
+++ website/literate-execution/test.mjs
@@ -1,11 +1,11 @@
 import { runTests, testURL, waitFor } from "./shared/webtest-lib.js"
 
 export const main = async () => {
-    await runTests(testURL("ar6-wg1-spm/figure-spm-4b")([
+    await runTests(testURL("ar6-wg1-spm/figure4b")([
         page => waitFor("#fig-output > svg:nth-of-type(1)")(page),
         page => waitFor("#fig-output > svg:nth-of-type(2)")(page),
     ]))()
-    await runTests(testURL("ar6-wg1-spm/table-spm-1")([
+    await runTests(testURL("ar6-wg1-spm/table1")([
         page => waitFor("#fig-output > div.para-text:nth-of-type(1)")(page),
         page => waitFor("#fig-output > div.para-text:nth-of-type(2)")(page)
     ]))()
