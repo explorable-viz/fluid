ce2b58d6d26bf29562f9e0e572c64918ca60e46e
Roly Perera <roly.perera@dynamicaspects.org>
2025-10-03 13:21:27 +0900

    ❗ [modify]: loadJson should use loadFileFromPath not (loadFile + fluidSrcPaths).

--- karma.conf.test.js
+++ karma.conf.test.js
@@ -44,7 +44,7 @@ module.exports = function (config) {
       logLevel: config.LOG_ERROR,
       proxies: {
          "/fluid/": "/base/fluid/",
-         "/test/dataset/": "/base/test/dataset/",
+         "/dataset/": "/base/test/dataset/",
          "/test/fluid/": "/base/test/fluid/"
       },
       reporters: ["mocha"],
