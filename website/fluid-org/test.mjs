d4f92177b8f6497ca5139be5126d176b8be26741
Roly Perera <roly.perera@dynamicaspects.org>
2025-10-04 14:18:05 +0900

    ❗ [fix]: Patch up some CSS selectors.

--- website/fluid-org/test.mjs
+++ website/fluid-org/test.mjs
@@ -1,8 +1,10 @@
 import { runTests, testURL, waitFor } from "./shared/webtest-lib.js"
 
 export const main = async () => {
-   await runTests(testURL("")([ page => waitFor("#fig-output > svg:nth-child(1)")(page) ]))()
-   await runTests(testURL("")([ page => waitFor("#fig-output > svg:nth-child(2)")(page) ]))()
+   await runTests(testURL("")([
+      page => waitFor("#fig-output > div:nth-of-type(1) > svg")(page),
+      page => waitFor("#fig-output > div:nth-of-type(2) > svg")(page)
+   ]))()
    await runTests(testURL("convolution")([ page => waitFor("svg#fig-output")(page) ]))()
    await runTests(testURL("moving-average")([ page => waitFor("svg")(page) ]))()
 }
