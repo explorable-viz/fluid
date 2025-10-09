2c071f57eaa5ac3ce4491e8ff2ba127fbbedef15
Jacob Pake <github@pake.dev>
2025-10-09 09:33:51 +0100

    I think this parens is correct

--- test/Specs/Comments.purs
+++ test/Specs/Comments.purs
@@ -5,7 +5,7 @@ import Test.Util.Suite (TestSpec)
 comments_cases :: Array TestSpec
 comments_cases =
    [ { file: "comments/nested-constr.fld"
-     , fwd_expect: """@doc(Paragraph("This" :| "is" :| "a" :| @doc(Paragraph("This" :| "is" :| "a" :| "nested" :| "docComment!" :| [])) "some string" :| "docComment!" :| [])) False"""
+     , fwd_expect: """@doc(Paragraph("This" :| "is" :| "a" :| (@doc(Paragraph("This" :| "is" :| "a" :| "nested" :| "docComment!" :| [])) "some string") :| "docComment!" :| [])) False"""
      }
    , { file: "comments/dicts.fld"
      , fwd_expect:
