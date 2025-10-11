e622a7be030479460ac3b11caea45114a61a0426
Roly Perera <roly.perera@dynamicaspects.org>
2025-10-11 18:36:47 +0900

    Basic calculation working, but not interesting. Need something that does some arithmetic or something.

--- src/App/View.purs
+++ src/App/View.purs
@@ -40,6 +40,7 @@ view' options title v@(Val _ v_opt _) =
       Paragraph (view options "" <$> from u)
 
 -- Convert annotated value to appropriate view, discarding top-level annotations for now.
+-- TODO: given the typeError clause, Partial no longer needed
 view :: Partial => Options -> String -> Val (SelStates 𝕊) -> View
 view options title v@(Val α _ u') = case u' of
    Int n -> pack (Text (show n × α))
