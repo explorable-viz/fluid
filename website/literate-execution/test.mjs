ec3a0428e5d0abb61e9703d4e97bc769bee29f2a
Roly Perera <roly.perera@dynamicaspects.org>
2025-11-11 14:07:46 +0000

    ❗ [fix]: Correction to regression test.

--- website/literate-execution/test.mjs
+++ website/literate-execution/test.mjs
@@ -1,14 +1,17 @@
 import { runTests, testURL, waitFor } from "./shared/webtest-lib.js"
 
 export const main = async () => {
-    await runTests(testURL("ar6-wg1/spm/figure4b")([
-        page => waitFor("#fig-output > svg:nth-of-type(1)")(page),
-        page => waitFor("#fig-output > svg:nth-of-type(2)")(page),
-    ]))()
-    await runTests(testURL("ar6-wg1/spm/table1")([
-        page => waitFor("#fig-output > div:nth-of-type(1).para-text")(page),
-        page => waitFor("#fig-output > div:nth-of-type(2).para-text")(page)
-    ]))()
-    await runTests(testURL("matrix-multiply")([ page => waitFor("svg#fig-output")(page) ]))()
-    console.log("Success!")
+  await runTests(testURL("ar6-wg1/spm/figure4b")([
+    page => waitFor("#fig-output > svg:nth-of-type(1)")(page),
+    page => waitFor("#fig-output > svg:nth-of-type(2)")(page),
+  ]))()
+  await runTests(testURL("ar6-wg1/spm/table1")([
+    page => waitFor("#fig-output > div:nth-of-type(1).para-text")(page),
+    page => waitFor("#fig-output > div:nth-of-type(2).para-text")(page)
+  ]))()
+  await runTests(testURL("matrix-multiply")([
+    page => waitFor("#fig-output > svg:nth-of-type(1)")(page),
+    page => waitFor("#fig-output > svg:nth-of-type(2)")(page),
+  ]))()
+  console.log("Success!")
 }
