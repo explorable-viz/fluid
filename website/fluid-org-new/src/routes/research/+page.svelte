51d4b9b3bf66426d69dc4195aafbb3088524b207
Roly Perera <roly.perera@dynamicaspects.org>
2026-02-23 14:04:52 +0000

    Migrate 'research' to Grid.

--- website/fluid-org-new/src/routes/research/+page.svelte
+++ website/fluid-org-new/src/routes/research/+page.svelte
@@ -13,11 +13,12 @@
 		<a href="http://youtube.com/watch?v=FCK5w4EZ1xQ">ICCS Summer School 2025</a><br>
 		<a href="https://www.youtube.com/watch?v=F3JaftEnFfM">ICCS Summer School 2024</a><br>
 		<a href="https://www.youtube.com/watch?v=2-S6yVyzypU">Programming for the Planet 2024</a><br>
-		<a href="https://www.youtube.com/watch?v=M_ePrtD9axk">POPL 2022 presentation</a>
+		<a href="https://www.youtube.com/watch?v=M_ePrtD9axk">POPL 2022</a>
     </p>
 
 		<h3 class="title">Related Papers</h3>
 		<p>
+    <a href="https://arxiv.org/abs/2511.09203">Galois Slicing as Automatic Differentiation</a> (preprint)<br>
     <a href="https://arxiv.org/abs/2601.06027">AI-Assisted Authoring for Transparent, Data-Driven Documents</a> (preprint)<br>
     <a href="https://dl.acm.org/doi/10.1145/3498668">Cognacy Queries over Dependence Graphs for Transparent Visualisations</a> (ESOP 2025)<br>
 		<a href="https://dl.acm.org/doi/10.1145/3498668">Linked Visualisations via Galois Dependencies</a> (POPL 2022)
