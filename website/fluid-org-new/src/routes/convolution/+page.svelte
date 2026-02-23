29e0bddb77898576b2bca83c586ff292fb0dc3f6
Roly Perera <roly.perera@dynamicaspects.org>
2026-02-23 13:03:50 +0000

    Yah just keep purging.

--- website/fluid-org-new/src/routes/convolution/+page.svelte
+++ website/fluid-org-new/src/routes/convolution/+page.svelte
@@ -41,20 +41,6 @@
 			<span class="highlight">literate execution</span> and way of authoring explorable explanations with
 			less effort.
 		</p>
-
-		<h4>An infrastructure for explorable explanations</h4>
-
-		<p>
-			As a simple illustration, consider the following Fluid implementation of convolution. The
-			program takes an input matrix and transforms it using a small matrix called a filter (or
-			kernel), as might be used in image processing to apply an effect like blurring or embossing.
-			Toggle the <span class="highlight">data pane</span> on the left to reveal the
-			<code>inputImage</code>
-			and
-			<code>filter</code>; then mouse over the <code>output</code> to see how the inputs are being used.
-			This implementation can automatically reveal how different cells in the output demand different
-			cells in the input array and filter.
-		</p>
 	</div>
 
 	<DataPane {showDataPane} toggle={() => (showDataPane = !showDataPane)}>
@@ -68,49 +54,13 @@
 			<Figure {spec} fld={convolution} />
 
 			<div>
-				<p class="when-data-pane-visible" style={showDataPane ? '' : 'display: none'}>
-					Notice how only certain parts of the input are relevant to a given output cell. Can some
-					of the irrelevant inputs be attributed to zeros in the filter? Which ones? Also notice how
-					the demand varies as we approach the edge of the output: because this implementation
-					treats the input as though it were padded with zeros at the boundary, parts of the filter
-					are irrelevant to output cells near the edge. Interactions like these would be more useful
-					if we could show the actual computation involved for a given element, rather than just the
-					IO relationships, but even this simple extensional view already reveals interesting things
-					about the implementation, without the need for a bespoke visualisation.
-				</p>
-
-				<p class="when-data-pane-visible" style={showDataPane ? '' : 'display: none'}>
-					What happens when you mouse over a corner of the output?
-				</p>
 			</div>
 		</div>
 	</div>
 	<div></div>
 	<div></div>
 	<div class="flex-left-align">
-		<h4>Relations of cognacy</h4>
-
-		<p>
-			The idea of <span class="highlight">related inputs</span> introduced earlier can also be
-			informative. Try interacting with the <code>inputImage</code> instead. The highlighted
-			<code>output</code>
-			now shows the elements that consume the data point under your mouse; the highlighted
-			<code>inputImage</code>
-			region includes all the <span class="highlight">cognates</span> of that data point: all the inputs
-			that have one of those outputs as an ancestor in the dependence graph. The highlighted region is
-			a kind of “light cone”, picking out a causally closed region of the dependence graph.
-		</p>
-
-		<p>
-			The key take away here is that the author can simply express convolution as a pure functional
-			algorithm; the Fluid runtime and visualisation front-end takes care of providing the
-			interactions. The library function <code>convolve</code> below implements the convolution
-			algorithm, and helper functions
-			<code>zero</code>, <code>wrap</code> and <code>extend</code> implement specific policies (“methods”)
-			for dealing with the boundary.
-		</p>
 		<CodeMirror name="matrix" src={matrix} />
-
 		<p class="right-justified"><a href="../convolution-wrapped/">continued..</a></p>
 	</div>
 </Grid>
