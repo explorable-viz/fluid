61edea6ac9dae3d5610a50d3214ff62384d24126
Roly Perera <roly.perera@dynamicaspects.org>
2026-02-24 12:39:22 +0000

    Getting Started placeholder.

--- website/fluid-org-new/src/routes/+page.svelte
+++ website/fluid-org-new/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
    import { Grid } from '@explorable-viz/fluid';
+    import Page from './faq/+page.svelte';
 </script>
 
 <Grid>
@@ -7,22 +8,19 @@
 	<div></div>
 	<div class="flex-left-align">
 		<h3 class="title">Transparent research outputs</h3>
-		<p>
-			With traditional print media, the figures, text and other content are disconnected from the
-			underlying data, making them hard to understand, evaluate and trust. Digital media, such as
-			online papers and articles, present an opportunity to make visual artifacts which are
-			connected to data and able to reveal those fine-grained relationships to an interested user.
-			This would enable research outputs, news articles and other data-driven artifacts to be more
-			transparent, self-explanatory and explorable &mdash; a nice goal but one which would be
-			impractical if we had to implement these features by hand for each output. <span
-				class="highlight">Fluid</span
-			> is a programming language that tracks how data flows through a computation and makes it possible
-			to author computational outputs where various transparency features are built-in.
-		</p>
+		<p> With traditional print media, the figures, text and other content are disconnected from the
+		underlying data, making them hard to understand, evaluate and trust. Fluid is a programming language
+		that tracks how data flows through a computation, making it possible to create computational outputs
+		which are automatically linked to the underlying data.</p>
+
+      <p>For some examples, see the <a
+      href="https://github.com/explorable-viz/fluid-article">fluid-article</a> template repository.</p>
 
 		<h3 class="title">Pure functional programming, Pythonic syntax</h3>
       <p>
       </p>
+
+		<h3 class="title">Getting started</h3>
 	</div>
 	<div></div>
 </Grid>
