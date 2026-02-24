a73cfe855e84dcd62dd6edc274e69c60f93f19fb
Roly Perera <roly.perera@dynamicaspects.org>
2026-02-24 16:11:45 +0000

    non-renewables.

---
+++ website/article-new/src/routes/non-renewables/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { DataPane, Figure, Grid } from '@explorable-viz/fluid';
+
+	let showDataPane = false;
+
+	const spec = {
+		fluidSrcPath: ['../fluid'],
+		inputs: ['nonRenewables'],
+		query: false,
+		linking: true
+	};
+
+	const toggleDataPane = () => {
+		showDataPane = !showDataPane;
+	};
+</script>
+
+<Grid {showDataPane}>
+	<div></div>
+	<div></div>
+	<div class="flex-left-align">
+		<h4>Non-Renewable Energy plots</h4>
+	</div>
+
+	<DataPane {showDataPane} toggle={toggleDataPane}>
+		<div class="flex-right-align data-pane">
+			<div id="fig-input"></div>
+		</div>
+	</DataPane>
+
+	<div class="flex-left-align">
+		<Figure {spec} fld="../fluid/non-renewables.fld" />
+	</div>
+</Grid>
+
+<style>
+	:global(.data-pane) {
+		max-width: 475px;
+	}
+
+	:global(.grid-container),
+	:global(.grid-container.data-pane-hidden) {
+		grid-template-columns: auto var(--toggle-button-width) var(--text-pane-width);
+	}
+
+	:global(.data-pane) {
+		padding-right: 0.25em;
+	}
+
+	:global(.data-pane *) {
+		font-size: 10pt;
+	}
+</style>
