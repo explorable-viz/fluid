710c3f24801b3fcafc375a9c60512249462f4719
Roly Perera <roly.perera@dynamicaspects.org>
2026-02-24 14:25:42 +0000

    Landing page.

--- website/article-new/src/routes/+page.svelte
+++ website/article-new/src/routes/+page.svelte
@@ -1,26 +1,31 @@
 <script>
    import { Grid } from '@explorable-viz/fluid';
-    import Page from './faq/+page.svelte';
 </script>
 
 <Grid>
 	<div></div>
 	<div></div>
-	<div class="flex-left-align">
-		<h3 class="title">Transparent research outputs</h3>
-		<p> With traditional print media, the figures, text and other content are disconnected from the
-		underlying data, making them hard to understand, evaluate and trust. Fluid is a programming language
-		that tracks how data flows through a computation, making it possible to create computational outputs
-		which are automatically linked to the underlying data.</p>
+   <div class="flex-left-align">
+      <div></div>
+      <h2>Fluid: Transparent Research Outputs</h2>
 
-      <p>For some examples, see the <a
-      href="https://github.com/explorable-viz/fluid-article">fluid-article</a> template repository.</p>
-
-		<h3 class="title">Pure functional programming, Pythonic syntax</h3>
-      <p>
-      </p>
-
-		<h3 class="title">Getting started</h3>
-	</div>
+      <!--<a href="renewables-linked">Transparent visualisations</a><br>-->
+      <ul>
+        <li>Linking visualisations to data</li>
+        <li>Transparent brushing-and-linking</li>
+      </ul>
+      <br>
+      <!--<a href="convolution">Infrastructure for explorable explanations</a><br>-->
+      <ul>
+        <li>Visualising intermediate values</li>
+        <li>Literate execution</li>
+      </ul>
+      <br>
+      <!--<a href="scigen-1805.02474v1-10">Transparent text</a><br>-->
+      <ul>
+        <li>Linking natural language to data</li>
+        <li>AI-assisted authoring tool</li>
+      </ul>
+   </div>
 	<div></div>
 </Grid>
