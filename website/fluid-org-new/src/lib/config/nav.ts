f5eaac31536d52391e1a22166e49ca77701cf398
Roly Perera <roly.perera@dynamicaspects.org>
2026-02-24 11:30:08 +0000

    GitHub link at the end; drop font-weight 500.

--- website/fluid-org-new/src/lib/config/nav.ts
+++ website/fluid-org-new/src/lib/config/nav.ts
@@ -1,7 +1,7 @@
 export const headerLinks = [
-	{ url: 'https://github.com/explorable-viz/fluid', title: 'GitHub' },
 	{ url: '/', title: 'Overview' },
 	{ url: '/faq', title: 'FAQ' },
 	{ url: '/supporters', title: 'Supporters' },
-	{ url: '/research', title: 'Research' }
+	{ url: '/research', title: 'Research' },
+	{ url: 'https://github.com/explorable-viz/fluid', title: 'GitHub' }
 ];
