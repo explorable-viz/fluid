6339bade4d6a477235227471ed50633a0a9cb504
Roly Perera <roly.perera@dynamicaspects.org>
2026-02-24 16:04:47 +0000

    energy-scatter.

---
+++ website/article-new/src/routes/energy-scatter/+page.svelte
@@ -0,0 +1,57 @@
+<script>
+	import { DataPane, Figure, Grid } from '@explorable-viz/fluid';
+
+	let showDataPane = false;
+
+	const spec = {
+		fluidSrcPath: ['../fluid'],
+		inputs: ['nonRenewables', 'renewables'],
+		query: false,
+		linking: true
+	};
+
+	const toggleDataPane = () => {
+		showDataPane = !showDataPane;
+	};
+</script>
+
+<Grid {showDataPane}>
+	<div></div>
+	<div></div>
+	<div class="flex-left-align">
+		<h3>Clean energy efficiency scatter plot</h3>
+	</div>
+
+	<DataPane {showDataPane} toggle={toggleDataPane}>
+		<div id="fig-data-pane" class="flex-right-align data-pane">
+			<div id="fig-input" class="data-pane-column"></div>
+		</div>
+	</DataPane>
+
+	<div class="flex-left-align">
+		<Figure {spec} fld="../fluid/energyscatter.fld" />
+	</div>
+</Grid>
+
+<style>
+	:global(.grid-container),
+	:global(.grid-container.data-pane-hidden) {
+		grid-template-columns: 0.8fr var(--toggle-button-width) var(--text-pane-width);
+	}
+
+	:global(.grid-container.double-size) {
+		max-width: 66.7vw;
+	}
+
+	:global(.data-pane) {
+		padding-right: 0.25em;
+	}
+
+	:global(.data-pane *) {
+		font-size: 10pt;
+	}
+
+	:global(.data-pane-column) {
+		border-right: 1px dotted #cccccc;
+	}
+</style>
