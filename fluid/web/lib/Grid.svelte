1c02b5b14a4848d3a6ee98b727fa4de4c819d2e8
Roly Perera <roly.perera@dynamicaspects.org>
2026-02-25 10:54:36 +0000

    Move .grid-container styling to Grid. Introduce doubleSize property on Grid.

--- fluid/web/lib/Grid.svelte
+++ fluid/web/lib/Grid.svelte
@@ -1,7 +1,33 @@
 <script lang="ts">
-	let { children, showDataPane } = $props();
+let {
+		children,
+		showDataPane,
+		doubleSize = false
+	} = $props();
 </script>
 
-<div id="grid" class="grid-container {showDataPane ? '' : 'data-pane-hidden'}">
+<div id="grid" class="grid-container
+   {showDataPane ? '' : 'data-pane-hidden'}
+   {doubleSize ? 'double-size' : ''}">
 	{@render children()}
 </div>
+
+<style>
+   .grid-container {
+      display: grid;
+      grid-template-columns: auto var(--toggle-button-width) var(--text-pane-width);
+      justify-content: center;
+   }
+
+   .grid-container.data-pane-hidden {
+      grid-template-columns: 0 var(--toggle-button-width) var(--text-pane-width);
+   }
+
+   .grid-container.double-size {
+      max-width: 66.7vw; /* undo effect of transform: scale(1.5) for layout purposes */
+   }
+
+   .grid-container > :last-child {
+      padding-right: 10px;
+   }
+</style>
