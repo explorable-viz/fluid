5d7dad7f4ed58567da4b1cd730da6b02309129ca
Roly Perera <roly.perera@dynamicaspects.org>
2026-03-21 10:31:38 +0000

    ❗ [fix]: Paths to fluid/shared

--- website/article/test.mjs
+++ website/article/test.mjs
@@ -1,4 +1,4 @@
-import { runTests, testURL, waitFor } from "../../dist/fluid/shared/webtest-lib.js"
+import { runTests, testURL, waitFor } from "../../fluid/dist/fluid/shared/webtest-lib.js"
 
 export const main = async () => {
    await runTests(testURL("convolution")([ page => waitFor("svg#fig-output")(page) ]))()
